1a3af41466783368cbd85237dcd0444bd531e77b
SongHayoung <37806785+SongHayoung@users.noreply.github.com>
2021-01-15 21:15:49 +0900

    -

---
+++ unique-word-abbreviation.cpp
@@ -0,0 +1,26 @@
+class ValidWordAbbr {
+private:
+    unordered_map<string, set<string>> words;
+
+private:
+    string getAbbreviation(string word) {
+        return word.length() <= 2 ? word : word[0] + to_string(word.length() - 2) + word[word.length() - 1];
+    }
+public:
+    ValidWordAbbr(vector<string>& dictionary) {
+        for(string word : dictionary) {
+            string abbreviation = getAbbreviation(word);
+            auto it = words.find(abbreviation);
+            if(it == words.end()) {
+                words.insert(make_pair(abbreviation, set<string> {word}));
+            } else {
+                it->second.insert(word);
+            }
+        }
+    }
+
+    bool isUnique(string word) {
+        auto it = words.find(getAbbreviation(word));
+        return it == words.end() || (*(it->second.begin()) == word && it->second.size() == 1);
+    }
+};
