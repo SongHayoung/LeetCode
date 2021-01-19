1393404b1ed12327ce7b3a8b275b7459c081d914
SongHayoung <37806785+SongHayoung@users.noreply.github.com>
2021-01-19 19:12:19 +0900

    -

---
+++ prime-palindrome.cpp
@@ -0,0 +1,51 @@
+class Solution {
+private:
+    bool isPrime(int n) {
+        int sqrtNum = sqrt(n);
+        for(int i = 2; i <= sqrtNum; i++) {
+            if(!(n % i))
+                return false;
+        }
+        return true;
+    }
+
+    int recursive(string num, int &len, int &N) {
+        if(num.length() == len) {
+            if(stoi(num) >= N && isPrime(stoi(num))) {
+                return stoi(num);
+            }
+            return -1;
+        }
+
+        if(len > num.length() * 2) {
+            for(int i = 0; i <= 9; i++) {
+                int ret = recursive(num + to_string(i), len, N);
+                if(ret != -1)
+                    return ret;
+            }
+
+            return -1;
+        } else {
+            return recursive(num + num[len - num.length() - 1], len, N);
+        }
+    }
+public:
+    int primePalindrome(int N) {
+        if(N <= 11) {
+            for(int i = max(N, 2); i <= 11; i++) {
+                if(isPrime(i))
+                    return i;
+            }
+        }
+        string num = to_string(N);
+        for(int len = num.length(); len <= 9; len++) {
+            for(int num = 1; num <= 9; num += 2) {
+                int ret = recursive(to_string(num), len, N);
+                if(ret != -1) {
+                    return ret;
+                }
+            }
+        }
+        return -1;
+    }
+};
