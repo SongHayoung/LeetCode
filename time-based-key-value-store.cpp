e24f3b12080764af2d107c2b3296389ad19e3fb8
SongHayoung <37806785+SongHayoung@users.noreply.github.com>
2021-05-01 13:41:00 +0900

    -

---
+++ time-based-key-value-store.cpp
@@ -0,0 +1,22 @@
+class TimeMap {
+    unordered_map<string, map<int, string>> m;
+public:
+    /** Initialize your data structure here. */
+    TimeMap() {}
+
+    void set(string key, string value, int timestamp) {
+        m[key][timestamp] = value;
+    }
+
+    string get(string key, int timestamp) {
+        auto res = m[key].upper_bound(timestamp);
+        return res == m[key].begin() ? "" : prev(res)->second;
+    }
+};
+
+/**
+ * Your TimeMap object will be instantiated and called as such:
+ * TimeMap* obj = new TimeMap();
+ * obj->set(key,value,timestamp);
+ * string param_2 = obj->get(key,timestamp);
+ */
