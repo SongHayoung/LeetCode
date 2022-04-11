5a665c16f5e541ff04513fc8f0e61e48edde647e
SongHayoung <37806785+SongHayoung@users.noreply.github.com>
2022-04-11 10:50:16 +0900

    Time: 0 ms (100.00%), Space: 6 MB (55.41%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.cpp
+++ 70-climbing-stairs/70-climbing-stairs.cpp
@@ -1,8 +1,8 @@
 class Solution {
 public:
     int climbStairs(int n) {
-        int dp = 1, res = 1;
-        for(int i = 2, tmp = 1; i <= n; i++, dp = res, res = tmp) {
+        int res = 1;
+        for(int i = 2, tmp = 1, dp = 1; i <= n; i++, dp = res, res = tmp) {
             tmp = res + dp;
         }
         return res;
