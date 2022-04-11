b031d0e2867eedd8402bee2a2418303a8d3de38a
SongHayoung <37806785+SongHayoung@users.noreply.github.com>
2022-04-11 11:02:34 +0900

    Time: 4 ms (47.08%), Space: 6 MB (58.46%) - LeetHub

---
+++ 171-excel-sheet-column-number/171-excel-sheet-column-number.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int titleToNumber(string columnTitle) {
+        int res = 0;
+        for(auto ch : columnTitle) {
+            res = res * 26 + (ch - 'A' + 1);
+        }
+        return res;
+    }
+};
