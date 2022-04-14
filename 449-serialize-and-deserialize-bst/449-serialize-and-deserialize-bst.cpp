79c8658ddd6076e444e149640ce59ca631bce4a9
SongHayoung <37806785+SongHayoung@users.noreply.github.com>
2022-04-14 11:05:59 +0900

    Time: 42 ms (72.06%), Space: 26.8 MB (70.00%) - LeetHub

---
+++ 449-serialize-and-deserialize-bst/449-serialize-and-deserialize-bst.cpp
@@ -0,0 +1,60 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Codec {
+    void sHelper(TreeNode* node, string& res) {
+        if(!node) res += "#,";
+        else {
+            res += to_string(node->val) + ',';
+            sHelper(node->left, res);
+            sHelper(node->right, res);
+        }
+    }
+    int parseNum(string& s, int& p) {
+        int res = 0;
+        while(p < s.length() and isdigit(s[p])) {
+            res = res * 10 + (s[p++] & 0b1111);
+        }
+        p++;
+        return res;
+    }
+    TreeNode* dHelper(string& d, int& i) {
+        if(i >= d.length()) return NULL;
+        if(d[i] == '#') {
+            i += 2;
+            return NULL;
+        }
+        TreeNode* node = new TreeNode(parseNum(d,i));
+        node->left = dHelper(d,i);
+        node->right = dHelper(d,i);
+        
+        return node;
+    }
+public:
+
+    // Encodes a tree to a single string.
+    string serialize(TreeNode* root) {
+        string res = "";
+        sHelper(root, res);
+        return res;
+    }
+
+    // Decodes your encoded data to tree.
+    TreeNode* deserialize(string data) {
+        int p = 0;
+        return dHelper(data, p);
+    }
+};
+
+// Your Codec object will be instantiated and called as such:
+// Codec* ser = new Codec();
+// Codec* deser = new Codec();
+// string tree = ser->serialize(root);
+// TreeNode* ans = deser->deserialize(tree);
+// return ans;
