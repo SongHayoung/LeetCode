6bc6f681f13f5e4d4b177f8c10cab69ce37b0790
SongHayoung <37806785+SongHayoung@users.noreply.github.com>
2022-04-15 10:50:07 +0900

    Time: 1397 ms (19.05%), Space: 97.1 MB (88.44%) - LeetHub

--- 1862-sum-of-floored-pairs/1862-sum-of-floored-pairs.cpp
+++ 1862-sum-of-floored-pairs/1862-sum-of-floored-pairs.cpp
@@ -3,9 +3,9 @@ public:
     int sumOfFlooredPairs(vector<int>& A) {
         sort(begin(A), end(A));
         int res = 0, n = A.size(), prevRes = 0, mod = 1e9 + 7;
-        for(int i = 0; i < n; i++) {
+        for(int i = 0, p, st = 0; i < n; st = ++i) {
             if(!i or A[i] != A[i-1]) {
-                int st = i, p; prevRes = 0;
+                prevRes = 0;
                 while(st < n) {
                     p = (A[st] / A[i]) + 1;
                     int nxt = lower_bound(begin(A) + st, end(A), A[i] * p) - begin(A);
