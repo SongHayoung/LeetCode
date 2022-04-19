6ce5afa394fbd10b6a2b3ab7610ffd333c85993d
SongHayoung <37806785+SongHayoung@users.noreply.github.com>
2022-04-19 10:08:52 +0900

    Time: 100 ms (63.69%), Space: 54.6 MB (32.48%) - LeetHub

--- 1230-toss-strange-coins/1230-toss-strange-coins.cpp
+++ 1230-toss-strange-coins/1230-toss-strange-coins.cpp
@@ -1,18 +1,11 @@
 class Solution {
     vector<vector<double>> dp;
     double helper(vector<double>& p, int t, int k) {
-        if(k == p.size()) return t == 0 ? 1.0 : 0.0;
-        if(p.size() - k < t) return 0;
-        if(t < 0) return 0;
+        if(k == p.size()) return t == 0;
+        if(p.size() - k < t or t < 0) return 0;
         if(dp[k][t] != -1) return dp[k][t];
         
-        auto& res = dp[k][t] = 0;
-        
-        double fo = helper(p,t-1,k+1) * p[k], ba = helper(p,t,k+1) * (1-p[k]);
-        
-        res = fo + ba;
-        
-        return res;
+        return dp[k][t] = helper(p,t-1,k+1) * p[k] + helper(p,t,k+1)* (1-p[k]);
     }
 public:
     double probabilityOfHeads(vector<double>& prob, int target) {
