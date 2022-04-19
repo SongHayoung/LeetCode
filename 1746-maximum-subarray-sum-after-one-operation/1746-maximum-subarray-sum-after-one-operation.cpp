98c54d447704b8c896c43b634e7504e2d44245cc
SongHayoung <37806785+SongHayoung@users.noreply.github.com>
2022-04-19 10:32:24 +0900

    Time: 125 ms (82.24%), Space: 71.1 MB (38.82%) - LeetHub

--- 1746-maximum-subarray-sum-after-one-operation/1746-maximum-subarray-sum-after-one-operation.cpp
+++ 1746-maximum-subarray-sum-after-one-operation/1746-maximum-subarray-sum-after-one-operation.cpp
@@ -2,12 +2,12 @@ class Solution {
 public:
     int maxSumAfterOperation(vector<int>& A) {
         int n = A.size(), res = 0;
-        vector<int> fw(n,0);
+        vector<int> kadane(n,0);
         for(int i = 0, sum = 0; i < n; i++) {
-            fw[i] = sum = max(sum + A[i], 0);
+            kadane[i] = sum = max(sum + A[i], 0);
         }
         for(int i = n - 1, sum = 0; i >= 0; i--) {
-            res = max(res, A[i] * A[i] + (i ? fw[i-1] : 0) + sum);
+            res = max(res, A[i] * A[i] + (i ? kadane[i-1] : 0) + sum);
             sum = max(sum + A[i], 0);
         }
         return res;
