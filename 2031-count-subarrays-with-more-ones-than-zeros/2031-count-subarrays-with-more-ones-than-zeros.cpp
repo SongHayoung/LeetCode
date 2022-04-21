d2fc53f9c88d69794c4c1636b1d2dad74ab1573b
SongHayoung <37806785+SongHayoung@users.noreply.github.com>
2022-04-21 10:20:09 +0900

    Time: 111 ms (88.73%), Space: 77.3 MB (97.18%) - LeetHub

--- 2031-count-subarrays-with-more-ones-than-zeros/2031-count-subarrays-with-more-ones-than-zeros.cpp
+++ 2031-count-subarrays-with-more-ones-than-zeros/2031-count-subarrays-with-more-ones-than-zeros.cpp
@@ -1,20 +1,27 @@
 class Solution {
+    int fenwick[200003];
+    void update(int n) {
+        while(n < 200003) {
+            fenwick[n] += 1;
+            n += n & -n;
+        }
+    }
+    int query(int n) {
+        int res = 0;
+        while(n > 0) {
+            res += fenwick[n];
+            n -= n & -n;
+        }
+        return res;
+    }
 public:
     int subarraysWithMoreZerosThanOnes(vector<int>& nums) {
-        unordered_map<int, int> mp {{0,1}};
-        int sum = 0, eq = 0, over = 0, mod = 1e9 + 7, res = 0;
+        int sum = 100001, mod = 1e9 + 7, res = 0;
+        update(sum);
         for(auto& n : nums) {
             sum += (n ? 1 : -1);
-            
-            auto peq = eq, pov = over;
-            
-            eq = mp[sum];
-            
-            if(n) over = (peq + pov + 1) % mod;
-            else over = (pov - eq + mod) % mod;
-            
-            mp[sum]++;
-            res = (res + over) % mod;
+            res = (res + query(sum - 1)) % mod;
+            update(sum);
         }
         
         return res;
