ded8784c65c8d177a4a51f182be1f412630adcfc
SongHayoung <37806785+SongHayoung@users.noreply.github.com>
2022-04-26 15:05:32 +0900

    Time: 1117 ms (44.44%), Space: 172.8 MB (61.11%) - LeetHub

---
+++ 1956-minimum-time-for-k-virus-variants-to-spread/1956-minimum-time-for-k-virus-variants-to-spread.cpp
@@ -0,0 +1,28 @@
+#define all(a) begin(a), end(a)
+class Solution {
+    int manhattanDistance(int x1, int y1, int x2, int y2) {
+        return abs(x1-x2) + abs(y1-y2);
+    }
+public:
+    int minDayskVariants(vector<vector<int>>& P, int k) {
+        int l = 100, r = 0, t = 0, b = 100;
+        for(auto& p : P) {
+            l = min(l, p[0]);
+            r = max(r, p[0]);
+            b = min(b, p[1]);
+            t = max(t, p[1]);
+        }
+        int res = INT_MAX;
+        for(int x = l; x <= r; x++) {
+            for(int y = b; y <= t; y++) {
+                vector<int> d;
+                for(auto& p : P) {
+                    d.push_back(manhattanDistance(x,y,p[0],p[1]));
+                }
+                sort(all(d));
+                res = min(res, d[k-1]);
+            }
+        }
+        return res;
+    }
+};
