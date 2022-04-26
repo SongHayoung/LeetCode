344762ada85c5026262d9c1ed1cd385cf544f8aa
SongHayoung <37806785+SongHayoung@users.noreply.github.com>
2022-04-26 14:06:40 +0900

    Time: 153 ms (21.15%), Space: 7.8 MB (73.08%) - LeetHub

--- 1515-best-position-for-a-service-centre/1515-best-position-for-a-service-centre.cpp
+++ 1515-best-position-for-a-service-centre/1515-best-position-for-a-service-centre.cpp
@@ -10,26 +10,25 @@ public:
             l = min(l, (db) p[0]);
             r = max(r, (db) p[0]);
         }
-        db res = DBL_MAX, X = 0, Y = 0;
-        for(db alpha = 10; alpha >= 1e-5; alpha /= 10) {
-            for(db x = l; x <= r; x += alpha) {
-                for(db y = b; y <= t; y += alpha) {
-                    auto ed = [&](db sum, vector<int>& p) {
+        db res = DBL_MAX, X = 0, Y = 0, gab = 10;
+        while(gab >= 1e-5) {
+            for(db x = l; x <= r; x += gab) {
+                for(db y = b; y <= t; y += gab) {
+                    db distance = accumulate(all(P), 0.0, [&](db sum, vector<int>& p) {
                         return sum + sqrt((x-p[0])*(x-p[0]) + (y-p[1])*(y-p[1]));
-                    };
-                    
-                    db d = accumulate(all(P), 0.0, ed);
-                    if(res > d) {
-                        res = d;
+                    });
+                    if(res > distance) {
+                        res = distance;
                         X = x;
                         Y = y;
                     }
                 }
             }
-            l = X - alpha;
-            r = X + 2*alpha;
-            b = Y - alpha;
-            t = Y + 2*alpha;
+            l = X - gab;
+            r = X + 2*gab;
+            b = Y - gab;
+            t = Y + 2*gab;
+            gab /= 10;
         }
         return res;
     }
