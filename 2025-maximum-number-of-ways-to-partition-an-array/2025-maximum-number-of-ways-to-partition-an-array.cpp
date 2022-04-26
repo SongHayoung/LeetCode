1bc37554594273be2df5a92abfe1c904de4611a8
SongHayoung <37806785+SongHayoung@users.noreply.github.com>
2022-04-26 10:56:01 +0900

    Time: 977 ms (60.00%), Space: 274.6 MB (7.03%) - LeetHub

--- 2025-maximum-number-of-ways-to-partition-an-array/2025-maximum-number-of-ways-to-partition-an-array.cpp
+++ 2025-maximum-number-of-ways-to-partition-an-array/2025-maximum-number-of-ways-to-partition-an-array.cpp
@@ -1,48 +1,39 @@
 class Solution {
-    int solve(vector<pair<int,int>>& q) {
-        sort(begin(q), end(q));
-        priority_queue<int, vector<int>, greater<int>> pq;
-        int sum = 0, res = 0;
-        for(int i = 0; i < q.size(); i++) {
-            while(!pq.empty() and pq.top() < q[i].first) {
-                sum -= 1;
-                pq.pop();
-            }
-            sum += 1;
-            pq.push(q[i].second);
-            res = max(res, sum);
-            if(sum + q.size() - i - 1 < res) break;
-        }
-        return res;
-    }
 public:
     int waysToPartition(vector<int>& nums, int k) {
         int n = nums.size();
         vector<long long> psum(n, nums[0]);
         unordered_map<long long,deque<int>> r,l{{nums[0],{0}}};
-        unordered_map<long long, vector<pair<int,int>>> query;
         for(int i = 1; i < n; i++) {
             psum[i] = psum[i-1] + nums[i];
             r[nums[i]].push_back(i);
         }
         int res = 0;
+        vector<int> q1(n,0),q2(n,0);
+        unordered_map<int, int> mp;
         for(int i = 0; i < n - 1; i++) {
-            long long lreq = psum.back() - psum[i] - psum[i], rreq = -lreq;
+            long long lreq = psum.back() - 2 * psum[i], rreq = -lreq;
             if(rreq == lreq) res++;
             else {
-                if(!r[k-rreq].empty()) {
-                    query[k-rreq].push_back({r[k-rreq].front(), r[k-rreq].back()});
+                if(r.count(k-rreq)) {
+                    q1[r[k-rreq].front()]++;
+                    q2[r[k-rreq].back()]--;
                 }
-                if(!l[k-lreq].empty()) {
-                    query[k-lreq].push_back({l[k-lreq].front(), l[k-lreq].back()});
+                if(l.count(k-lreq)) {
+                    q1[l[k-lreq].front()]++;
+                    q2[l[k-lreq].back()]--;
                 }
             }
+            if(l[nums[i+1]].size() == 2) l[nums[i+1]].pop_back();
             l[nums[i+1]].push_back(i+1);
             r[nums[i+1]].pop_front();
+            if(r[nums[i+1]].empty()) r.erase(nums[i+1]);
         }
         
-        for(auto& [_, q] : query) {
-            res = max(res,solve(q));
+        for(int i = 0; i < n; i++) {
+            mp[nums[i]] += q1[i];
+            res = max(res, mp[nums[i]]);
+            mp[nums[i]] += q2[i];
         }
         
         return res;
