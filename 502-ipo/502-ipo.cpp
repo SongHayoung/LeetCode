6d2ac240be90f26e6f90e2c926ee0878d2837b11
SongHayoung <37806785+SongHayoung@users.noreply.github.com>
2022-04-26 23:50:02 +0900

    Time: 308 ms (51.95%), Space: 82.1 MB (40.45%) - LeetHub

--- 502-ipo/502-ipo.cpp
+++ 502-ipo/502-ipo.cpp
@@ -1,16 +1,16 @@
 class Solution {
 public:
-    int findMaximizedCapital(int k, int w, vector<int>& p, vector<int>& c) {
+    int findMaximizedCapital(int k, int res, vector<int>& p, vector<int>& c) {
         int n = p.size();
         priority_queue<int> pq;
         vector<pair<int,int>> v;
         for(int i = 0; i < n; i++) {
             v.push_back({c[i], p[i]});
         }
-        int i = 0, res = 0;
+        int i = 0;
         sort(begin(v), end(v));
         while(k--) {
-            while(i < n and v[i].first <= res + w) {
+            while(i < n and v[i].first <= res) {
                 pq.push(v[i++].second);
             }
             if(!pq.empty()) {
@@ -18,6 +18,6 @@ public:
             }
         }
         
-        return res + w;
+        return res;
     }
 };
