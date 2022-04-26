02d816379734160a2ac9a8f9e476fe0c2a06038c
SongHayoung <37806785+SongHayoung@users.noreply.github.com>
2022-04-26 12:20:43 +0900

    Time: 2828 ms (5.35%), Space: 94.1 MB (80.66%) - LeetHub

--- 2040-kth-smallest-product-of-two-sorted-arrays/2040-kth-smallest-product-of-two-sorted-arrays.cpp
+++ 2040-kth-smallest-product-of-two-sorted-arrays/2040-kth-smallest-product-of-two-sorted-arrays.cpp
@@ -1,34 +1,5 @@
 #define all(a) begin(a), end(a)
 class Solution {
-    long long mi(vector<int>& A1, vector<int>& A2, vector<int>& B1, vector<int>& B2, int A3, int B3) {
-        long long mi = LLONG_MAX;
-        if(A1.size() and B2.size())
-            mi = min(mi, 1ll * A1[0] * B2.back());
-        if(A2.size() and B1.size())
-            mi = min(mi, 1ll * A2.back() * B1[0]);
-        if(A3 or B3)
-            mi = min(mi, 0ll);
-        if(A1.size() and B1.size())
-            mi = min(mi, 1ll * A1.back() * B1.back());
-        if(A2.size() and B2.size())
-            mi = min(mi, 1ll * A2[0] * B2[0]);
-        return mi;
-    }
-    
-    long long ma(vector<int>& A1, vector<int>& A2, vector<int>& B1, vector<int>& B2, int A3, int B3) {
-        long long ma = LLONG_MIN;
-        if(A1.size() and B2.size())
-            ma = max(ma, 1ll * A1.back() * B2[0]);
-        if(A2.size() and B1.size())
-            ma = max(ma, 1ll * A2[0] * B1.back());
-        if(A3 or B3)
-            ma = max(ma, 0ll);
-        if(A1.size() and B1.size())
-            ma = max(ma, 1ll * A1[0] * B1[0]);
-        if(A2.size() and B2.size())
-            ma = max(ma, 1ll * A2.back() * B2.back());
-        return ma;
-    }
     pair<long long, bool> search(vector<int>& A, vector<int>& B, long long t) {
         if(A.size() < B.size())
             return search(B, A, t);
@@ -57,10 +28,7 @@ class Solution {
     }
 public:
     long long kthSmallestProduct(vector<int>& A, vector<int>& B, long long k) {
-       
-        
-        //long long res = LLONG_MAX, l = mi(A1,A2,B1,B2,A3,B3), r = ma(A1,A2,B1,B2,A3,B3);
-        long long res = LLONG_MAX, l = LLONG_MIN/2, r = LLONG_MAX/2;
+        long long res = LLONG_MAX/2, l = LLONG_MIN, r = LLONG_MAX/2;
         while(l <= r) {
             long long m = (l + r) / 2;
             auto [s, h] = search(A,B,m);
