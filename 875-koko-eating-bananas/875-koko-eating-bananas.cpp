c7a6c13cd419e421b638ad90c7892289d63804c2
SongHayoung <37806785+SongHayoung@users.noreply.github.com>
2022-04-27 10:19:19 +0900

    Time: 64 ms (47.57%), Space: 19 MB (27.50%) - LeetHub

---
+++ 875-koko-eating-bananas/875-koko-eating-bananas.cpp
@@ -0,0 +1,19 @@
+class Solution {
+    long search(vector<int>& p, long m) {
+        long res = accumulate(begin(p), end(p), 0l, [&](long sum, int b) {
+           return sum + ceil(1.0 * b / m); 
+        });
+        return res;
+    }
+public:
+    int minEatingSpeed(vector<int>& piles, int h) {
+        long l = 1, r = INT_MAX;
+        while(l <= r) {
+            long m = (l + r) / 2;
+            long s = search(piles, m);
+            if(s > h) l = m + 1;
+            else r = m - 1;
+        }
+        return l;
+    }
+};
