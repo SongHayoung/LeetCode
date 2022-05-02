b94d796123e50b652a197cc9aaab7ee6f71ca67c
SongHayoung <37806785+SongHayoung@users.noreply.github.com>
2022-05-03 00:40:13 +0900

    Time: 368 ms (50.88%), Space: 17.2 MB (5.26%) - LeetHub

--- 548-split-array-with-equal-sum/548-split-array-with-equal-sum.cpp
+++ 548-split-array-with-equal-sum/548-split-array-with-equal-sum.cpp
@@ -12,18 +12,17 @@ public:
         
         for(int i = 1; i < n; i++) {
             int sum = psum[i];
-            if(i + 1 > n) continue;
+            if(i + 1 >= n) continue;
             for(auto& j : mp[psum[i+1] + sum]) {
-                if(j + 2 > n or j <= i) continue;
+                if(j + 2 >= n or j <= i) continue;
                 if(vis[j][0].count(sum)) continue;
                 
                 vis[j][0].insert(sum);
                 for(auto& k : mp[psum[j+2] + sum]) {
-                    if(k + 2 > n or k <= j + 1) continue;
+                    if(k + 2 >= n or k <= j + 1) continue;
                     if(vis[k][1].count(sum)) continue;
                     
                     vis[k][1].insert(sum);
-                    if(n - 1 <= k + 1) continue;
                     if(psum.back() == psum[k+2] + sum)
                         return true;
                 }
