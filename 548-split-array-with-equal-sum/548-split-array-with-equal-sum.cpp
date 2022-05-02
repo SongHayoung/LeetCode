a906a24b22b3475400f6011a8cb9dce1d22e8bd0
SongHayoung <37806785+SongHayoung@users.noreply.github.com>
2022-05-03 00:35:33 +0900

    Time: 384 ms (45.61%), Space: 17.4 MB (5.26%) - LeetHub

---
+++ 548-split-array-with-equal-sum/548-split-array-with-equal-sum.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    bool splitArray(vector<int>& A) {
+        int n = A.size();
+        vector<int> psum(n + 1, 0);
+        unordered_map<int, vector<int>> mp;
+        for(int i = 0; i < n; i++) {
+            mp[psum[i+1] = psum[i] + A[i]].push_back(i);
+        }
+        unordered_set<int> vis[n+1][4];
+        for(int i = 1; i < n; i++) {
+            int sum = psum[i];
+            if(i + 1 > n) continue;
+            for(auto& j : mp[psum[i+1] + sum]) {
+                if(j + 2 > n) continue;
+                if(j <= i) continue;
+                if(vis[j][0].count(sum)) continue;
+                vis[j][0].insert(sum);
+                for(auto& k : mp[psum[j+2] + sum]) {
+                    if(k + 2 > n) continue;
+                    if(k <= j+1) continue;
+                    if(vis[k][1].count(sum)) continue;
+                    vis[k][1].insert(sum);
+                    for(auto& l : mp[psum[k+2] + sum]) {
+                        if(l <= k + 1) continue;
+                        if(l == n - 1) {
+                            return true;
+                        }
+                    }
+                }
+            }
+        }
+        return false;
+    }
+};
