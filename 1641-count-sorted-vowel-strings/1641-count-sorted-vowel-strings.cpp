e505240c1d7485bfb1d0f58f7232a1d2bf913ef2
SongHayoung <37806785+SongHayoung@users.noreply.github.com>
2022-05-11 09:55:14 +0900

    Time: 4 ms (35.46%), Space: 6 MB (58.60%) - LeetHub

--- 1641-count-sorted-vowel-strings/1641-count-sorted-vowel-strings.cpp
+++ 1641-count-sorted-vowel-strings/1641-count-sorted-vowel-strings.cpp
@@ -1,14 +1,5 @@
 class Solution {
     int dp[51][5];
-    int helper(int n, int m) {
-        if(n == -1) return 1;
-        if(m == -1) return 0;
-        if(dp[n][m] != -1) return dp[n][m];
-        dp[n][m] = 0;
-        for(int i = 0; i <= m; i++)
-            dp[n][m] += helper(n - 1, i);
-        return dp[n][m];
-    }
 public:
     int countVowelStrings(int n) {
         memset(dp, 0, sizeof dp);
