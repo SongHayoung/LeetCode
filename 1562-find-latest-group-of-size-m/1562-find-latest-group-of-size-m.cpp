0d716cfef635786cee4f332c4006c2b431506bed
SongHayoung <37806785+SongHayoung@users.noreply.github.com>
2022-05-12 09:56:39 +0900

    Time: 169 ms (86.22%), Space: 82.7 MB (61.78%) - LeetHub

--- 1562-find-latest-group-of-size-m/1562-find-latest-group-of-size-m.cpp
+++ 1562-find-latest-group-of-size-m/1562-find-latest-group-of-size-m.cpp
@@ -1,44 +1,16 @@
 class Solution {
-    int merge(map<int,int>& mp, int pos, int& m) {
-        auto it = mp.lower_bound(pos);
-        bool wasM = false;
-        if(it != end(mp) and it->first == pos + 1) {
-            auto nxt = it;
-            if(nxt->second - nxt->first + 1 == m)
-                wasM = true;
-            it = mp.insert({pos, nxt->second}).first;
-            mp.erase(nxt);
-        } else {
-            it = mp.insert({pos, pos}).first;
-        }
-        
-        if(it != begin(mp)) {
-            auto prv = prev(it);
-            if(prv->second + 1 == it->first) {
-                auto nxt = it;
-                it = prv;
-                if(it->second - it->first + 1 == m)
-                    wasM = true;
-                it->second = nxt->second;
-                mp.erase(nxt);
-            }
-        }
-        if(it->second - it->first + 1 == m)
-            return 1;
-        if(wasM)
-            return -1;
-        return 0;
-    }
 public:
     int findLatestStep(vector<int>& arr, int m) {
-        map<int, int> mp;
-        int res = -1;
-        for(int i = 0; i < arr.size(); i++) {
-            int merged = merge(mp, arr[i], m);
-            if(merged == 1)
+        int n = arr.size(), res = -1;
+        vector<int> merge(n + 2), count(n + 1);
+        for(int i = 0; i < n; i++) {
+            int a = arr[i], left = merge[a-1], right = merge[a+1];
+            merge[a] = merge[a-left] = merge[a+right] = right + left + 1;
+            count[left]--;
+            count[right]--;
+            count[merge[a]]++;
+            if(count[m])
                 res = i + 1;
-            else if(merged == -1)
-                res = i;
         }
         return res;
     }
