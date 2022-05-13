8449945f66e20fa68c582d259e86a867d5b7f36a
SongHayoung <37806785+SongHayoung@users.noreply.github.com>
2022-05-13 15:48:57 +0900

    Time: 585 ms (29.17%), Space: 144.6 MB (16.25%) - LeetHub

--- 2013-detect-squares/2013-detect-squares.cpp
+++ 2013-detect-squares/2013-detect-squares.cpp
@@ -1,34 +1,28 @@
 class DetectSquares {
-    int p[1001][1001];
-    bool valid(pair<int,int> p) {
-        return 0 <= p.first and p.first <= 1000 and 0 <= p.second and p.second <= 1000;
-    }
-    int get(pair<int,int> p1,pair<int,int> p2,pair<int,int> p3,pair<int,int> p4) {
-        if(!valid(p1)) return 0;
-        if(!valid(p2)) return 0;
-        if(!valid(p2)) return 0;
-        if(!valid(p3)) return 0;
-        
-        return p[p2.first][p2.second] * p[p3.first][p3.second] * p[p4.first][p4.second];
+    set<int> yx[1001];
+    int p[1001][1001] = {0,};
+    int get(int y, int x) {
+        if(0 <= y and y <= 1000 and 0 <= x and x <= 1000)
+            return p[y][x];
+        return 0;
     }
 public:
-    DetectSquares() {
-        memset(p, 0, sizeof p);
-    }
+    DetectSquares() {}
     
     void add(vector<int> point) {
-        p[point[0]][point[1]]++;
+        int y = point[0], x = point[1];
+        p[y][x]++;
+        yx[y].insert(x);
     }
     
     int count(vector<int> point) {
-        int res = 0, y = point[0], x = point[1];
-        for(int distance = 1; distance <= 1000; distance++) {
-            res += get({y,x}, {y+distance,x-distance}, {y+distance,x}, {y,x-distance});
-            res += get({y,x}, {y+distance,x+distance}, {y+distance,x}, {y,x+distance});
-            res += get({y,x}, {y-distance,x-distance}, {y-distance,x}, {y,x-distance});
-            res += get({y,x}, {y-distance,x+distance}, {y-distance,x}, {y,x+distance});
+        int y = point[0], x = point[1], res = 0;
+        for(auto& X : yx[y]) {
+            int dis = x - X;
+            if(dis == 0) continue;
+            res += get(y+dis,X) * get(y,X) * get(y+dis,x);
+            res += get(y-dis,X) * get(y,X) * get(y-dis,x);
         }
-
         return res;
     }
 };
