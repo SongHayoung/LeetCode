174c311c0a0ccf1a726b9ae3028b36c9841ca60d
SongHayoung <37806785+SongHayoung@users.noreply.github.com>
2022-05-13 15:14:33 +0900

    Time: 27 ms (26.88%), Space: 13.8 MB (31.17%) - LeetHub

--- 539-minimum-time-difference/539-minimum-time-difference.cpp
+++ 539-minimum-time-difference/539-minimum-time-difference.cpp
@@ -1,29 +1,21 @@
 class Solution {
-    int parse(string& s) { //string to time with minites;
-        return ((s[0] & 0b1111)*10 + (s[1] & 0b1111)) * 60 + (s[3] & 0b1111) * 10 + (s[4] & 0b1111);
+    int toTime(string& timePoint) {
+        auto pos = timePoint.find(':');
+        return stoi(timePoint.substr(0,pos)) * 60 + stoi(timePoint.substr(pos + 1));
+    }
+    int compare(int a, int b) {
+        return (b - a + 1440) % 1440;
     }
 public:
     int findMinDifference(vector<string>& timePoints) {
-        if(timePoints.size() >= 1441) return 0; //duplications
-        int res = INT_MAX;
-        set<int> s;
-        for(auto& time : timePoints) {
-            int minute = parse(time);
-            if(s.empty()) { s.insert(minute);continue; }
-            if(!res) return res;
-            auto it = s.lower_bound(minute);
-            if(it != s.end()) { //after minute
-                res = min(res, *it - minute);
-            } else {
-                res = min(res, *s.begin() + 1440 - minute);
-            }
-
-            if(it == s.begin()) { //before minute
-                res = min(res, minute + 1440 - *prev(s.end()));
-            } else {
-                res = min(res, minute - *prev(it));
-            }
-            s.insert(minute);
+        vector<int> times;
+        for(auto& timePoint : timePoints) {
+            times.push_back(toTime(timePoint));
+        }
+        sort(begin(times), end(times));
+        int n = times.size(), res = INT_MAX;
+        for(int i = 0; i < n; i++) {
+            res = min(res, compare(times[i], times[(i + 1) % n]));
         }
         return res;
     }
