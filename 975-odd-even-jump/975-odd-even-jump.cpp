5adc38da360b93fa2fb6eb569fae96dda670005a
SongHayoung <37806785+SongHayoung@users.noreply.github.com>
2022-05-17 18:52:32 +0900

    Time: 229 ms (15.69%), Space: 35.2 MB (29.80%) - LeetHub

--- 975-odd-even-jump/975-odd-even-jump.cpp
+++ 975-odd-even-jump/975-odd-even-jump.cpp
@@ -1,36 +1,48 @@
 class Solution {
-    vector<vector<int>> jumpTo;
+    int n;
+    vector<int> oddTable;
+    vector<int> evenTable;
     vector<vector<int>> dp;
-    int helper(int pos, bool odd, int target) {
-        if(pos == target) return 1;
+    int helper(int pos, bool odd) {
         if(dp[pos][odd] != -1) return dp[pos][odd];
-        dp[pos][odd] = 0;
-        if(jumpTo[pos][odd] != -1)
-            dp[pos][odd] = helper(jumpTo[pos][odd], !odd, target);
-        return dp[pos][odd];
+        if(odd) {
+            if(oddTable[pos] == -1) return dp[pos][odd] = 0;
+            return dp[pos][odd] = helper(oddTable[pos], !odd);
+        } else {
+            if(evenTable[pos] == -1) return dp[pos][odd] = 0;
+            return dp[pos][odd] = helper(evenTable[pos], !odd);
+        }
     }
 public:
     int oddEvenJumps(vector<int>& arr) {
-        int n = arr.size();
-        jumpTo = vector<vector<int>>(n, vector<int>(2,-1));
+        n = arr.size();
+        oddTable = vector<int>(n, -1);
+        evenTable = vector<int>(n, -1);
         dp = vector<vector<int>>(n, vector<int>(2,-1));
+        dp[n - 1][0] = dp[n - 1][1] = 1;
+        
         map<int, int> mp;
         for(int i = n - 1; i >= 0; i--) {
-            auto it = mp.lower_bound(arr[i]);
-            if(it != end(mp))
-                jumpTo[i][1] = it->second;
-            if(it != end(mp) and it->first == arr[i])
-                jumpTo[i][0] = it->second;
-            else if(it != begin(mp))
-                jumpTo[i][0] = prev(it)->second;
-            mp[arr[i]] = i; 
+            auto lb = mp.lower_bound(arr[i]);
+            if(lb != end(mp)) {
+                oddTable[i] = lb->second;
+            }
+            auto ub = mp.upper_bound(arr[i]);
+            if(ub != begin(mp)) {
+                --ub;
+                evenTable[i] = ub->second;
+            }
+            
+            mp[arr[i]] = i;
         }
         
         
-        int res =0;
+        int res = 0;
+        
         for(int i = 0; i < n; i++) {
-            res += helper(i,true,n-1);
+            res += helper(i, true);
         }
+        
         return res;
     }
 };
