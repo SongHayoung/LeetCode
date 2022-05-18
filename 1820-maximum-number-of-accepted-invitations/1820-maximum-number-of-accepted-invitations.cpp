de4e98f2afbb78226a800fcf52aca3a81a0609b8
SongHayoung <37806785+SongHayoung@users.noreply.github.com>
2022-05-18 11:35:54 +0900

    Time: 725 ms (5.44%), Space: 95.9 MB (5.44%) - LeetHub

--- 1820-maximum-number-of-accepted-invitations/1820-maximum-number-of-accepted-invitations.cpp
+++ 1820-maximum-number-of-accepted-invitations/1820-maximum-number-of-accepted-invitations.cpp
@@ -1,31 +1,73 @@
 class Solution {
-    vector<int> seen, match;
-    bool dfs(int u, int r, vector<vector<int>>& adj) {
-        seen[u] = r;
-        for(auto v : adj[u]) {
-            if(match[v] == 0 or (seen[match[v]] != r and dfs(match[v], r, adj))) {
-                match[v] = u;
-                return true;
-            }
-        }
-        return false;
-    }
-public:
-    int maximumInvitations(vector<vector<int>>& grid) {
-        int res = 0, n = grid.size(), m = grid[0].size();
-        vector<vector<int>> adj(n + 1);
-        seen = vector<int>(n + 1);
-        match = vector<int>(m + 1);
-        for(int i = 0; i < n; i++) {
-            for(int j = 0; j < m; j++) {
-                if(grid[i][j]) {
-                    adj[i + 1].push_back(j + 1);
+    vector<vector<int>> flo, cap;
+    vector<int> path;
+    vector<vector<int>> adj;
+    int source, sink;
+    
+    bool bfs(int u, int v) {
+        path = vector<int>(sink + 1);
+        queue<int> q;
+        q.push(u);
+        while(!q.empty() and !path[v]) {
+            auto n = q.front(); q.pop();
+            for(auto& m : adj[n]) {
+                if(cap[n][m] - flo[n][m] > 0 and !path[m]) {
+                    path[m] = n;
+                    q.push(m);
                 }
             }
         }
-        for(int i = 1; i <= n; i++)
-            res += dfs(i, i, adj);
         
+        return path[v] != 0;
+    }
+    
+    int mcmf() {
+        int res = 0;
+        while(bfs(source, sink)) {
+            int v = sink, mi = INT_MAX;
+            while(v != source) {
+                int u = path[v];
+                mi = min(mi, cap[u][v] - flo[u][v]);
+                v = u;
+            }
+            v = sink;
+            while(v != source) {
+                int u = path[v];
+                flo[u][v] += mi;
+                flo[v][u] -= mi;
+                v = u;
+            }
+            res++;
+        }
         return res;
+    }
+public:
+    int maximumInvitations(vector<vector<int>>& grid) {
+        int n = grid.size(), m = grid[0].size();
+        source = n + m + 10, sink = n + m + 11;
+        path = vector<int>(sink + 1);
+        flo = cap = vector<vector<int>>(sink + 1, vector<int>(sink + 1));
+        adj = vector<vector<int>>(sink + 1);
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < m; j++) {
+                if(!grid[i][j]) continue;
+                adj[i+1].push_back(n + j+1);
+                adj[n + j+1].push_back(i+1);
+                
+                cap[i+1][n + j + 1] = 1;
+            }
+        }
+        for(int i = 0; i < n; i++) {
+            cap[source][i + 1] = 1;
+            adj[source].push_back(i + 1);
+            adj[i + 1].push_back(source);
+        }
+        for(int i = 0; i < m; i++) {
+            cap[n + i + 1][sink] = 1;
+            adj[sink].push_back(n + i+1);
+            adj[n + i+1].push_back(sink);
+        }
+        
+        return mcmf();
     }
 };
