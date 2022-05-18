3b58bcaad43592de168f75f08410238fccf53b9a
SongHayoung <37806785+SongHayoung@users.noreply.github.com>
2022-05-18 14:55:07 +0900

    Time: 10 ms (38.87%), Space: 9.3 MB (25.92%) - LeetHub

--- 920-number-of-music-playlists/920-number-of-music-playlists.cpp
+++ 920-number-of-music-playlists/920-number-of-music-playlists.cpp
@@ -1,12 +1,14 @@
 class Solution {
 public:
-    int numMusicPlaylists(int n, int l, int k) {
-        long long dp[101][101] = {1}, mod = 1e9 + 7;
-        for(long long i = 1; i <= l; i++) {
-            for(long long j = 1; j <= n; j++) {
-                dp[i][j] = (dp[i-1][j-1] * (n - j + 1) + dp[i-1][j] * (max(0ll, j - k))) % mod;
+    int numMusicPlaylists(int n, int goal, int k) {
+        long long mod = 1e9 + 7;
+        vector<vector<long long>> dp(n + 1, vector<long long>(goal + 1, 0));
+        dp[0][0] = 1;
+        for(long long i = 1; i <= n; i++) {
+            for(long long j = 1; j <= goal; j++) {
+                dp[i][j] = (dp[i-1][j-1] * (n - (i - 1)) + dp[i][j - 1] * (max(0ll, i - k))) % mod;
             }
         }
-        return dp[l][n];
+        return dp[n][goal];
     }
 };
