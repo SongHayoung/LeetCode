e60f4090ecb0ec4cedd673a0bee735e741855469
SongHayoung <37806785+SongHayoung@users.noreply.github.com>
2022-05-19 14:19:20 +0900

    Time: 361 ms (89.22%), Space: 77.5 MB (91.57%) - LeetHub

--- 354-russian-doll-envelopes/354-russian-doll-envelopes.cpp
+++ 354-russian-doll-envelopes/354-russian-doll-envelopes.cpp
@@ -1,20 +1,19 @@
 class Solution {
 public:
-    int maxEnvelopes(vector<vector<int>>& envelopes) {
-        unordered_map<int, unordered_set<int>> tree;
-        sort(envelopes.begin(), envelopes.end(), [](const vector<int>& v1, const vector<int>& v2) -> bool { return v1[0] == v2[0] ? v1[1] > v2[1] : v1[0] < v2[0]; });
-        int sz = envelopes.size();
-        vector<int> contain;
-        contain.push_back(INT_MIN);
-        for(int i = 0; i < sz; i++) {
-            if(contain.back() < envelopes[i][1])
-                contain.push_back(envelopes[i][1]);
+    int maxEnvelopes(vector<vector<int>>& A) {
+        sort(begin(A), end(A), [](vector<int>& a, vector<int>& b) {
+           if(a[0] == b[0]) return a[1] > b[1];
+            return a[0] < b[0];
+        });
+        vector<int> st;
+        for(auto& a : A) {
+            int n = a[1];
+            if(st.empty() or st.back() < n) st.push_back(n);
             else {
-                auto it = lower_bound(contain.begin(), contain.end(), envelopes[i][1]);
-                *it = envelopes[i][1];
+                auto lb = lower_bound(begin(st), end(st), n);
+                *lb = n;
             }
         }
-
-        return contain.size() - 1;
+        return st.size();
     }
 };
