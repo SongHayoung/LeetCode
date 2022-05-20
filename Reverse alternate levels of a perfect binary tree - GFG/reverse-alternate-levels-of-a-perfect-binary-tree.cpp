860ca3a30fe29bf16a32dc328cf7374e74d0ea27
SongHayoung <37806785+SongHayoung@users.noreply.github.com>
2022-05-20 18:08:38 +0900

    Added solution - LeetHub

---
+++ Reverse alternate levels of a perfect binary tree - GFG/reverse-alternate-levels-of-a-perfect-binary-tree.cpp
@@ -0,0 +1,145 @@
+// { Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node
+{
+    int data;
+    struct Node *left;
+    struct Node *right;
+};
+// Utility function to create a new Tree Node
+Node* newNode(int val)
+{
+    Node* temp = new Node;
+    temp->data = val;
+    temp->left = NULL;
+    temp->right = NULL;
+    
+    return temp;
+}
+// Function to Build Tree
+Node* buildTree(string str)
+{   
+    // Corner Case
+    if(str.length() == 0 || str[0] == 'N')
+            return NULL;
+    
+    // Creating vector of strings from input 
+    // string after spliting by space
+    vector<string> ip;
+    
+    istringstream iss(str);
+    for(string str; iss >> str; )
+        ip.push_back(str);
+        
+    // Create the root of the tree
+    Node* root = newNode(stoi(ip[0]));
+        
+    // Push the root to the queue
+    queue<Node*> queue;
+    queue.push(root);
+        
+    // Starting from the second element
+    int i = 1;
+    while(!queue.empty() && i < ip.size()) {
+            
+        // Get and remove the front of the queue
+        Node* currNode = queue.front();
+        queue.pop();
+            
+        // Get the current node's value from the string
+        string currVal = ip[i];
+            
+        // If the left child is not null
+        if(currVal != "N") {
+                
+            // Create the left child for the current node
+            currNode->left = newNode(stoi(currVal));
+                
+            // Push it to the queue
+            queue.push(currNode->left);
+        }
+            
+        // For the right child
+        i++;
+        if(i >= ip.size())
+            break;
+        currVal = ip[i];
+            
+        // If the right child is not null
+        if(currVal != "N") {
+                
+            // Create the right child for the current node
+            currNode->right = newNode(stoi(currVal));
+                
+            // Push it to the queue
+            queue.push(currNode->right);
+        }
+        i++;
+    }
+    
+    return root;
+}
+
+void reverseAlternate(struct Node *root);
+void inorder(Node * node)
+{
+    if(node==NULL)
+        return;
+    
+    inorder(node->left);
+    cout<<node->data<<" ";
+    inorder(node->right);
+}
+
+int main()
+{
+
+    int t;
+	scanf("%d ",&t);
+    while(t--)
+    {
+        string s;
+		getline(cin,s);
+        Node* root = buildTree(s);
+        reverseAlternate(root);
+        inorder(root);
+        cout<<endl;
+    }
+    return 1;
+}// } Driver Code Ends
+
+
+/* A binary tree Node
+struct Node
+{
+    int data;
+    Node* left, * right;
+}; */
+
+void reverseAlternate(Node *root)
+{
+    deque<Node*> dq;
+    bool reverse = false;
+    dq.push_back(root);
+    
+    while(!dq.empty()) {
+        int sz = dq.size();
+        if(reverse) {
+            int l = 0, r = sz - 1;
+            while(l < r) {
+                swap(dq[l++]->data, dq[r--]->data);
+            }
+        }
+        while(sz--) {
+            auto f = dq.front(); dq.pop_front();
+            if(f->left and f->right) {
+                dq.push_back(f->left);
+                dq.push_back(f->right);
+            }
+        }
+        
+        reverse = !reverse;
+    }
+}
