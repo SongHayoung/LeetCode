0b8fc506d06a1cdfd37726dfa022dc36ed725c1a
SongHayoung <37806785+SongHayoung@users.noreply.github.com>
2022-05-22 11:09:15 +0900

    Time: 5 ms (77.15%), Space: 6.6 MB (64.42%) - LeetHub

--- 647-palindromic-substrings/647-palindromic-substrings.cpp
+++ 647-palindromic-substrings/647-palindromic-substrings.cpp
@@ -1,22 +1,24 @@
 class Solution {
-    int compare(string& s, int start, int end) {
-        int res = 0;
-        while(start != -1 && end != s.length()) {
-            if(s[start] == s[end]) {
-                start--, end++, res++;
-            } else {
-                return res;
-            }
-        }
-        return res;
-    }
 public:
     int countSubstrings(string s) {
-        int res = 0;
-        for(int i = 0; i < s.length(); i++) {
-            res += 1 + compare(s, i - 1, i + 1) + compare(s, i, i + 1);
+        string ss = "#";
+        for(auto& ch : s) {
+            ss.push_back(ch);
+            ss.push_back('#');
         }
-
+        int n = ss.length(), res = 0;
+        vector<int> dp(n);
+        int l = 0, r = -1;
+        for(int i = 0; i < n; i++) {
+            dp[i] = max(0, min(r - i, (r + l - i >= 0 ? dp[r + l - i] : -1)));
+            while(i + dp[i] < n and i - dp[i] >= 0 and ss[i-dp[i]] == ss[i+dp[i]])
+                dp[i]++;
+            res += dp[i] / 2;
+            if(i + dp[i] > r) {
+                r = i + dp[i];
+                l = i - dp[i];
+            }
+        }
         return res;
     }
 };
