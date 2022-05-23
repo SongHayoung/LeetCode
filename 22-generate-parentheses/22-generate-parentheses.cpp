5590ca094894dd5c12d6c81445d63f319cda5a0c
SongHayoung <37806785+SongHayoung@users.noreply.github.com>
2022-05-23 14:06:27 +0900

    Time: 4 ms (71.44%), Space: 11.8 MB (71.03%) - LeetHub

--- 22-generate-parentheses/22-generate-parentheses.cpp
+++ 22-generate-parentheses/22-generate-parentheses.cpp
@@ -1,25 +1,23 @@
 class Solution {
-    bool valid(string& s) {
-        int st = 0;
-        for(auto& c: s) {
-            switch (c) {
-                case '(' : st++; break;
-                case ')' : st--; break;
+    vector<string> res;
+    void helper(string& s, int open, int close, int len, int st) {
+        if(len == s.length()) res.push_back(s);
+        else {
+            if(open) {
+                s.push_back('(');
+                helper(s, open - 1, close, len, st + 1);
+                s.pop_back();
             }
-            if(st < 0) return false;
+            if(close and st) {
+                s.push_back(')');
+                helper(s, open, close - 1, len, st - 1);
+                s.pop_back();
+            }
         }
-        return st == 0;
     }
 public:
     vector<string> generateParenthesis(int n) {
-        stringstream ss;
-        for(int i = 0; i < n; i++) ss << '(';
-        for(int i = 0; i < n; i++) ss << ')';
-        string s = ss.str();
-        vector<string> res;
-        do if(valid(s)){
-            res.push_back(s);
-        } while(next_permutation(begin(s), end(s)));
+        helper(string() = "", n, n, 2 * n, 0);
         return res;
     }
 };
