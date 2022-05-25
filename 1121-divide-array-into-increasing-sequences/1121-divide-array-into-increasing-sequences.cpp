15d7d18eb90ce3ff9b01218c21f6f7b07af97ccd
SongHayoung <37806785+SongHayoung@users.noreply.github.com>
2022-05-25 10:46:56 +0900

    Time: 167 ms (56.10%), Space: 73.5 MB (39.02%) - LeetHub

--- 1121-divide-array-into-increasing-sequences/1121-divide-array-into-increasing-sequences.cpp
+++ 1121-divide-array-into-increasing-sequences/1121-divide-array-into-increasing-sequences.cpp
@@ -1,42 +1,12 @@
 class Solution {
 public:
     bool canDivideIntoSubsequences(vector<int>& nums, int k) {
-        if(k == 1) return true;
-        unordered_map<int, int> freq;
-        for(auto& n : nums) freq[n]++;
-        
-        int mafreq = -1, f = -1;
-        for(auto& [n, c] : freq) {
-            if(c > f) {
-                mafreq = n;
-                f = c;
-            }
+        int mafreq = 0, l = 0, r = 0, n = nums.size();
+        while(r < n) {
+            while(r < n and nums[r] == nums[l]) r++;
+            mafreq = max(mafreq, r - l);
+            l = r;
         }
-        
-        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
-        
-        freq[mafreq] = 0;
-        pq.push({1, f});
-        
-        for(auto& [n, c] : freq) {
-            if(c == 0) continue;
-            if(pq.empty()) break;
-            while(c and !pq.empty()) {
-                auto [len, count] = pq.top(); pq.pop();
-                if(count <= c) {
-                    len += 1;
-                    c -= count;
-                    if(len == k) continue;
-                    else pq.push({len, count});
-                } else {
-                    pq.push({len, count - c});
-                    if(len + 1 < k)
-                        pq.push({len + 1, c});
-                    c = 0;
-                }
-            }
-        }
-        
-        return pq.empty();
+        return mafreq * k <= n;
     }
 };
