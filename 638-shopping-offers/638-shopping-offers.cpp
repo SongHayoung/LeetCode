5b9a6d70410f22749f61d0d9cacca32ecd48a9b2
SongHayoung <37806785+SongHayoung@users.noreply.github.com>
2022-05-30 17:59:05 +0900

    Time: 27 ms (64.15%), Space: 15.1 MB (54.43%) - LeetHub

--- 638-shopping-offers/638-shopping-offers.cpp
+++ 638-shopping-offers/638-shopping-offers.cpp
@@ -1,13 +1,23 @@
 class Solution {
+    unordered_map<int, int> dp;
+    int makeKey(vector<int>& needs) {
+        int res = 0;
+        for(int i = 0; i < needs.size(); i++) {
+            res += (needs[i]<<(i*4));
+        }
+        return res;
+    }
 public:
-    int shoppingOffers(const vector<int>& price, const vector<vector<int>>& special, const vector<int>& needs) {
+    int shoppingOffers(vector<int>& price, vector<vector<int>>& special, vector<int>& needs) {
+        int key = makeKey(needs);
+        if(dp.count(key)) return dp[key];
         int result = inner_product(price.begin(), price.end(), needs.begin(), 0);
-        for (const vector<int>& offer : special) {
+        for (vector<int>& offer : special) {
             vector<int> r = can(needs, offer);
             if (r.empty()) continue;
             result = min(result, offer.back() + shoppingOffers(price, special, r));
         }
-        return result;
+        return dp[key] = result;
     }
     vector<int> can(const vector<int>& needs, const vector<int>& offer) {
         vector<int> r(needs.size(), 0);
