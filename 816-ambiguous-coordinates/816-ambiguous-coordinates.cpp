cb9832ca7d6fb0fad9a2ff1dba3adef572b4e8b0
SongHayoung <37806785+SongHayoung@users.noreply.github.com>
2022-05-31 11:56:48 +0900

    Time: 21 ms (21.43%), Space: 13.9 MB (5.10%) - LeetHub

--- 816-ambiguous-coordinates/816-ambiguous-coordinates.cpp
+++ 816-ambiguous-coordinates/816-ambiguous-coordinates.cpp
@@ -1,17 +1,21 @@
 class Solution {
     vector<string> dnc(string s) {
-        vector<string> res;
-        deque<char> front, back(begin(s), end(s));
         if(s.back() == '0') {
             if(s.length() > 1 and s[0] == '0') return {};
             return {s};
         }
+        
+        vector<string> res;
+        deque<char> front, back(begin(s), end(s));
+        
         for(int i = 0; i < s.length(); i++) {
             front.push_back(back.front()); back.pop_front();
             if(front[0] == '0' and front.size() > 1) break;
             if(!back.empty() and back.back() == '0') continue;
+            
             string f(begin(front), end(front));
             string b(begin(back), end(back));
+            
             if(b.length() == 0) res.push_back(f);
             else res.push_back(f + "." + b);
         }
