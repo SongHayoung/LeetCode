f98bd31f6772567aedfe0765bb414c9098e830c3
SongHayoung <37806785+SongHayoung@users.noreply.github.com>
2022-05-31 11:56:28 +0900

    Time: 23 ms (16.33%), Space: 16.1 MB (5.10%) - LeetHub

--- 816-ambiguous-coordinates/816-ambiguous-coordinates.cpp
+++ 816-ambiguous-coordinates/816-ambiguous-coordinates.cpp
@@ -2,12 +2,12 @@ class Solution {
     vector<string> dnc(string s) {
         vector<string> res;
         deque<char> front, back(begin(s), end(s));
-        unordered_map<char, int> mp;
-        for(auto& ch : s) mp[ch]++;
-        
+        if(s.back() == '0') {
+            if(s.length() > 1 and s[0] == '0') return {};
+            return {s};
+        }
         for(int i = 0; i < s.length(); i++) {
             front.push_back(back.front()); back.pop_front();
-            mp[front.back()]--;
             if(front[0] == '0' and front.size() > 1) break;
             if(!back.empty() and back.back() == '0') continue;
             string f(begin(front), end(front));
