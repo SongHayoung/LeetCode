c51197dc634d0abb29da8fb0c9cd4289627ebb2b
SongHayoung <37806785+SongHayoung@users.noreply.github.com>
2022-05-31 16:59:39 +0900

    Time: 96 ms (73.78%), Space: 22.4 MB (74.08%) - LeetHub

--- 854-k-similar-strings/854-k-similar-strings.cpp
+++ 854-k-similar-strings/854-k-similar-strings.cpp
@@ -1,37 +1,27 @@
 class Solution {
-    vector<string> helper(string& a, string& o) {
-        int i = 0, n = a.length();
-        for(; i < n and o[i] == a[i]; i++) {};
-        vector<string> res;
-        for(int j = i + 1; j < n; j++) {
-            if(a[j] == o[i]) {
-                swap(a[j], a[i]);
-                res.push_back(a);
-                swap(a[j], a[i]);
-            }
-        }
-        return res;
-    }
 public:
-    int kSimilarity(string s1, string s2) {
-        if(s1 == s2) return 0;
-        queue<string> q;
-        q.push(s1);
-        int res = 1;
-        unordered_set<string> vis{s1};
-        while(!q.empty()) {
-            int sz = q.size();
-            while(sz--) {
-                auto s = q.front(); q.pop();
-                for(auto& nxt : helper(s, s2)) {
-                    if(vis.count(nxt)) continue;
-                    if(nxt == s2) return res;
-                    q.push(nxt);
-                    vis.insert(nxt);
+    int kSimilarity(string A, string B) {
+        for(int i = 0; i < A.length(); i++) {
+            if(A[i] == B[i]) continue;
+            vector<int> match;
+            int res = INT_MAX;
+            for(int j = i + 1; j < A.length(); j++) {
+                if(B[i] != A[j]) continue;
+                match.push_back(j);
+                if(B[j] == A[i]) {
+                    swap(A[i], A[j]);
+                    return 1 + kSimilarity(A.substr(i + 1), B.substr(i + 1));
                 }
             }
-            res++;
+            
+            for(auto& j : match) {
+                swap(A[i], A[j]);
+                res = min(res, 1 + kSimilarity(A.substr(i + 1), B.substr(i + 1)));
+                swap(A[i], A[j]);
+            }
+            
+            return res;
         }
-        return -1;
+        return 0;
     }
 };
