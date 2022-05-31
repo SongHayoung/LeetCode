215de018dce36efbb34c73bf999ccda5440c29aa
SongHayoung <37806785+SongHayoung@users.noreply.github.com>
2022-05-31 15:50:46 +0900

    Time: 50 ms (66.31%), Space: 12.6 MB (97.31%) - LeetHub

--- 838-push-dominoes/838-push-dominoes.cpp
+++ 838-push-dominoes/838-push-dominoes.cpp
@@ -2,12 +2,7 @@ class Solution {
 public:
     string pushDominoes(string s) {
         int l = 0, r = s.length() - 1;
-        while(l <= r and s[l] != 'R') {
-            if(s[l] == 'L') {
-                for(int i = l - 1; i >= 0 and s[i] == '.'; i--) s[i] = 'L';
-            }
-            l++;
-        }
+
         while(l <= r and s[r] != 'L') {
             if(s[r] == 'R') {
                 for(int i = r + 1; r < s.length() and s[i] == '.'; i++) s[i] = 'R';
@@ -16,14 +11,17 @@ public:
         }
         while(l <= r) {
             while(l <= r and s[l] != 'R') {
-            if(s[l] == 'L') {
-                for(int i = l - 1; i >= 0 and s[i] == '.'; i--) s[i] = 'L';
+                if(s[l] == 'L') {
+                    for(int i = l - 1; i >= 0 and s[i] == '.'; i--) s[i] = 'L';
+                }
+                l++;
             }
-            l++;
-        }
+            
             if(l >= r) break;
+            
             int lr = l + 1;
             while(lr < r and s[lr] == '.') lr++;
+            
             if(s[lr] == 'R') {
                 for(int i = l; i <= lr; i++) {
                     s[i] = 'R';
