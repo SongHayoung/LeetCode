3e36f5e7e249182e5cfaca4d049b79d5c240318b
SongHayoung <37806785+SongHayoung@users.noreply.github.com>
2022-06-10 10:31:42 +0900

    Time: 11 ms (69.06%), Space: 11.8 MB (65.02%) - LeetHub

--- 89-gray-code/89-gray-code.cpp
+++ 89-gray-code/89-gray-code.cpp
@@ -1,27 +1,13 @@
 class Solution {
-    bool helper(vector<int>& res, int& n, unordered_set<int>& us) {
-        if(res.size() == 1<<n) return true;
-        else {
-            int last = res.back();
-            for(int i = 0; i < n; i++) {
-                bool bit = (last & (1<<i));
-                int next = last + (bit ? -(1<<i) : (1<<i));
-                if(us.count(next)) continue;
-                res.push_back(next);
-                us.insert(next);
-                if(helper(res, n, us)) return true;
-                res.pop_back();
-                us.erase(next);
-            }
-        }
-        return false;
-    }
 public:
     vector<int> grayCode(int n) {
-        int mask = (1<<n) - 1;
-        unordered_set<int> us{0};
         vector<int> res{0};
-        helper(res, n, us);
+        for(int i = 0; i < n; i++) {
+            int sz = res.size();
+            for(int j = sz - 1; j >= 0; j--) {
+                res.push_back(res[j] | 1<<i);
+            }
+        }
         return res;
     }
 };
