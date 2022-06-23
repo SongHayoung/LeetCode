1504ca68a3b01095741eb029160cb5e2894a6c19
SongHayoung <37806785+SongHayoung@users.noreply.github.com>
2022-06-23 12:13:11 +0900

    Time: 167 ms (44.62%), Space: 46.6 MB (18.73%) - LeetHub

---
+++ 2075-decode-the-slanted-ciphertext/2075-decode-the-slanted-ciphertext.cpp
@@ -0,0 +1,34 @@
+class Solution {
+    string parse(vector<vector<char>>& A, int p) {
+        int i = 0, j = p;
+        int n = A.size(), m = A[0].size();
+        string res = "";
+        while(i < n and j < m) {
+            res.push_back(A[i++][j++]);
+        }
+        return res;
+    }
+    bool valid(string& s) {
+        for(auto& ch : s)
+            if(ch != ' ') return true;
+        return false;
+    }
+public:
+    string decodeCiphertext(string s, int rows) {
+        if(rows == 1) return s;
+        int n = s.length();
+        vector<vector<char>> A(rows, vector<char>(n / rows));
+        for(int i = 0, k = 0; i < rows; i++) {
+            for(int j = 0; j < n / rows; j++) {
+                A[i][j] = s[k++];
+            }
+        }
+        string res = "";
+        for(int i = 0; i < n / rows; i++) {
+            string token = parse(A, i);
+            res += token;
+        }
+        while(!res.empty() and res.back() == ' ') res.pop_back();
+        return res;
+    }
+};
