cc3ef8d6185db4c3d81c1c944c99a0b832820251
SongHayoung <37806785+SongHayoung@users.noreply.github.com>
2022-06-30 11:28:20 +0900

    Time: 55 ms (27.37%), Space: 12.4 MB (70.57%) - LeetHub

--- 1541-minimum-insertions-to-balance-a-parentheses-string/1541-minimum-insertions-to-balance-a-parentheses-string.cpp
+++ 1541-minimum-insertions-to-balance-a-parentheses-string/1541-minimum-insertions-to-balance-a-parentheses-string.cpp
@@ -2,26 +2,20 @@ class Solution {
 public:
     int minInsertions(string s) {
         int po = 0, o = 0, c = 0, n = s.length(), i = 0;
-        while(i < n) {
+        for(int i = 0; i < n; i++) {
             if(s[i] == '(') po++;
             else {
                 if(i + 1 < n and s[i + 1] == ')') {
-                    if(po) {
-                        po--;
-                    } else o++;
+                    if(po) po--;
+                    else o++;
+                    
                     i += 1;
                 } else {
-                    if(po) {
-                        po--;
-                        c++;
-                    } else {
-                        c++,o++;
-                    }
+                    if(po) po--, c++;
+                    else c++, o++;
                 }
             }
-            i++;
         }
-        
         
         return po * 2 + o + c;
     }
