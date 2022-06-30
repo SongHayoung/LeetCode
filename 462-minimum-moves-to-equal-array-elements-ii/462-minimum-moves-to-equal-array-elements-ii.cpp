597d2aa98244d93d3a037f8b458a89e01e66c4d9
SongHayoung <37806785+SongHayoung@users.noreply.github.com>
2022-06-30 10:30:09 +0900

    Time: 29 ms (5.90%), Space: 11 MB (15.75%) - LeetHub

--- 462-minimum-moves-to-equal-array-elements-ii/462-minimum-moves-to-equal-array-elements-ii.cpp
+++ 462-minimum-moves-to-equal-array-elements-ii/462-minimum-moves-to-equal-array-elements-ii.cpp
@@ -8,15 +8,11 @@ class Solution {
     }
 public:
     int minMoves2(vector<int>& A) {
-        long long res = LLONG_MAX, ma = *max_element(begin(A), end(A)), mi = *min_element(begin(A), end(A));
-        while(mi <= ma) {
-            int m = mi + (ma - mi) / 2;
-            long long diff = helper(A, m);
-            long long diff2 = helper(A, m + 1);
-            if(diff < diff2) ma = m - 1;
-            else mi = m + 1;
-            res = min({res, diff, diff2});
-        }
+        sort(begin(A), end(A));
+        int n = A.size(), m = A[n/2];
+        int res = 0;
+        for(auto& a : A)
+            res += abs(m-a);
         return res;
     }
 };
