586af81d2256bd71f4c71b4cbd3983733b69d295
SongHayoung <37806785+SongHayoung@users.noreply.github.com>
2022-07-07 11:05:11 +0900

    Time: 1 ms (79.26%), Space: 6.2 MB (81.68%) - LeetHub

--- 1638-count-substrings-that-differ-by-one-character/1638-count-substrings-that-differ-by-one-character.cpp
+++ 1638-count-substrings-that-differ-by-one-character/1638-count-substrings-that-differ-by-one-character.cpp
@@ -1,19 +1,24 @@
 class Solution {
-    bool valid(string& s, int p1, string& t, int p2, int len) {
-        int diff = 0;
-        while(len-- and diff < 2) {
-            if(s[p1++] != t[p2++]) diff++;
-        }
-        return diff == 1;
-    }
 public:
     int countSubstrings(string s, string t) {
         int n = s.length(), m = t.length(), res = 0;
-        for(int len = 1; len <= min(n,m); len++) {
-            for(int i = 0; i <= n - len; i++) {
-                for(int j = 0; j <= m - len; j++) {
-                    if(valid(s,i,t,j,len)) res += 1;
-                }
+        int ldp[101][101] = {}, rdp[101][101] = {};
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < m; j++) {
+                if(s[i] == t[j])
+                    ldp[i+1][j+1] = ldp[i][j] + 1;
+            }
+        }
+        for(int i = n - 1; i >= 0; i--) {
+            for(int j = m - 1; j >= 0; j--) {
+                if(s[i] == t[j])
+                    rdp[i][j] = rdp[i+1][j+1] + 1;
+            }
+        }
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < m; j++) {
+                if(s[i] != t[j])
+                    res += (ldp[i][j] + 1) * (rdp[i+1][j+1] + 1);
             }
         }
         return res;
