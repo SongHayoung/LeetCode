6617cc2c943ac7ae44798337d563626606164217
SongHayoung <37806785+SongHayoung@users.noreply.github.com>
2022-07-08 13:04:53 +0900

    Time: 3 ms (79.83%), Space: 9.1 MB (42.02%) - LeetHub

--- 932-beautiful-array/932-beautiful-array.cpp
+++ 932-beautiful-array/932-beautiful-array.cpp
@@ -1,12 +1,14 @@
 class Solution {
+    unordered_map<int, vector<int>> dp;
 public:
     vector<int> beautifulArray(int n) {
         if(n == 1) return {1};
+        if(dp.count(n)) return dp[n];
         auto odd = beautifulArray((n + 1) / 2);
         auto even = beautifulArray(n/2);
         vector<int> res;
         for(auto& o : odd) res.push_back(o * 2 - 1);
         for(auto& e : even) res.push_back(e * 2);
-        return res;
+        return dp[n] = res;
     }
 };
