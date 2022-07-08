5f4c312c789dcd6e4f466835479d9bc3a620a45f
SongHayoung <37806785+SongHayoung@users.noreply.github.com>
2022-07-08 12:25:31 +0900

    Time: 406 ms (59.42%), Space: 85.9 MB (69.44%) - LeetHub

--- 2033-minimum-operations-to-make-a-uni-value-grid/2033-minimum-operations-to-make-a-uni-value-grid.cpp
+++ 2033-minimum-operations-to-make-a-uni-value-grid/2033-minimum-operations-to-make-a-uni-value-grid.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int minOperations(vector<vector<int>>& A, int x) {
         vector<int> B;
-        int res = 0, n = A.size(), m = A[0].size();
+        int res = 0;
         for(auto& row : A) for(auto& col : row) B.push_back(col);
         sort(begin(B), end(B));
         int median = B[B.size() / 2];
