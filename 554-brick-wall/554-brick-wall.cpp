e37d57d7e3f54d187ae35747520efbe6564a74a8
SongHayoung <37806785+SongHayoung@users.noreply.github.com>
2022-07-13 09:18:47 +0900

    Time: 45 ms (96.12%), Space: 21.7 MB (41.99%) - LeetHub

--- 554-brick-wall/554-brick-wall.cpp
+++ 554-brick-wall/554-brick-wall.cpp
@@ -1,39 +1,12 @@
 class Solution {
 public:
-    int leastBricks(vector<vector<int>>& A) {
-        priority_queue<pair<int, int>,vector<pair<int, int>>,greater<pair<int, int>>> q;
-        priority_queue<int, vector<int>, greater<int>> fin;
-        int res = A.size(), inc = 0, now = 0, sum = 0;
-        for(auto& row : A) {
-            int psum = 0;
-            for(auto& col : row) {
-                q.push({psum,psum+col});
-                psum += col;
+    int leastBricks(vector<vector<int>>& wall) {
+        unordered_map<int, int> freq;
+        int n = wall.size(), res = n;
+        for(auto& row : wall) {
+            for(int i = 0, psum = 0; i < row.size() - 1; i ++) {
+                res = min(res, n - ++freq[psum += row[i]]);
             }
-            sum = psum;
-        }
-        while(!q.empty()) {
-            int mi = q.top().first;
-            now = mi;
-            if(!fin.empty()) mi = min(mi, fin.top());
-            while(!fin.empty() and fin.top() == mi) {
-                inc--;
-                fin.pop();
-            }
-            if(now != 0 and now != sum) res = min(res, inc);
-            while(!q.empty() and q.top().first == mi) {
-                fin.push(q.top().second); q.pop();
-                inc++;
-            }
-        }
-        while(!fin.empty()) {
-            int f = fin.top();
-            now = f;
-            while(!fin.empty() and fin.top() == f) {
-                inc--;
-                fin.pop();
-            }
-            if(now != 0 and now != sum) res = min(res,inc);
         }
         return res;
     }
