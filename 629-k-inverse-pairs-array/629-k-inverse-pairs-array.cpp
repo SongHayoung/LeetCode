8ab8d81da4a1fcdd3a8b308489f37a16f91579d8
SongHayoung <37806785+SongHayoung@users.noreply.github.com>
2022-07-17 20:47:42 +0900

    Time: 81 ms (39.74%), Space: 14.9 MB (51.92%) - LeetHub

--- 629-k-inverse-pairs-array/629-k-inverse-pairs-array.cpp
+++ 629-k-inverse-pairs-array/629-k-inverse-pairs-array.cpp
@@ -1,14 +1,14 @@
 class Solution {
-    long long dp[1010][1010];
+    long long dp[1010][1010], mod = 1e9 + 7;
 public:
     int kInversePairs(int n, int k) {
         dp[0][0] = 1;
-        for (int N = 1; N <= n; ++N)
-            for (int K = 0; K <= k; ++K) {
-                dp[N][K] = (dp[N - 1][K] + (K > 0 ? dp[N][K - 1] : 0)) % 1000000007;
-                if (K >= N)
-                    dp[N][K] = (1000000007 + dp[N][K] - dp[N - 1][K - N]) % 1000000007;
+        for(int i = 1; i <= n; i++) {
+            for(int j = 0; j <= k; j++) {
+                dp[i][j] = (dp[i-1][j] + (j ? dp[i][j-1] : 0)) % mod;
+                if(j >= i) dp[i][j] = (mod + dp[i][j] - dp[i-1][j-i]) % mod;
             }
+        }
         return dp[n][k];
     }
 };
