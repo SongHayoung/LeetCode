abdbda07956810d0338b802a7de11091d381a8a0
SongHayoung <37806785+SongHayoung@users.noreply.github.com>
2022-07-20 12:16:57 +0900

    Time: 194 ms (78.85%), Space: 58.1 MB (53.21%) - LeetHub

--- 1391-check-if-there-is-a-valid-path-in-a-grid/1391-check-if-there-is-a-valid-path-in-a-grid.cpp
+++ 1391-check-if-there-is-a-valid-path-in-a-grid/1391-check-if-there-is-a-valid-path-in-a-grid.cpp
@@ -6,13 +6,15 @@ class Solution {
         if(street == 4) return dir == 1 or dir == 2;
         if(street == 5) return dir == 0 or dir == 3;
         if(street == 6) return dir == 0 or dir == 1;
+        
         return false;
     }
-    bool connected(int street1, int dir, int street2) {
-        if(dir == 1) return street2 == 1 or street2 == 3 or street2 == 5;
-        if(dir == 3) return street2 == 1 or street2 == 4 or street2 == 6;
+    bool connected(int dir, int street2) {
         if(dir == 0) return street2 == 2 or street2 == 3 or street2 == 4;
+        if(dir == 1) return street2 == 1 or street2 == 3 or street2 == 5;
         if(dir == 2) return street2 == 2 or street2 == 5 or street2 == 6;
+        if(dir == 3) return street2 == 1 or street2 == 4 or street2 == 6;
+        
         return false;
     }
 public:
@@ -28,7 +30,7 @@ public:
             for(int dir = 0; dir < 4; dir++) {
                 if(!validDir(A[y][x], dir)) continue;
                 int ny = y + dy[dir],  nx = x + dx[dir];
-                if(0 <= ny and ny < n and 0 <= nx and nx < m and !vis[ny][nx] and connected(A[y][x], dir , A[ny][nx])) {
+                if(0 <= ny and ny < n and 0 <= nx and nx < m and !vis[ny][nx] and connected(dir , A[ny][nx])) {
                     q.push({ny,nx});
                     vis[ny][nx] = true;
                 }
