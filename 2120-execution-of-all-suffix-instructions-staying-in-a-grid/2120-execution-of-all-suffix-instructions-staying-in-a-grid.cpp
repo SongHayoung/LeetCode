8df96375884dcbdf31085309f898bc6614d3aacf
SongHayoung <37806785+SongHayoung@users.noreply.github.com>
2022-08-04 10:22:11 +0900

    Time: 14 ms (99.17%), Space: 11.6 MB (17.17%) - LeetHub

--- 2120-execution-of-all-suffix-instructions-staying-in-a-grid/2120-execution-of-all-suffix-instructions-staying-in-a-grid.cpp
+++ 2120-execution-of-all-suffix-instructions-staying-in-a-grid/2120-execution-of-all-suffix-instructions-staying-in-a-grid.cpp
@@ -1,33 +1,17 @@
 class Solution {
 public:
-    vector<int> executeInstructions(int n, vector<int>& startPos, string s) {
-        int y = startPos[0], x = startPos[1];
-        vector<pair<int, int>> pos;
-        for(auto& ch : s) {
-            if(ch == 'U') y--;
-            else if(ch == 'D') y++;
-            else if(ch == 'L') x--;
-            else x++;
-            pos.push_back({y,x});
-        }
-        vector<int> res;
-        for(int i = 0; i < pos.size(); i++) {
-            for(int j = i; j <= pos.size(); j++) {
-                if(j == pos.size()) {
-                    res.push_back(j - i);
-                    break;
-                }
-                auto [yy, xx] = pos[j];
-                if(0 <= yy and yy < n and 0 <= xx and xx < n) continue;
-                res.push_back(j - i);
-                break;
-            }
-            for(int j = i; j < pos.size(); j++) {
-                if(s[i] == 'U') pos[j].first++;
-                else if(s[i] == 'D') pos[j].first--;
-                else if(s[i] == 'L') pos[j].second++;
-                else pos[j].second--;
-            }
+    vector<int> executeInstructions(int n, vector<int>& p, string s) {
+        int m = s.length(), y = m + n, x = m + n;
+        vector<int> Y(y * 2, m), X(x * 2, m);
+        vector<int> res(m);
+        for(int i = m - 1; i >= 0; i--) {
+            Y[y] = X[x] = i;
+            if(s[i] == 'U') y++;
+            else if(s[i] == 'D') y--;
+            else if(s[i] == 'L') x++;
+            else x--;
+            
+            res[i] = min({m, Y[y - p[0] - 1], Y[y - p[0] + n], X[x - p[1] - 1], X[x - p[1] + n]}) - i;
         }
         return res;
     }
