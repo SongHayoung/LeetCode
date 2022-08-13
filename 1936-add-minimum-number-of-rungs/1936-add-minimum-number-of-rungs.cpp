7ede3b880ae0873e19e8c7ffcb03b54d798517a0
SongHayoung <37806785+SongHayoung@users.noreply.github.com>
2022-08-13 12:23:41 +0900

    Time: 102 ms (99.79%), Space: 54.3 MB (26.51%) - LeetHub

--- 1936-add-minimum-number-of-rungs/1936-add-minimum-number-of-rungs.cpp
+++ 1936-add-minimum-number-of-rungs/1936-add-minimum-number-of-rungs.cpp
@@ -3,9 +3,7 @@ public:
     int addRungs(vector<int>& rungs, int dist) {
         int res = 0, now = 0;
         for(auto& r : rungs) {
-            if(r - now > dist) {
-                res += (r - now - 1) / dist;
-            }
+            if(r - now > dist) res += (r - now - 1) / dist;
             now = r;
         }
         return res;
