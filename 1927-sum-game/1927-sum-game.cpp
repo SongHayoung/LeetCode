6d798d1bb405aa626d15e924c3087d040767b05f
SongHayoung <37806785+SongHayoung@users.noreply.github.com>
2022-08-13 12:38:01 +0900

    Time: 36 ms (51.65%), Space: 11.4 MB (50.00%) - LeetHub

---
+++ 1927-sum-game/1927-sum-game.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool sumGame(string s) {
+        int q = 0, sum = 0, n = s.length();
+        for(int i = 0; i < n / 2; i++) {
+            if(s[i] == '?') q++;
+            else sum += s[i] - '0';
+        }
+        for(int i = n / 2; i < n; i++) {
+            if(s[i] == '?') q--;
+            else sum -= s[i] - '0';
+        }
+        
+        return q & 1 or sum + q * 9 / 2 != 0;
+    }
+};
