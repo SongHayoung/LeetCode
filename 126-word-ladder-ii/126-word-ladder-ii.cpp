71e8916ab2d078d01ca766bddbfaa2f57e296e08
SongHayoung <37806785+SongHayoung@users.noreply.github.com>
2022-08-14 11:05:25 +0900

    Time: 93 ms (14.15%), Space: 10.4 MB (51.71%) - LeetHub

--- 126-word-ladder-ii/126-word-ladder-ii.cpp
+++ 126-word-ladder-ii/126-word-ladder-ii.cpp
@@ -1,68 +1,94 @@
 class Solution {
-    unordered_map<string, vector<string>> g;
-    unordered_set<string> c;
-    int len = INT_MAX;
-    vector<vector<string>> res;
-    bool similar(string& s1, string& s2) { //o(k)
-        int diff = 0;
-        for(int i = 0; i < s1.length() and diff < 2; i++) {
-            if(s1[i] != s2[i]) diff++;
+public:
+    vector<vector<string>> findLadders(string beginWord, string endWord, vector<string>& wordList) {
+        
+        wordList.insert(wordList.begin(), beginWord);
+        for (int i = 1; i < wordList.size(); i++){
+            if (wordList[i].compare(wordList[0]) == 0){
+                wordList[i] = wordList.back();
+                wordList.pop_back();
+                break;
+            }
         }
-        return diff == 1;
-    }
-    
-    void buildG(vector<string>& w, string& begin) { //o(n^2)
-        for(int i = 0; i < w.size(); i++) {
-            for(int j = i + 1; j < w.size(); j++) {
-                if(similar(w[i], w[j])) {
-                    g[w[i]].push_back(w[j]);
-                    g[w[j]].push_back(w[i]);
+        unordered_map<string, int> word_to_index;
+        for (int i = 0; i < wordList.size(); i++){
+            word_to_index.insert({wordList[i], i});
+        }
+        if (word_to_index.count(endWord) == 0) return {};
+        
+        vector<vector<int>> edges(word_to_index.size());
+        for (int i = 0; i < wordList.size(); i++){
+            for (int j = 0; j < wordList.size(); j++){
+                if (i == j) continue;
+                if (differ_by_one(wordList[i], wordList[j])){
+                    edges[i].push_back(j);
                 }
             }
         }
-        if(!g.count(begin)) {
-            for(int i = 0; i < w.size(); i++) {
-                if(similar(begin,w[i])) 
-                    g[begin].push_back(w[i]);
-            }
-        }
-    }
-    
-    void bfs(string& st, string& t) {
-        queue<vector<string>> q;
-        unordered_set<string> v{st};
-        q.push({st});
-        while(res.empty()) {
-            int sz = q.size();
-            unordered_set<string> lv;
-            while(sz--) {
-                auto vec = q.front(); q.pop();
-                for(auto nxt : g[vec.back()]) {
-                    if(v.count(nxt)) continue;
-                    if(nxt == t) {
-                        vec.push_back(nxt);
-                        res.push_back(vec);
-                        break;
-                    } else {
-                        lv.insert(nxt);
-                        vec.push_back(nxt);
-                        q.push(vec);
-                        vec.pop_back();
+        // BFS search, lets start from node=0, 
+        int start_node = 0, target_node = word_to_index[endWord];
+        vector<int> marked(wordList.size(), INT_MAX);   marked[start_node] = 0;
+        queue<int> units;   units.push(start_node);
+        int r = 0;
+        int min_step = INT_MAX;
+        while(!units.empty()){
+            int round_size = units.size();
+            for (int i = 0; i < round_size; i++){
+                int node = units.front();  units.pop();
+                if (node == target_node){
+                    min_step = min(min_step, r);
+                }
+                for (int j = 0; j < edges[node].size(); j++){
+                    int update_node = edges[node][j];
+                    if (r+1 < marked[update_node]){
+                        marked[update_node] = r+1;
+                        units.push(update_node);
                     }
                 }
             }
-            v.insert(lv.begin(),lv.end());
+            r++;
         }
+        // printf("min_step = %d\n", min_step);
+        if (min_step == INT_MAX){
+            return {};
+        }
+        queue<vector<string>> seqs; seqs.push({wordList[target_node]});
+        r = min_step;
+        while(r > 0){
+            int round_size = seqs.size();
+            for (int i = 0; i < round_size; i++){
+                vector<string> seq = seqs.front();  seqs.pop();
+                string back = seq.back();
+                int node = word_to_index[back];
+                for (int j = 0; j < edges[node].size(); j++){
+                    int update_node = edges[node][j];
+                    if (marked[update_node] == r - 1){
+                        seq.push_back(wordList[update_node]);
+                        seqs.push(seq);
+                        seq.pop_back();
+                    }
+                }
+            }
+            r--;
+        }
+        vector<vector<string>> result;
+        while(!seqs.empty()){
+            vector<string> seq = seqs.front(); seqs.pop();
+            reverse(seq.begin(), seq.end());
+            result.push_back(seq);
+        }
+        return result;
     }
-    
-public:
-    vector<vector<string>> findLadders(string beginWord, string endWord, vector<string>& wordList) {
-        buildG(wordList, beginWord);
-        if(!g.count(endWord)) return res;
-        vector<string> tmp{beginWord};
-        c.insert(beginWord);
-        bfs(beginWord, endWord);
+    bool differ_by_one(string a, string b){
         
-        return res;
+        int differ = 0;
+        for (int i = 0; i < a.size(); i++){
+            if (a[i] != b[i]){
+                differ += 1;
+                if (differ > 1) return false;
+            }
+        }
+        return differ == 1;
     }
 };
+
