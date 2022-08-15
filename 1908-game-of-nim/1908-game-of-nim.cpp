9c1b2f29541d6561109d600e0b47e7290c243f03
SongHayoung <37806785+SongHayoung@users.noreply.github.com>
2022-08-15 11:59:17 +0900

    Time: 692 ms (31.82%), Space: 30.2 MB (36.36%) - LeetHub

---
+++ 1908-game-of-nim/1908-game-of-nim.cpp
@@ -0,0 +1,23 @@
+class Solution {
+    unordered_map<int, bool> dp;
+
+    bool helper(int now) {
+        if(dp.count(now)) return dp[now];
+        cout<<now<<endl;
+        bool& res = dp[now] = false;
+        for(int i = 1; i <= now; i *= 10) {
+            int cur = (now / i) % 10;
+            for(int j = 1; j <= cur; j++) {
+                if(!helper(now - j * i)) return res = true;
+            }
+        }
+        return res;
+    }
+public:
+    bool nimGame(vector<int>& piles) {
+        dp[0] = false;
+        int now = 0;
+        for(int i = 0; i < piles.size(); i++) now = now * 10 + piles[i];
+        return helper(now);
+    }
+};
