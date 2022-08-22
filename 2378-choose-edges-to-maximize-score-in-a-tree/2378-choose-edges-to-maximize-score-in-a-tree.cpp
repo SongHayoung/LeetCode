af54c0979a7bcacf8dafb130894134d59e6c9005
SongHayoung <37806785+SongHayoung@users.noreply.github.com>
2022-08-22 12:25:04 +0900

    Time: 590 ms (100.00%), Space: 171 MB (100.00%) - LeetHub

---
+++ 2378-choose-edges-to-maximize-score-in-a-tree/2378-choose-edges-to-maximize-score-in-a-tree.cpp
@@ -0,0 +1,23 @@
+class Solution {
+    vector<vector<pair<long long, long long>>> adj;
+    pair<long long, long long> dfs(int u) {
+        long long inc = 0, exc = 0;
+        for(auto& [v,w] : adj[u]) {
+            auto [vinc, vexc] = dfs(v);
+            inc = max(inc, w + vexc - vinc);
+            exc += max(vinc, vexc);
+        }
+        return {inc + exc, exc};
+    }
+public:
+    long long maxScore(vector<vector<int>>& A) {
+        int n = A.size();
+        adj = vector<vector<pair<long long, long long>>>(n);
+        for(int i = 1; i < n; i++) {
+            int par = A[i][0], w = A[i][1];
+            adj[par].push_back({i,max(0,w)});
+        }
+        auto [inc, exc] = dfs(0);
+        return max(inc,exc);
+    }
+};
