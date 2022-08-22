e64fb601ceeaa1ba9f32b99f67184df9bd6f2ca5
SongHayoung <37806785+SongHayoung@users.noreply.github.com>
2022-08-22 13:13:41 +0900

    Time: 7 ms (12.29%), Space: 5.8 MB (99.44%) - LeetHub

--- 1432-max-difference-you-can-get-from-changing-an-integer/1432-max-difference-you-can-get-from-changing-an-integer.cpp
+++ 1432-max-difference-you-can-get-from-changing-an-integer/1432-max-difference-you-can-get-from-changing-an-integer.cpp
@@ -2,19 +2,19 @@ class Solution {
 public:
     int maxDiff(int num) {
         int ma = num, mi = num;
-                string u = to_string(num);
-                for(char f = '0'; f <= '9'; f++) {
-                    for(char t = '0'; t <= '9'; t++) {
-                        if(u[0] == f and t == '0') continue;
-                        if(f == t) continue;
-                        string now = u;
-                        for(int i = 0; i < now.length(); i++) {
-                            if(now[i] == f) now[i] = t;
-                        }
-                        ma = max(ma, stoi(now));
-                        mi = min(mi, stoi(now));
-                    }
+        string u = to_string(num);
+        for(char f = '0'; f <= '9'; f++) {
+            for(char t = '0'; t <= '9'; t++) {
+                if(u[0] == f and t == '0') continue;
+                if(f == t) continue;
+                string now = u;
+                for(int i = 0; i < now.length(); i++) {
+                    if(now[i] == f) now[i] = t;
                 }
+                ma = max(ma, stoi(now));
+                mi = min(mi, stoi(now));
+            }
+        }
         
         return ma - mi;
     }
