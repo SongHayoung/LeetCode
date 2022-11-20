5f06115176db1d3fe285a973eaa118442b6ec260
SongHayoung <37806785+SongHayoung@users.noreply.github.com>
2022-11-20 13:18:30 +0900

    Time: 2812 ms (7.14%), Space: 14.9 MB (71.43%) - LeetHub

--- 2478-number-of-beautiful-partitions/2478-number-of-beautiful-partitions.cpp
+++ 2478-number-of-beautiful-partitions/2478-number-of-beautiful-partitions.cpp
@@ -1,26 +1,35 @@
+#include <bits/stdc++.h>
+
 long long dp[1010][1010];
 class Solution {
     bool prime(char ch) {
         return ch == '2' or ch == '3' or ch == '5' or ch == '7';
     }
     long long mod = 1e9 + 7;
-    long long helper(string& s, int k, int& l, int p) {
-        if(k == 0) return p == s.length();
-        if(p + l * k > s.length() or !prime(s[p])) return 0;
+    vector<int> np;
+    long long helper(string& s, int p, int k, int& len) {
+        if(p >= s.length() or k < 0) return 0;
+        if(k == 0) return prime(s.front()) and s.size() - p >= len;
         long long& res = dp[p][k];
         if(res != -1) return res;
-        res = 0;
-        for(int i = p + l - 1; i < s.length(); i++) {
-            if(!prime(s[i])) res = (res + helper(s,k-1,l,i+1)) % mod;
+        dp[p][k] = 0;
+        if(!prime(s.front())) return 0;
+        auto lb = lower_bound(begin(np), end(np), p + len - 1) - begin(np);
+        for(int i = np.size() - 1; i >= 0 and np[i] >= p + len - 1; i--) {
+            res = (res + helper(s, np[i] + 1, k - 1, len)) % mod;
         }
-
         return res;
     }
 public:
     int beautifulPartitions(string s, int k, int minLength) {
         if(!prime(s.front()) or prime(s.back())) return 0;
         int n = s.length();
-        memset(dp,-1, sizeof dp);
-        return helper(s,k,minLength,0);
+        np.clear();
+        for(int i = 0; i < n - 1; i++) {
+            if(!prime(s[i]) and prime(s[i + 1])) np.push_back(i);
+        }
+        np.push_back(n-1);
+        memset(dp,-1,sizeof dp);
+        return helper(s,0, k-1,minLength);
     }
 };
