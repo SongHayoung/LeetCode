6e867b12f52220cd56ec287357e8ba7c34284020
SongHayoung <37806785+SongHayoung@users.noreply.github.com>
2022-11-25 10:26:17 +0900

    Time: 205 ms (61.76%), Space: 41.6 MB (74.64%) - LeetHub

---
+++ 0907-sum-of-subarray-minimums/0907-sum-of-subarray-minimums.cpp
@@ -0,0 +1,23 @@
+class Solution {
+private:
+    int getJump(vector<int>& arr, vector<int>& jump, int cur) {
+        int start = cur + 1;
+        while(start < arr.size() && arr[cur] <= arr[start]) {
+            start += jump[start];
+        }
+        return start - cur;
+    }
+
+public:
+    int sumSubarrayMins(vector<int>& arr) {
+        vector<int> jump(arr.size() + 1, 0);
+        vector<int> memo(arr.size() + 1, 0);
+        int res = 0;
+        int mod = 1e9 + 7;
+        for(int i = arr.size() - 1; i >= 0; --i) {
+            jump[i] = getJump(arr, jump, i);
+            res = (res + (memo[i] = (arr[i] * jump[i] + memo[i + jump[i]]) % mod)) % mod;
+        }
+        return res;
+    }
+};
