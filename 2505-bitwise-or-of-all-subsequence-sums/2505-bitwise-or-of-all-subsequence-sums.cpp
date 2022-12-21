1d9bea80291bb7cfe708321dca58c30cd087eeab
SongHayoung <37806785+SongHayoung@users.noreply.github.com>
2022-12-21 14:24:10 +0900

    Time: 159 ms (36.19%), Space: 52.2 MB (59.05%) - LeetHub

--- 2505-bitwise-or-of-all-subsequence-sums/2505-bitwise-or-of-all-subsequence-sums.cpp
+++ 2505-bitwise-or-of-all-subsequence-sums/2505-bitwise-or-of-all-subsequence-sums.cpp
@@ -1,10 +1,16 @@
 class Solution {
 public:
     long long subsequenceSumOr(vector<int>& nums) {
-        long long res = 0, psum = 0;
+        long long bit[64] = {};
         for(auto n : nums) {
-            psum += n;
-            res |= n | psum;
+            for(long long b = 0; b < 32; b++) {
+                if(n & (1ll<<b)) bit[b] += 1;
+            }
+        }
+        long long res = 0;
+        for(int i = 0; i < 64; i++) {
+            if(i) bit[i] += bit[i-1] / 2;
+            if(bit[i]) res |= (1ll<<i);
         }
         return res;
     }
