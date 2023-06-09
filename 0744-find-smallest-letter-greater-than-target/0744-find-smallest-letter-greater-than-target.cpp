863d8fe5ccc03000fd1ee6a03f61253ea273fb83
SongHayoung <37806785+SongHayoung@users.noreply.github.com>
2023-06-09 10:19:40 +0900

    Time: 22 ms (19.50%), Space: 15.7 MB (96.78%) - LeetHub

---
+++ 0744-find-smallest-letter-greater-than-target/0744-find-smallest-letter-greater-than-target.cpp
@@ -0,0 +1,8 @@
+class Solution {
+public:
+    char nextGreatestLetter(vector<char>& letters, char target) {
+        auto it = upper_bound(begin(letters), end(letters), target);
+        if(it == end(letters)) return letters[0];
+        return *it;
+    }
+};
