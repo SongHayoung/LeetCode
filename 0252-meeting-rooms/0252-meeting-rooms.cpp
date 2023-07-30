4fd0e6b5ff79fad0b3b213314d2e3c357a6e2771
SongHayoung <37806785+SongHayoung@users.noreply.github.com>
2023-07-30 14:09:52 +0900

    Time: 18 ms (73.12%), Space: 11.5 MB (99.26%) - LeetHub

---
+++ 0252-meeting-rooms/0252-meeting-rooms.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    bool canAttendMeetings(vector<vector<int>>& intervals) {
+        if(intervals.size() <= 1) return true;
+        sort(begin(intervals), end(intervals));
+        for(int i = 0; i < intervals.size() - 1; i++) {
+            if(intervals[i][1] > intervals[i+1][0]) return false;
+        }
+        return true;
+    }
+};
