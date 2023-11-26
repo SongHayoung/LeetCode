dbb5ba89d34fb4fcbbe8ae458b3fc75022c6906f
SongHayoung <37806785+SongHayoung@users.noreply.github.com>
2023-11-26 13:33:41 +0900

    Time: 676 ms (33.33%), Space: 356.9 MB (16.67%) - LeetHub

---
+++ 2948-make-lexicographically-smallest-array-by-swapping-elements/2948-make-lexicographically-smallest-array-by-swapping-elements.cpp
@@ -0,0 +1,38 @@
+class Solution {
+    int uf[101010];
+    int find(int u) {
+        return uf[u] == u ? u : uf[u] = find(uf[u]);
+    }
+    void uni(int u, int v) {
+        int pu = find(u), pv = find(v);
+        uf[pu] = uf[pv] = min(pu,pv);
+    }
+public:
+    vector<int> lexicographicallySmallestArray(vector<int>& A, int limit) {
+        vector<int> s = A;
+        sort(begin(s), end(s));
+        s.erase(unique(begin(s), end(s)), end(s));
+        unordered_map<int, int> ref;
+        for(int i = 0; i < s.size(); i++) {
+            ref[s[i]] = i;
+        }
+        for(int i = 0; i < s.size(); i++) uf[i] = i;
+        for(int i = 0; i < s.size() - 1; i++) {
+            if(s[i+1] - s[i] <= limit) uni(i,i+1);
+        }
+        unordered_map<int, deque<int>> gr;
+        for(int i = 0; i < A.size(); i++) {
+            int f = find(ref[A[i]]);
+            gr[f].push_back(A[i]);
+        }
+        for(auto& [k,v] : gr) sort(begin(v), end(v));
+        vector<int> res(A.size());
+        for(int i = 0; i < A.size(); i ++) {
+            int f = find(ref[A[i]]);
+            res[i] = gr[f].front();
+            gr[f].pop_front();
+        }
+        return res;
+    }
+};
+
