ee7a596806c139887b0339a2adf4a2ef50c4ccbb
SongHayoung <37806785+SongHayoung@users.noreply.github.com>
2023-11-30 23:19:59 +0900

    Time: 70 ms (9.93%), Space: 28.3 MB (10.49%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.cpp
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -1,18 +1,20 @@
 class Solution {
-    string sort(string str) {
-        std::sort(begin(str), end(str));
-        return str;
-    }
+	string keyOf(string& str) {
+		vector<int> frequency(26);
+		for(auto& ch : str) frequency[ch-'a'] += 1;
+		string key = "";
+		for(int i = 0; i < 26; i++) key += to_string(frequency[i]) + "#";
+		return key;
+	}
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
-        unordered_map<string, vector<string>> m;
-        vector<vector<string>> res;
-        for(auto& str : strs) {
-            m[sort(str)].push_back(str);
-        }
-        for(auto& entity : m) {
-            res.push_back(entity.second);
-        }
-        return res;
+	    unordered_map<string, vector<string>> hashTable;
+	    for(auto& str : strs) {
+		    string hashKey = keyOf(str);
+		    hashTable[hashKey].push_back(str);
+	    }        
+	    vector<vector<string>> res;
+	    for(auto& [_,values] : hashTable) res.push_back(values);
+	    return res;
     }
 };
