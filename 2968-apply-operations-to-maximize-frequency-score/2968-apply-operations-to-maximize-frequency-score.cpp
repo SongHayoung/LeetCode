fecf0f11b7eebf4d565bd875cce39166d11152c8
SongHayoung <37806785+SongHayoung@users.noreply.github.com>
2023-12-17 14:16:13 +0900

    Time: 153 ms (88.89%), Space: 85.5 MB (44.44%) - LeetHub

---
+++ 2968-apply-operations-to-maximize-frequency-score/2968-apply-operations-to-maximize-frequency-score.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int maxFrequencyScore(vector<int>& A, long long k) {
+        int i = 0, n = A.size();
+        sort(A.begin(), A.end());
+        for (int j = 0; j < n; ++j) {
+            if ((k -= A[j] - A[(i + j) / 2]) < 0)
+                k += A[(i + j + 1) / 2] - A[i++];
+        }
+
+        return n - i;
+    }
+};
