65078b04d3f366732003cbbe08e1d802f639e219
SongHayoung <37806785+SongHayoung@users.noreply.github.com>
2023-12-24 01:03:38 +0900

    Time: 6 ms (100.00%), Space: 19.1 MB (66.67%) - LeetHub

---
+++ 10031-count-the-number-of-incremovable-subarrays-i/10031-count-the-number-of-incremovable-subarrays-i.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int incremovableSubarrayCount(vector<int>& A) {
+        A.insert(begin(A),-1);
+        A.push_back(1e9 + 10);
+        long long res = 0, le = 0, ri = A.size() - 1, n = A.size();
+        for(int i = 0; i < n - 1; i++) {
+            if(A[i] < A[i+1]) le = i+1;
+            else break;
+            if(i == n - 2) return (n - 2) * (n - 1) / 2;
+        }
+        while(ri - 1 > le and A[ri-1] > A[le] and (ri < n ? A[ri-1] < A[ri] : true)) ri--;
+        while(le) {
+            res += (n - ri);
+            le -= 1;
+            if(le >= 0) {
+                while(ri - 1 > le and A[ri-1] > A[le] and (ri < n ? A[ri-1] < A[ri] : true)) ri--;
+            }
+        }
+        res += (n - ri);
+        return res;
+    }
+};
