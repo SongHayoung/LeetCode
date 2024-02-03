0629b847785954f57e2c26329f58287bbfb8ad06
SongHayoung <37806785+SongHayoung@users.noreply.github.com>
2024-02-03 10:37:02 +0900

    Time: 28 ms (57.46%), Space: 38.5 MB (8.32%) - LeetHub

--- 0485-max-consecutive-ones/0485-max-consecutive-ones.cpp
+++ 0485-max-consecutive-ones/0485-max-consecutive-ones.cpp
@@ -2,9 +2,7 @@ class Solution {
 public:
     int findMaxConsecutiveOnes(vector<int>& nums) {
         int res = 0, now = 0;
-        for(auto& x : nums) {
-            res = max(res, now = x ? now + 1 : 0);
-        }
+        for(auto& x : nums) res = max(res, now = x ? now + 1 : 0);
         return res;
     }
 };
