7eb10e52564a6939faed82a459741e750855dad1
SongHayoung <37806785+SongHayoung@users.noreply.github.com>
2024-02-09 17:41:49 +0900

    Time: 18 ms (14.22%), Space: 21 MB (9.05%) - LeetHub

--- 0914-x-of-a-kind-in-a-deck-of-cards/0914-x-of-a-kind-in-a-deck-of-cards.cpp
+++ 0914-x-of-a-kind-in-a-deck-of-cards/0914-x-of-a-kind-in-a-deck-of-cards.cpp
@@ -2,18 +2,13 @@ class Solution {
 public:
     bool hasGroupsSizeX(vector<int>& deck) {
         unordered_map<int, int> freq;
-        unordered_set<int> us;
+        int g = deck.size();
         for(int i = 0; i < deck.size(); i++) {
             freq[deck[i]]++;
-            if(deck.size() % (i + 1) == 0 and i) us.insert(i+1);
         }
         for(auto& [k,v] : freq) {
-            unordered_set<int> uus;
-            for(auto x : us) {
-                if(v % x == 0) uus.insert(x);
-            }
-            swap(us,uus);
+            g = __gcd(g,v);
         }
-        return us.size();
+        return g != 1;
     }
 };
