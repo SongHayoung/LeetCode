4587bbb2c7b273831597040364ecdc00810591f8
SongHayoung <37806785+SongHayoung@users.noreply.github.com>
2024-04-02 23:28:11 +0900

    Time: 171 ms (12.50%), Space: 298.6 MB (7.81%) - LeetHub

---
+++ 0644-maximum-average-subarray-ii/0644-maximum-average-subarray-ii.cpp
@@ -0,0 +1,28 @@
+class Solution {
+    bool helper(vector<int>& A, int k, double avg) {
+        vector<double> dp;
+        for(int i = 0; i < A.size(); i++) {
+            dp.push_back(A[i] - i * avg);
+        }
+        double ma = -1e9;
+        for(int i = dp.size() - k - 1, j = dp.size() - 1; i >= 0; i--,j--) {
+            ma = max(ma, dp[j]);
+            if(ma >= dp[i]) return true;
+        }
+        return false;
+    }
+public:
+    double findMaxAverage(vector<int>& nums, int k) {
+        double l = -1e4, r = 1e4, EPS = 1e-9;
+        nums.insert(begin(nums),0);
+        for(int i = 1; i < nums.size(); i++) nums[i] += nums[i-1];
+        while(r - l >= EPS) {
+            double m = l + (r - l) / 2;
+            bool ok = helper(nums, k, m);
+            if(ok) {
+                l = m;
+            } else r = m;
+        }
+        return r;
+    }
+};
