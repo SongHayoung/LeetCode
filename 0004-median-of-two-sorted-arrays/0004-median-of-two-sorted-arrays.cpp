e5bce316827c1dc1c01ff510b7203db97e5c9a1c
SongHayoung <37806785+SongHayoung@users.noreply.github.com>
2024-04-02 21:13:26 +0900

    Time: 13 ms (96.13%), Space: 94.3 MB (91.28%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
@@ -1,21 +1,22 @@
 class Solution {
 public:
-    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-        int n = nums1.size(), m = nums2.size();
-        if(n > m) return findMedianSortedArrays(nums2, nums1);
-        int l = 0, r = n, mid = (n + m) / 2;
+    double findMedianSortedArrays(vector<int>& A, vector<int>& B) {
+        int n = A.size(), m = B.size();
+        if(n > m) return findMedianSortedArrays(B,A);
+        int tot = n + m, l = 0, r = n;
         while(l <= r) {
-            int i = l + (r - l) / 2;
-            int j = mid - i;
-            if(i > 0 and nums1[i-1] > nums2[j]) {
-                r = i - 1;
-            } else if(i < n and nums1[i] < nums2[j-1]) {
-                l = i + 1;
-            } else {
-                int right = (i == n) ? nums2[j] : (j == m) ? nums1[i] : min(nums1[i], nums2[j]);
-                if((n + m) & 1) return right;
-                int left = (i == 0) ? nums2[j - 1] : (j == 0) ? nums1[i - 1] : max(nums1[i - 1], nums2[j - 1]);
-                return (left + right) / 2.0;
+            int m1 = l + (r - l) / 2;
+            int m2 = tot / 2 - m1;
+            if(m1 and A[m1-1] > B[m2]) r = m1 - 1;
+            else if(m1 < n and A[m1] < B[m2-1]) l = m1 + 1;
+            else {
+                int x = INT_MAX, y = INT_MIN;
+                if(m1 != n) x = min(x, A[m1]);
+                if(m2 != m) x = min(x, B[m2]);
+                if(tot & 1) return x;
+                if(m1) y = max(y, A[m1-1]);
+                if(m2) y = max(y, B[m2-1]);
+                return (x + y) / 2.;
             }
         }
         return -1;
