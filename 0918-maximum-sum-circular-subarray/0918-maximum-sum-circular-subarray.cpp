97ba094e5aabfbbd70e50614dfd9440a4406eb8f
SongHayoung <37806785+SongHayoung@users.noreply.github.com>
2024-04-02 15:56:27 +0900

    Time: 77 ms (5.14%), Space: 46.7 MB (5.14%) - LeetHub

--- 0918-maximum-sum-circular-subarray/0918-maximum-sum-circular-subarray.cpp
+++ 0918-maximum-sum-circular-subarray/0918-maximum-sum-circular-subarray.cpp
@@ -1,15 +1,19 @@
 class Solution {
 public:
-    int maxSubarraySumCircular(vector<int>& nums) {
-        int curMax = 0, maxSum = INT_MIN, curMin = 0, minSum = INT_MAX, tot = 0;
-        for(auto& n : nums) {
-            curMax = max(curMax + n, n);
-            maxSum = max(maxSum, curMax);
-            curMin = min(curMin + n, n);
-            minSum = min(minSum, curMin);
-            tot += n;
+    int maxSubarraySumCircular(vector<int>& A) {
+        deque<pair<int, int>> dq;
+        int res = INT_MIN;
+        dq.push_back({-1,0});
+        for(int i = 0, pre = 0; i < A.size() * 2; i++) {
+            pre += A[i%A.size()];
+            while(dq.size() and dq.back().second >= pre) {
+                res = max(res, pre - dq.back().second);
+                dq.pop_back();
+            }
+            if(dq.size() and i - dq.front().first == A.size() + 1) dq.pop_front();
+            if(dq.size()) res = max(res, pre - dq.front().second);
+            dq.push_back({i,pre});
         }
-
-        return tot == minSum ? maxSum : max(maxSum, tot - minSum);
+        return res;
     }
 };
