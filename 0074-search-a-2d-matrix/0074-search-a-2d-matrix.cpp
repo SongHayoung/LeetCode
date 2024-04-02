a17e0f8af6ba99b464c6fc12d8ae504cf3f98f0f
SongHayoung <37806785+SongHayoung@users.noreply.github.com>
2024-04-02 16:20:24 +0900

    Time: 0 ms (100.00%), Space: 11.9 MB (49.88%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
@@ -1,20 +1,29 @@
 class Solution {
+    bool ok(vector<int>& A, int target) {
+        int l = 0, r = A.size() - 1;
+        while(l <= r) {
+            int m = l + (r - l) / 2;
+            if(A[m] == target) return true;
+            else if(A[m] < target) l = m + 1;
+            else r = m - 1;
+        }
+        return false; 
+    }
+    bool row(vector<vector<int>>& matrix, int target) {
+        int l = 0, r = matrix.size() - 1, res = 0;
+        while(l <= r) {
+            int m = l + (r - l) / 2;
+            if(matrix[m].front() <= target and target <= matrix[m].back()) {
+                cout<<m<<endl;
+                return ok(matrix[m], target);
+            } else if(matrix[m].back() < target) {
+                l = m + 1;
+            } else r = m - 1;
+        }
+        return res;
+    }
 public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
-        int n = matrix.size(), l = 0, r = n - 1;
-        while(l < r) {
-	int m = l + (r-l)/2;
-	if(matrix[m][0] == target) return true;
-	else if(matrix[m][0] > target) r = m - 1;
-    else if(matrix[m].back() < target) {
-        l = m+1;
-    }
-	else if(l == m) break;
-    else l = m;
-	}
-        auto it = lower_bound(matrix[l].begin(), matrix[l].end(), target);
-	return it != matrix[l].end() and *it == target;
+        return row(matrix, target);
     }
 };
-
-
