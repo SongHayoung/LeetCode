a92fe21c09fc5a50175a39feb4c64c3d94e84253
SongHayoung <37806785+SongHayoung@users.noreply.github.com>
2024-04-02 16:47:37 +0900

    Time: 281 ms (15.28%), Space: 182.6 MB (6.05%) - LeetHub

--- 0373-find-k-pairs-with-smallest-sums/0373-find-k-pairs-with-smallest-sums.cpp
+++ 0373-find-k-pairs-with-smallest-sums/0373-find-k-pairs-with-smallest-sums.cpp
@@ -1,26 +1,27 @@
 class Solution {
 public:
     vector<vector<int>> kSmallestPairs(vector<int>& nums1, vector<int>& nums2, int k) {
-        map<int, vector<vector<int>>> m;
-        int cnt(0);
-        for(auto& n1 : nums1) {
-            for(auto& n2 : nums2) {
-                int sum = n1 + n2;
-                if(cnt < k) {
-                    m[sum].push_back({n1, n2});
-                    cnt++;
-                } else if(prev(m.end())->first > sum) {
-                    m[sum].push_back({n1, n2});
-                    prev(m.end())->second.pop_back();
-                    if(prev(m.end())->second.empty()) {
-                        m.erase(prev(m.end()));
+        map<int, vector<vector<int>>> mp;
+        int cnt = 0;
+        for(auto& x : nums1) {
+            for(auto& y : nums2) {
+                int sum = x + y;
+                if(cnt == k and prev(end(mp))->first <= sum) break;
+                cnt++;
+                mp[sum].push_back({x,y});
+                if(cnt > k) {
+                    prev(end(mp))->second.pop_back();
+                    if(prev(end(mp))->second.size() == 0) {
+                        mp.erase(prev(end(mp)));
                     }
-                } else break;
+                    cnt--;
+                }
             }
+            if(cnt == k and prev(end(mp))->first <= x + nums2[0]) break;
         }
         vector<vector<int>> res;
-        for(auto e : m) {
-            res.insert(res.end(), e.second.begin(), e.second.end());
+        for(auto& [_, vec] : mp) {
+            for(auto& v : vec) res.push_back(v);
         }
         return res;
     }
