0ade264a1aefe6702974088eb6f227708121a908
SongHayoung <37806785+SongHayoung@users.noreply.github.com>
2024-04-03 20:25:06 +0900

    Time: 97 ms (77.24%), Space: 74.2 MB (94.39%) - LeetHub

--- 2863-maximum-length-of-semi-decreasing-subarrays/2863-maximum-length-of-semi-decreasing-subarrays.cpp
+++ 2863-maximum-length-of-semi-decreasing-subarrays/2863-maximum-length-of-semi-decreasing-subarrays.cpp
@@ -1,38 +1,17 @@
-struct Seg{
-    int mi, ma, val;
-    Seg *left, *right;
-    Seg(vector<int>& A, int l, int r) : mi(A[l]), ma(A[r]), val(-1), left(nullptr), right(nullptr) {
-        if(l ^ r) {
-            int m = l + (r - l) / 2;
-            left = new Seg(A,l,m);
-            right = new Seg(A,m+1,r);
-        }
-    }
-    int query(int n) {
-        if(ma < n) return val;
-        if(mi >= n) return -1;
-        return max(left->query(n), right->query(n));
-    }
-    void update(int n, int k) {
-        if(mi <= n and n <= ma) {
-            val = max(val, k);
-            if(left) left->update(n,k);
-            if(right) right->update(n,k);
-        }
-    }
-};
 class Solution {
 public:
-    int maxSubarrayLength(vector<int>& nums) {
-        vector<int> S = nums;
-        sort(begin(S), end(S));
-        S.erase(unique(begin(S), end(S)), end(S));
-        Seg* seg = new Seg(S,0,S.size()-1);
-        int res = 0;
-        for(int i = nums.size() - 1; i >= 0; i--) {
-            int p = seg->query(nums[i]);
-            if(p != -1) res = max(res, p - i + 1);
-            seg->update(nums[i], i);
+    int maxSubarrayLength(vector<int>& A) {
+        vector<int> st;
+        int n = A.size(), res = 0;
+        for(int i = n - 1; i >= 0; i--) {
+            if(st.empty() or A[st.back()] > A[i]) st.push_back(i);
+        }
+        for(int i = 0, mi = INT_MIN; i < n and st.size(); i++) {
+            while(st.size() and st.back() <= i) st.pop_back();
+            while(st.size() and A[i] > A[st.back()]) {
+               res = max(res, st.back() - i + 1);
+                st.pop_back();
+            }
         }
         return res;
     }
