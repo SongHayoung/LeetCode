c6ac23b31393c6bb06d7ad3704cf5e9a0c772326
SongHayoung <37806785+SongHayoung@users.noreply.github.com>
2024-04-04 11:03:22 +0900

    Time: 8 ms (58.85%), Space: 10.9 MB (52.00%) - LeetHub

--- 1043-partition-array-for-maximum-sum/1043-partition-array-for-maximum-sum.cpp
+++ 1043-partition-array-for-maximum-sum/1043-partition-array-for-maximum-sum.cpp
@@ -1,18 +1,14 @@
 class Solution {
-    int dp[505];
-    int helper(vector<int>& A, int k, int p) {
-        if(p == A.size()) return 0;
-        if(dp[p] != -1) return dp[p];
-        int n = A.size();
-        for(int i = p, ma = 0; i < min(n, p + k); i++) {
-            ma = max(ma, A[i]);
-            dp[p] = max(dp[p], ma * (i - p + 1) + helper(A, k, i + 1));
-        }
-        return dp[p];
-    }
 public:
     int maxSumAfterPartitioning(vector<int>& A, int k) {
-        memset(dp, -1, sizeof dp);
-        return helper(A,k,0);
+        vector<int> dp(A.size() + 1);
+        for(int i = A.size() - 1; i >= 0; i--) {
+            int ma = 0;
+            for(int j = i; j < i + k and j < A.size(); j++) {
+                ma = max(ma, A[j]);
+                dp[i] = max(dp[i], ma * (j - i + 1) + dp[j+1]);
+            }
+        }
+        return dp[0];
     }
 };
