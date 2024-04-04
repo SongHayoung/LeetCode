e97ad831752e211095fe5a7e78e51e7a865f6f84
SongHayoung <37806785+SongHayoung@users.noreply.github.com>
2024-04-04 11:09:28 +0900

    Time: 19 ms (8.09%), Space: 10.9 MB (52.00%) - LeetHub

--- 1043-partition-array-for-maximum-sum/1043-partition-array-for-maximum-sum.cpp
+++ 1043-partition-array-for-maximum-sum/1043-partition-array-for-maximum-sum.cpp
@@ -1,14 +1,16 @@
 class Solution {
 public:
     int maxSumAfterPartitioning(vector<int>& A, int k) {
-        vector<int> dp(A.size() + 1);
+        deque<int> dq;
         for(int i = A.size() - 1; i >= 0; i--) {
+            dq.push_front(A[i]);
+            if(dq.size() > k) dq.pop_back();
             int ma = 0;
-            for(int j = i; j < i + k and j < A.size(); j++) {
-                ma = max(ma, A[j]);
-                dp[i] = max(dp[i], ma * (j - i + 1) + dp[j+1]);
+            for(int j = 0; j < dq.size(); j++) {
+                ma = max(ma, dq[j]);
+                A[i] = max(A[i], ma * (j + 1) + (i + j + 1 < A.size() ? A[i+j+1] : 0));
             }
         }
-        return dp[0];
+        return A[0];
     }
 };
