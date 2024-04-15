60d3884349a7b7d0a81420e358528fc43417029a
SongHayoung <37806785+SongHayoung@users.noreply.github.com>
2024-04-16 08:14:10 +0900

    Time: 10 ms (64.50%), Space: 18.1 MB (18.61%) - LeetHub

--- 2896-apply-operations-to-make-two-strings-equal/2896-apply-operations-to-make-two-strings-equal.cpp
+++ 2896-apply-operations-to-make-two-strings-equal/2896-apply-operations-to-make-two-strings-equal.cpp
@@ -1,25 +1,25 @@
 class Solution {
 public:
     int minOperations(string s1, string s2, int x) {
-        vector<int> p;
+        vector<int> pos;
         for(int i = 0; i < s1.length(); i++) {
-            if(s1[i] != s2[i]) p.push_back(i);
+            if(s1[i] != s2[i]) pos.push_back(i);
         }
-        if(p.size() & 1) return -1;
-        vector<vector<long long>> dp(p.size() + 2, vector<long long>(p.size() + 2, 1e9));
+        if(pos.size() & 1) return -1;
+        vector<vector<long long>> dp(pos.size() + 1, vector<long long>(pos.size() + 2, 1e9));
         dp[0][0] = 0;
-        for(int i = 0; i < p.size(); i++) {
-            for(int j = 0; j <= p.size(); j++) {
+        for(int i = 0; i < pos.size(); i++) {
+            for(int j = 0; j <= pos.size(); j++) {
                 if(j == 0) {
-                    dp[i+1][1] = min(dp[i+1][1], dp[i][j]);
+                    dp[i+1][j+1] = min(dp[i+1][j+1], dp[i][j]);
                 } else {
                     dp[i+1][j-1] = min(dp[i+1][j-1], dp[i][j] + x);
                 }
-                if(i + 1 < p.size()) {
-                    dp[i+2][j] = min(dp[i+2][j], dp[i][j] + min(p[i+1] - p[i], x));
+                if(i + 1 < pos.size()) {
+                    dp[i+2][j] = min(dp[i+2][j], dp[i][j] + min(x, pos[i+1] - pos[i]));
                 }
             }
         }
-        return dp[p.size()][0];
+        return dp[pos.size()][0];
     }
 };
