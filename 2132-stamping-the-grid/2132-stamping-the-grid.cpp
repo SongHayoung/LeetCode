f857897cd59a1e66e7fd1b18088f0a389b28a496
SongHayoung <37806785+SongHayoung@users.noreply.github.com>
2024-04-17 23:18:32 +0900

    Time: 448 ms (5.77%), Space: 227.7 MB (6.73%) - LeetHub

--- 2132-stamping-the-grid/2132-stamping-the-grid.cpp
+++ 2132-stamping-the-grid/2132-stamping-the-grid.cpp
@@ -1,61 +1,59 @@
 class Solution {
-    bool judge(vector<vector<int>>& grid) {
-        int sum = 0;
-        for(auto& r : grid)
-            sum += accumulate(begin(r),end(r),0);
-        return sum == grid.size() * grid[0].size();
+    vector<vector<int>> pre;
+    int query(int y1, int x1, int y2, int x2) {
+        if(y2 + 1 >= pre.size()) return 1;
+        if(x2 + 1 >= pre[0].size()) return 1;
+        return pre[y2+1][x2+1] - pre[y2+1][x1] - pre[y1][x2+1] + pre[y1][x1];
+    }
+    vector<vector<int>> masking(vector<vector<int>>& grid, int stampHeight, int stampWidth) {
+        int n = grid.size(), m = grid[0].size();
+        pre = vector<vector<int>>(n + 1, vector<int>(m + 1));
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < m; j++) {
+                pre[i+1][j+1] = grid[i][j] + pre[i+1][j] + pre[i][j+1] - pre[i][j];
+            }
+        }
+        vector<vector<int>> mark(n, vector<int>(m));
+        for(int i = 0; i < n - stampHeight + 1; i++) {
+            for(int j = 0; j < m - stampWidth + 1; j++) {
+                if(query(i,j,i+stampHeight-1,j+stampWidth-1) == 0) mark[i][j] = 1;
+            }
+        }
+        vector<int> cnt(m);
+        queue<pair<int, int>> q;
+        vector<vector<int>> res(n, vector<int>(m));
+        for(int i = 0; i < n; i++) {
+            vector<int> pre(m+1);
+            while(q.size() and q.front().first == i) {
+                cnt[q.front().second]--;
+                q.pop();
+            }
+            for(int j = 0; j < m; j++) {
+                if(mark[i][j]) {
+                    q.push({i+stampHeight, j});
+                    cnt[j]++;
+                }
+                if(cnt[j]) {
+                    pre[j] += cnt[j];
+                    pre[j+stampWidth] -= cnt[j];
+                }
+            }
+            for(int j = 0; j < m; j++) {
+                if(j) pre[j] += pre[j-1];
+                if(pre[j]) res[i][j] = 1;
+                if(grid[i][j]) res[i][j] = 1;
+            }
+        }
+        return res;
     }
 public:
-    bool possibleToStamp(vector<vector<int>>& grid, int h, int w) {
+    bool possibleToStamp(vector<vector<int>>& grid, int stampHeight, int stampWidth) {
         int n = grid.size(), m = grid[0].size();
-        if(h > n or w > m) return judge(grid);
-        vector<int> st(m, 0);
-        vector<vector<bool>> mark(n, vector<bool>(m));
-        
-        for(int i = 0; i < h - 1; i++) { //pre calculate with dp technique
-            for(int j = 0; j < m; j++) {
-                if(!grid[i][j]) st[j] += 1;
-                else st[j] = 0;
-            }
+        auto res1 = masking(grid,stampHeight,stampWidth);
+        for(int i = 0; i < n; i++) for(int j = 0; j < m; j++) {
+            if(res1[i][j]) continue;
+            return false;
         }
-        
-        for(int i = h - 1; i < n; i++) {
-            for(int j = 0; j < m; j++) {// dp technique
-                if(!grid[i][j]) st[j] += 1;
-                else st[j] = 0;
-            }
-            queue<int> q;
-            for(int j = 0; j < w - 1; j++) { //sliding window technique
-                if(st[j] < h) q.push(j);
-            }
-            for(int l = 0, r = w - 1; r < m; l++,r++) { //marking top left of stamp
-                while(!q.empty() and q.front() < l) q.pop();
-                if(st[r] < h) q.push(r);
-                if(q.empty()) mark[i-h+1][l] = true;
-            }
-        }
-        
-        vector<int> row(m + 1);
-        for(int i = 0; i < n; i++) {
-            for(int j = 0; j < m; j++) { //diff array technique
-                if(mark[i][j]) {
-                    row[j]++;
-                    row[j + w]--;
-                }
-                if(i >= h and mark[i-h][j]) {
-                    row[j]--;
-                    row[j + w]++;
-                }
-            }
-            int sum = 0;
-            for(int j = 0; j < m; j++) { //prefix sum technique
-                sum += row[j];
-                if(!sum and !grid[i][j]) {
-                    return false;
-                }
-            }
-        }
-        
         return true;
     }
 };
