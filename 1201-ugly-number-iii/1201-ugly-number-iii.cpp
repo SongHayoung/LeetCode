1aa8dd055a40ecb98001cd186ba38f1699ef81c4
SongHayoung <37806785+SongHayoung@users.noreply.github.com>
2024-04-17 21:19:33 +0900

    Time: 0 ms (100.00%), Space: 7.1 MB (81.36%) - LeetHub

--- 1201-ugly-number-iii/1201-ugly-number-iii.cpp
+++ 1201-ugly-number-iii/1201-ugly-number-iii.cpp
@@ -1,66 +1,24 @@
+constexpr auto lcm(auto x, auto... xs)
+{
+    return ((x = std::lcm(x, xs)), ...);
+}
 class Solution {
-private:
-    long long gcd(int p, int q) {
-        if(q == 0) {
-            return p;
-        }
-        return gcd(q, p % q);
-    }
-
-    long long lcm(long long p, long long q) {
-        return p * q / gcd(p, q);
-    }
-
-    template<typename T>
-    void removeMultiplies(set<T> &sets) {
-        vector<T> vec(sets.begin(), sets.end());
-
-        for(int i = 0; i < vec.size(); i++) {
-            for(int j = i + 1; j < vec.size(); j++) {
-                if(!(max(vec[i], vec[j]) % min(vec[i], vec[j])))
-                    sets.erase(max(vec[i], vec[j]));
-            }
-        }
-    }
-    
-    long sequence(long long m, set<int> nums, set<long long> lcms) {
-        long ret = 0;
-        for(int num : nums) {
-            ret += m / num;
-        }
-
-        for(long lcm : lcms) {
-            ret -= m / lcm;
-        }
-
-        return ret;
+    bool helper(long long n, long long a, long long b, long long c, long long m) {
+        long long ab = lcm(a,b), ac = lcm(a,c), bc = lcm(b,c), abc = lcm(a,b,c);
+        long long cnt = m / a + m / b + m / c - m / ab - m / ac - m / bc + m / abc;
+        return cnt >= n;
     }
 public:
     int nthUglyNumber(int n, int a, int b, int c) {
-        priority_queue<pair<long long, long long>> pq;
-        long long l = 0, r = n * min(a, min(b,c)), m;
-        set<int> nums{a,b,c};
-        removeMultiplies(nums);
-        set<long long> lcms;
-        for(auto it = nums.begin(); it != nums.end(); it++) {
-            for(auto it2 = std::next(it, 1); it2 != nums.end(); it2++) {
-                lcms.insert(lcm(max(*it, *it2), min(*it, *it2)));
-            }
+        int l = 0, r = INT_MAX, res = INT_MAX;
+        while(l <= r) {
+            int m = l + (r - l) / 2;
+            bool ok = helper(n,a,b,c,m);
+            if(ok) {
+                r = m - 1;
+                res = m;
+            } else l = m + 1;
         }
-
-        removeMultiplies(lcms);
-
-
-        while(l + 1 < r) {
-            m = (l + r)>>1;
-            if(sequence(m, nums, lcms) >= n)
-                r = m;
-            else
-                l = m;
-        }
-
-        while(sequence(l, nums, lcms) != n)
-            l++;
-        return l;
+        return res;
     }
 };
