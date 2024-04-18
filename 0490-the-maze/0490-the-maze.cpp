80e3e985aaeaa38e18f36f48488ce22141ab6670
SongHayoung <37806785+SongHayoung@users.noreply.github.com>
2024-04-18 11:07:25 +0900

    Time: 30 ms (58.51%), Space: 22 MB (81.77%) - LeetHub

--- 0490-the-maze/0490-the-maze.cpp
+++ 0490-the-maze/0490-the-maze.cpp
@@ -1,31 +1,28 @@
 class Solution {
-
 public:
     bool hasPath(vector<vector<int>>& maze, vector<int>& start, vector<int>& destination) {
-        if(start == destination) return true;
-
+        queue<pair<int, int>> q;
         int n = maze.size(), m = maze[0].size();
-        int dx[4] = {0,1,0,-1}, dy[4] = {-1,0,1,0};
-        maze[start[0]][start[1]] = -1;
-        queue<vector<int>> q;
-        q.push(start);
-        while(!q.empty()) {
-            auto pos = q.front(); q.pop();
-            int y = pos[0], x = pos[1];
+        vector<vector<bool>> vis(n, vector<bool>(m));
+        int dy[4]{-1,0,1,0}, dx[4]{0,1,0,-1};
+        auto push = [&](int y, int x) {
+            if(!vis[y][x]) {
+                q.push({y,x});
+                vis[y][x] = true;
+            }
+        };
+        push(start[0], start[1]);
+        while(q.size()) {
+            auto [y,x] = q.front(); q.pop();
             for(int i = 0; i < 4; i++) {
                 int ny = y, nx = x;
-
-                while(0 <= ny + dy[i] and ny + dy[i] < n and 0 <= nx + dx[i] and nx + dx[i] < m and maze[ny + dy[i]][nx + dx[i]] <= 0) { //move straight
-                    ny += dy[i];
-                    nx += dx[i];
+                while(0 <= ny + dy[i] and ny + dy[i] < n and 0 <= nx + dx[i] and nx + dx[i] < m and maze[ny+dy[i]][nx+dx[i]] == 0) {
+                    ny = ny + dy[i];
+                    nx = nx + dx[i];
                 }
-                if(!maze[ny][nx]) {
-                    maze[ny][nx] = -1;
-                    q.push({ny,nx});
-                    if(destination[0] == ny and destination[1] == nx) return true;
-                }
+                push(ny,nx);
             }
         }
-        return false;
+        return vis[destination[0]][destination[1]];
     }
 };
