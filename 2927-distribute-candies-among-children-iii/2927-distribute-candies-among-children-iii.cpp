746ad6f593e30a72fc4e8f36595d9a44dda32dfb
SongHayoung <37806785+SongHayoung@users.noreply.github.com>
2024-04-18 21:42:39 +0900

    Time: 4 ms (16.67%), Space: 8.9 MB (33.33%) - LeetHub

--- 2927-distribute-candies-among-children-iii/2927-distribute-candies-among-children-iii.cpp
+++ 2927-distribute-candies-among-children-iii/2927-distribute-candies-among-children-iii.cpp
@@ -1,24 +1,10 @@
 class Solution {
+    long long count(long long a) {
+        return a < 0 ? 0 : (a + 2) * (a + 1) / 2;
+    }
 public:
     long long distributeCandies(int n, int limit) {
         if(limit * 3 < n) return 0;
-        long long res = 0, hold = min(limit, n);
-        for(long long i = max(0, n - 2 * limit); i <= hold; i++) {
-            long long x = n - i;
-            if(x <= limit) {
-                long long rem = hold - i + 1;
-                res += rem * (2 * x - rem + 1) / 2 + rem;
-                break;
-            }
-            else {
-                long long l = i, r = min(n - limit - 1ll, hold), cnt = r - l + 1;
-                res += 2 * limit * cnt + cnt;
-                res -= cnt * (n - l + n - r) / 2;
-
-
-                i = n - limit - 1;
-            }
-        }
-        return res;
+        return count(n) - 3 * count(n - limit - 1) + 3 * count(n - 2 * limit - 2) - 3 * count(n - 3 * limit - 3);
     }
 };
