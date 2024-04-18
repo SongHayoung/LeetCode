c882d1081200f5b62c239bab0c9b8fd881195133
SongHayoung <37806785+SongHayoung@users.noreply.github.com>
2024-04-18 15:27:09 +0900

    Time: 119 ms (10.43%), Space: 139.6 MB (6.20%) - LeetHub

--- 0134-gas-station/0134-gas-station.cpp
+++ 0134-gas-station/0134-gas-station.cpp
@@ -4,12 +4,11 @@ public:
         vector<pair<int, int>> st;
         int n = gas.size(), now = 0;
         for(int i = 0; i < 2 * n; i++) {
+            while(st.size() and st.back().second > now) st.pop_back();
             st.push_back({i,now});
             int g = gas[i % n], c = cost[i % n];
             now = now + g - c;
-            while(st.size() and st.back().second > now) st.pop_back();
-            int at = (i + 1);
-            if(st.size() and (i + 1) - st.front().first >= n) return (i + 1) % n;
+            if(st.back().first - st.front().first >= n) return i % n;
         }
         return -1;
     }
