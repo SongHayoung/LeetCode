743da7de21822328adda7d87f7bc014602ed28d9
SongHayoung <37806785+SongHayoung@users.noreply.github.com>
2024-04-18 19:49:58 +0900

    Time: 12 ms (90.31%), Space: 13.3 MB (31.59%) - LeetHub

--- 1209-remove-all-adjacent-duplicates-in-string-ii/1209-remove-all-adjacent-duplicates-in-string-ii.cpp
+++ 1209-remove-all-adjacent-duplicates-in-string-ii/1209-remove-all-adjacent-duplicates-in-string-ii.cpp
@@ -1,46 +1,15 @@
 class Solution {
-    int isDuplicate(list<char> &l, int k) {
-        if (l.size() < k) return l.size();
-        auto it = l.begin();
-        int i = k;
-        while (i--) {
-            if (*it != l.front()) return k - i - 1;
-            it++;
-        }
-        return -1;
-    }
-
-    void __pop_front(list<char> &l, int k) {
-        auto to = l.begin(); advance(to, k);
-        l.erase(l.begin(), to);
-    }
-
-    void __push_front(list<char> &from, list<char> &to, int k) {
-        while (k-- && from.size()) {
-            to.push_front(from.back());
-            from.pop_back();
-        }
-    }
-
-    void __push_back(list<char>& from, list<char>& to, int k) {
-        while(k-- && from.size()) {
-            to.push_back(from.front());
-            from.pop_front();
-        }
-    }
-
 public:
     string removeDuplicates(string s, int k) {
-        list<char> res, ss(begin(s), end(s));
-        while (ss.size()) {
-            int dup = isDuplicate(ss, k);
-            if (dup == -1) {
-                __pop_front(ss, k);
-                __push_front(res, ss, k);
-            } else {
-                __push_back(ss, res, dup);
-            }
+        vector<pair<char,int>> st;
+        for(auto& ch : s) {
+            if(st.size() == 0 or st.back().first != ch) {
+                st.push_back({ch,1});
+            } else st.back().second++;
+            if(st.back().second == k) st.pop_back();
         }
-        return string(res.begin(), res.end());
+        string res = "";
+        for(auto& [ch,cnt] : st) res += string(cnt,ch);
+        return res;
     }
 };
