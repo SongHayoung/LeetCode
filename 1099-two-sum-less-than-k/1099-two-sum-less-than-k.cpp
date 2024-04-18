7600483374cb1474eccdc662a8cfe8ae587de6f6
SongHayoung <37806785+SongHayoung@users.noreply.github.com>
2024-04-18 10:54:54 +0900

    Time: 6 ms (31.46%), Space: 11.5 MB (39.44%) - LeetHub

--- 1099-two-sum-less-than-k/1099-two-sum-less-than-k.cpp
+++ 1099-two-sum-less-than-k/1099-two-sum-less-than-k.cpp
@@ -1,12 +1,14 @@
 class Solution {
 public:
-    int twoSumLessThanK(vector<int>& nums, int k) {
-        int res = -1;
-        sort(begin(nums), end(nums));
-        int l = 0, r = nums.size() - 1;
+    int twoSumLessThanK(vector<int>& A, int k) {
+        int l = 0, r = A.size() - 1, res = -1;
+        sort(begin(A), end(A));
         while(l < r) {
-            if(nums[l] + nums[r] < k) res = max(res, nums[l++] + nums[r]);
-            else r--;
+            int sum = A[l] + A[r];
+            if(sum < k) {
+                res = max(res, sum);
+                l++;
+            } else r--;
         }
         return res;
     }
