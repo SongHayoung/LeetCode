f112a812039b68cef7d4ef2e9c12470f57a72327
SongHayoung <37806785+SongHayoung@users.noreply.github.com>
2024-04-18 11:25:50 +0900

    Time: 25 ms (20.16%), Space: 29.5 MB (81.89%) - LeetHub

--- 0665-non-decreasing-array/0665-non-decreasing-array.cpp
+++ 0665-non-decreasing-array/0665-non-decreasing-array.cpp
@@ -1,22 +1,15 @@
 class Solution {
 public:
     bool checkPossibility(vector<int>& nums) {
-        int n = nums.size();
-        vector<bool> suf(nums.size() + 1);
-        suf[n] = suf[n-1] = true;
-        for(int i = n - 2; i >= 0; i--) {
-            if(nums[i] > nums[i+1]) break;
-            suf[i] = true;
-        }
-        bool ok = true;
-        for(int i = 0; i < n and ok; i++) {
-            if(ok and suf[i+1]) {
-                if(i == 0) return true;
-                if(i + 1 == n) return true;
-                if(nums[i-1] <= nums[i+1]) return true;
+        int ok = 0;
+        for(int i = 1; i < nums.size() and ok < 2; i++) {
+            if(nums[i] < nums[i-1]) {
+                ok++;
+                if(i == 1 or nums[i-2] <= nums[i]) {
+                    nums[i-1] = nums[i];
+                } else nums[i] = nums[i-1];
             }
-            if(i and nums[i] < nums[i-1]) ok = false;
         }
-        return false;
+        return ok < 2;
     }
 };
