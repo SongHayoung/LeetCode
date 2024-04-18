fdebae2483e5ed87ac153f301f5f0b7dd7b8f68d
SongHayoung <37806785+SongHayoung@users.noreply.github.com>
2024-04-18 15:14:08 +0900

    Time: 127 ms (5.93%), Space: 139.7 MB (6.20%) - LeetHub

--- 0134-gas-station/0134-gas-station.cpp
+++ 0134-gas-station/0134-gas-station.cpp
@@ -1,17 +1,15 @@
 class Solution {
 public:
     int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
-        int n = gas.size(), tank = 0;
-        vector<pair<int, int>> st;
+        vector<pair<int, int>> st{{-1,0}};
+        int n = gas.size(), now = 0;
         for(int i = 0; i < 2 * n; i++) {
-            if(i < n) {
-                st.push_back({i,tank});
-            }
-            tank = tank + gas[i%n] - cost[i%n];
-            while(st.size() and st.back().second > tank) st.pop_back();
-            if(i >= n) {
-                if(st.size() and st.front().first <= i - n) return st.front().first;
-            }
+            int g = gas[i % n], c = cost[i % n];
+            now = now + g - c;
+            while(st.size() and st.back().second > now) st.pop_back();
+            int at = (i + 1);
+            if(st.size() and i - st.front().first >= n) return (i + 1) % n;
+            st.push_back({i,now});
         }
         return -1;
     }
