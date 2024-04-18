8aaeb104a561faf80ccff856872652ef5d468a29
SongHayoung <37806785+SongHayoung@users.noreply.github.com>
2024-04-18 23:50:21 +0900

    Time: 30 ms (87.25%), Space: 39.6 MB (95.22%) - LeetHub

--- 0825-friends-of-appropriate-ages/0825-friends-of-appropriate-ages.cpp
+++ 0825-friends-of-appropriate-ages/0825-friends-of-appropriate-ages.cpp
@@ -4,7 +4,7 @@ public:
         vector<int> freq(121);
         for(auto a : ages) freq[a]++;
         int res = 0;
-        for(int x = 15; x < 121; x++) {
+        for(int x = 1; x < 121; x++) {
             if(!freq[x]) continue;
             for(int y = x * 0.5 + 8; y <= x; y++) {
                 if(!freq[y]) continue;
