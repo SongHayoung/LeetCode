0bfc59a192d0b3f008c72936416fcae0c391acd0
SongHayoung <37806785+SongHayoung@users.noreply.github.com>
2024-04-19 19:51:55 +0900

    Time: 549 ms (8.41%), Space: 128.8 MB (6.26%) - LeetHub

--- 0945-minimum-increment-to-make-array-unique/0945-minimum-increment-to-make-array-unique.cpp
+++ 0945-minimum-increment-to-make-array-unique/0945-minimum-increment-to-make-array-unique.cpp
@@ -1,43 +1,34 @@
-class Solution {
-    int add(map<int,int>& mp, int x) {
-        auto it = mp.upper_bound(x);
-        if(it == begin(mp)) {
-            mp[x] = x;
-            if(it->first == x + 1) {
-                mp[x] = it->second;
-                mp.erase(it);
-            }
-            return 0;
-        }
-        --it;
-        int res = 0;
-        if(it->second + 1 == x) {
-            it->second = x;
-        } else if(it->second < x) {
-            mp[x] = x;
-            it++;
-        } else {
-            res = it->second - x + 1;
-            it->second++;
-        }
-        auto nit = next(it);
-        if(nit != end(mp) and it->second + 1 == nit->first) {
-            it->second = nit->second;
-            mp.erase(nit);
-        }
-        
-        
-        
-        
-        return res;
+unordered_map<int, int> uf, cnt;
+int find(int u) {
+    if(!uf.count(u)) {
+        cnt[u] = 1;
+        return uf[u] = u;
     }
+    return uf[u] == u ? u : uf[u] = find(uf[u]);
+}
+void uni(int u, int v) {
+    int pu = find(u), pv = find(v);
+    if(pu == pv) return;
+    cnt[pu] = cnt[pv] = cnt[pu] + cnt[pv];
+    uf[pu] = uf[pv] = min(pu,pv);
+}
+int best(int x) {
+    if(!uf.count(x)) return x;
+    int root = find(x), max = cnt[root];
+    return root + max;
+}
+class Solution {
 public:
     int minIncrementForUnique(vector<int>& nums) {
-        map<int, int> mp;
+        uf.clear();
+        cnt.clear();
         int res = 0;
-        mp[nums[0]] = nums[0];
-        for(int i = 1; i < nums.size(); i++) {
-            res += add(mp,nums[i]);
+        for(auto& x : nums) {
+            int now = best(x);
+            res += now - x;
+            find(now);
+            if(uf.count(now - 1)) uni(now, now - 1);
+            if(uf.count(now + 1)) uni(now, now + 1);
         }
         return res;
     }
