92bcf0c749ff3dbe17732c806794a83903504417
SongHayoung <37806785+SongHayoung@users.noreply.github.com>
2024-04-19 13:32:06 +0900

    Time: 3 ms (93.53%), Space: 11 MB (63.08%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -1,12 +1,19 @@
 class Solution {
-    unordered_map<int,int> dp[20];
-    int helper(vector<int>& A, int pos, int sum) {
-        if(pos == A.size()) return sum == 0;
-        if(dp[pos].count(sum)) return dp[pos][sum];
-        return dp[pos][sum] = helper(A,pos + 1, sum + A[pos]) + helper(A, pos + 1, sum - A[pos]);
-    }
 public:
     int findTargetSumWays(vector<int>& nums, int target) {
-        return helper(nums,0,-target);
+        int sum = accumulate(nums.begin(), nums.end(), 0);
+        if(sum < target) return 0;
+        int x = sum - target;
+        if(x & 1) return 0;
+        x /= 2;
+        vector<int> dp(x + 1);
+        dp[0] = 1;
+        for(auto& n : nums) {
+            for(int i = x - n; i >= 0; i--) {
+                dp[i+n] += dp[i];
+            }
+        }
+        
+        return dp.back();
     }
 };
