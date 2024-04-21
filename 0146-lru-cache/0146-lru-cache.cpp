375208952be3a843644d7b5fd7e24fc9185aef7c
SongHayoung <37806785+SongHayoung@users.noreply.github.com>
2024-04-21 20:13:57 +0900

    Time: 373 ms (29.02%), Space: 179.5 MB (33.44%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.cpp
+++ 0146-lru-cache/0146-lru-cache.cpp
@@ -1,57 +1,36 @@
-struct Node {
-    int key, value;
-    Node* prev, *next;
-    Node(int key, int val) : key(key), value(val), prev(nullptr), next(nullptr) {}
-};
+
 class LRUCache {
-    unordered_map<int,Node*> mp;
-    Node* head;
-    Node* tail;
     int cap;
-    void promote(int key) {
-        Node* node = mp[key];
-        if(tail->key == key) {
-            tail = tail->prev;
-        }
-        if(node->next) node->next->prev = node->prev;
-        if(node->prev) node->prev->next = node->next;
-        
-        node->next = node->prev = nullptr;
-        
-        node->next = head->next;
-        if(node->next) node->next->prev = node;
-        head->next = node;
-        node->prev = head;
-        
-        if(mp.size() > cap) {
-            tail = tail->prev;
-            mp.erase(tail->next->key);
-            tail->next = nullptr;
-        }
-        
-        if(tail->next) {
-            tail = tail->next;
-        }
-    }
+    list<int> li;
+    unordered_map<int,list<int>::iterator> it;
+    unordered_map<int,int> cache;
 public:
     LRUCache(int capacity): cap(capacity) {
-        head = new Node(-1, -1);
-        tail = head;
+
     }
 
     int get(int key) {
-        if(!mp.count(key)) return -1;
-        int res = mp[key]->value;
-        promote(key);
-        return res;
+        if(!cache.count(key)) return -1;
+        li.erase(it[key]);
+        li.push_front(key);
+        it[key] = begin(li);
+        return cache[key];
     }
 
     void put(int key, int value) {
-        if(mp.count(key)) {
-            mp[key]->value = value;
-        } else mp[key] = new Node(key,value);
-        promote(key);
-        return;
+        if(cap == cache.size() and !cache.count(key)) {
+            int evict = li.back();
+            li.pop_back();
+            cache.erase(evict);
+            it.erase(evict);
+        }
+        cache[key] = value;
+        if(it.count(key)) {
+            li.erase(it[key]);
+        }
+        li.push_front(key);
+        it[key] = begin(li);
+        return;cache[key];
     }
 };
 
@@ -61,3 +40,4 @@ public:
  * int param_1 = obj->get(key);
  * obj->put(key,value);
  */
+
