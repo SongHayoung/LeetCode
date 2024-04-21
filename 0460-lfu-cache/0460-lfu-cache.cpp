f4e8e3ae03ce305c0c5ced417ae0f1f7bdcf7d2a
SongHayoung <37806785+SongHayoung@users.noreply.github.com>
2024-04-21 20:09:49 +0900

    Time: 409 ms (40.80%), Space: 190.7 MB (34.52%) - LeetHub

--- 0460-lfu-cache/0460-lfu-cache.cpp
+++ 0460-lfu-cache/0460-lfu-cache.cpp
@@ -1,65 +1,55 @@
 class LFUCache {
-  unordered_map<int, tuple<int, std::list<int>::iterator, int>> mp;
-  unordered_map<int, std::list<int>> cnt;
-  int capacity = 0, lowest_cnt = 1;
-
+    int cap,lo;
+    unordered_map<int,pair<int, int>> cache;
+    unordered_map<int, list<int>> ord;
+    unordered_map<int, list<int>::iterator> it;
 public:
-  LFUCache(int capacity) : capacity(capacity) {}
+    LFUCache(int capacity): cap(capacity), lo(0) {}
 
-  int get(int key) {
-    if (capacity == 0) {
-      return -1;
+    void incr(int key) {
+        ord[cache[key].second].erase(it[key]);
+        if(ord[cache[key].second].size() == 0) {
+            ord.erase(cache[key].second);
+            if(lo == cache[key].second) lo++;
+        }
+
+        cache[key].second++;
+        ord[cache[key].second].push_front(key);
+        it[key] = begin(ord[cache[key].second]);
     }
 
-    if (auto it = mp.find(key); it == mp.end()) {
-      return -1;
-    } else {
-      auto &[value, node, count] = it->second;
-      auto &list = cnt[count];
-      list.erase(node);
-      if (list.size() == 0 && count == lowest_cnt) {
-        ++lowest_cnt;
-      }
-
-      auto &new_list = cnt[++count];
-      new_list.push_back(key);
-      node = --new_list.end();
-
-      return value;
+    int get(int key) {
+        if(!cache.count(key)) return -1;
+        incr(key);
+        return cache[key].first;
     }
-  }
-
-  void put(int key, int value) {
-    if (capacity == 0) {
-      return;
+    void del() {
+        int key = *prev(ord[lo].end());
+        ord[lo].pop_back();
+        if(ord[lo].size() == 0) ord.erase(lo);
+        it.erase(key);
+        cache.erase(key);
     }
 
-    if (auto it = mp.find(key); it != mp.end()) {
-      auto &[cur_value, node, count] = it->second;
-      auto &list = cnt[count];
-      list.erase(node);
-      if (list.size() == 0 && count == lowest_cnt) {
-        ++lowest_cnt;
-      }
-      auto &new_list = cnt[++count];
-      new_list.push_back(key);
-      cur_value = value;
-      node = --new_list.end();
-    } else if (mp.size() < capacity) {
-      auto &list = cnt[1];
-      list.push_back(key);
-      mp.insert({key, tuple{value, --list.end(), 1}});
-      lowest_cnt = 1;
-    } else {
-      auto &lowest_list = cnt[lowest_cnt];
-      auto lru_key_it = lowest_list.begin();
-      mp.erase(*lru_key_it);
-      lowest_list.erase(lru_key_it);
-
-      auto &list = cnt[1];
-      list.push_back(key);
-      mp.insert({key, tuple{value, --list.end(), 1}});
-      lowest_cnt = 1;
+    void put(int key, int value) {
+        if(cache.size() == cap and !cache.count(key)) {
+            del();
+        }
+        if(cache.count(key)) {
+            cache[key].first = value;
+            incr(key);
+        } else {
+            lo = 1;
+            cache[key] = {value, 1};
+            ord[1].push_front(key);
+            it[key] = begin(ord[cache[key].second]);
+        }
     }
-  }
 };
+
+/**
+ * Your LFUCache object will be instantiated and called as such:
+ * LFUCache* obj = new LFUCache(capacity);
+ * int param_1 = obj->get(key);
+ * obj->put(key,value);
+ */
