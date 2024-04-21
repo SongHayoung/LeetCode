6ea9f2c7db472a9d602327133c867e3cc999c5bb
SongHayoung <37806785+SongHayoung@users.noreply.github.com>
2024-04-21 20:17:41 +0900

    Time: 0 ms (100.00%), Space: 8 MB (74.01%) - LeetHub

--- 0050-powx-n/0050-powx-n.cpp
+++ 0050-powx-n/0050-powx-n.cpp
@@ -1,17 +1,18 @@
 class Solution {
-    double helper(double x, long long n) {
-        if(n == 0) return 1;
-        double res = helper(x, n>>1);
-        res = (res * res);
-        if(n & 1) return res * x;
-        return res;
-    }
 public:
     double myPow(double x, int n) {
-        bool sign = (n >= 0);
+        if(n == 0) return 1;
+        if(x == 0) return 0;
+        if(x == 1) return 1;
+        if(x == -1) return n & 1 ? -1 : 1;
         long long N = n;
-        N = abs(N);
-        double res = helper(x, N);
-        return sign ? res : 1 / res;
+        if(N < 0) x = 1. / x, N = -N;
+        double res = 1;
+        while(N) {
+            if(N & 1) res = res * x;
+            x = x * x;
+            N /= 2;
+        }
+        return res;
     }
 };
