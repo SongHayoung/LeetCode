0edf6094087c203d368f6a264f4dd44327b953ac
SongHayoung <37806785+SongHayoung@users.noreply.github.com>
2024-04-22 22:30:26 +0900

    Time: 37 ms (10.93%), Space: 94.5 MB (61.95%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
@@ -3,21 +3,21 @@ public:
     double findMedianSortedArrays(vector<int>& A, vector<int>& B) {
         int n = A.size(), m = B.size();
         if(n > m) return findMedianSortedArrays(B,A);
-        int l = 0, r = n, tot = n + m;
+        int tot = n + m, l = 0, r = n;
         while(l <= r) {
             int m1 = l + (r - l) / 2;
             int m2 = tot / 2 - m1;
             if(m1 and A[m1-1] > B[m2]) r = m1 - 1;
             else if(m1 != n and A[m1] < B[m2-1]) l = m1 + 1;
             else {
-                int ri = INT_MAX;
-                if(m1 != n) ri = min(ri, A[m1]);
-                if(m2 != m) ri = min(ri, B[m2]);
-                if(tot & 1) return ri;
-                int le = INT_MIN;
-                if(m1) le = max(le, A[m1-1]);
-                if(m2) le = max(le, B[m2-1]);
-                return 1. * (le + ri) / 2.;
+                int r = INT_MAX;
+                if(m1 != n) r = min(r, A[m1]);
+                if(m2 != m) r = min(r, B[m2]);
+                if(tot & 1) return r;
+                int l = INT_MIN;
+                if(m1) l = max(l, A[m1-1]);
+                if(m2) l = max(l, B[m2-1]);
+                return (l + r) / 2.;
             }
         }
         return -1;
