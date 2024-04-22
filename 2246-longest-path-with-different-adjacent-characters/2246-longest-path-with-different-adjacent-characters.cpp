eca267e31225b36aa82d3737c2298c49f7277641
SongHayoung <37806785+SongHayoung@users.noreply.github.com>
2024-04-22 18:37:54 +0900

    Time: 1119 ms (32.06%), Space: 268.7 MB (16.69%) - LeetHub

--- 2246-longest-path-with-different-adjacent-characters/2246-longest-path-with-different-adjacent-characters.cpp
+++ 2246-longest-path-with-different-adjacent-characters/2246-longest-path-with-different-adjacent-characters.cpp
@@ -1,36 +1,24 @@
 class Solution {
-    int res = 0;
-    vector<vector<int>> g;
-
-    int helper(int u, string& s) {
-        priority_queue<int, vector<int>, greater<int>> pq;
-        for(auto& v : g[u]) {
-            int path = helper(v,s);
-            if(s[v] != s[u]) {
-                pq.push(path);
-                if(pq.size() > 2) pq.pop();
-            }
+    int dfs(vector<vector<int>>& adj, int& res, int u, string& s) {
+        vector<int> best{0,0};
+        for(auto& v : adj[u]) {
+            int sub = dfs(adj, res, v, s);
+            if(s[v] == s[u]) continue;
+            best.push_back(sub);
+            sort(rbegin(best), rend(best));
+            best.pop_back();
         }
-
-        while(pq.size() < 2) pq.push(0);
-
-        int path1 = pq.top(); pq.pop();
-        int path2 = pq.top(); pq.pop();
-
-        res = max(res, path1 + path2 + 1);
-
-        return path2 + 1;
+        res = max(res, best[0] + best[1] + 1);
+        return best[0] + 1;
     }
 public:
-    int longestPath(vector<int>& p, string s) {
-        int n = p.size(), root = -1;
-        g = vector<vector<int>>(n);
-
-        for(int i = 0; i < n; i++) {
-            if(p[i] == -1) root = i;
-            else g[p[i]].push_back(i);
+    int longestPath(vector<int>& parent, string s) {
+        vector<vector<int>> adj(s.length());
+        for(int i = 1; i < parent.size(); i++) {
+            adj[parent[i]].push_back(i);
         }
-
-        return max(helper(root, s), res);
+        int res = 0;
+        dfs(adj,res,0,s);
+        return res;
     }
 };
