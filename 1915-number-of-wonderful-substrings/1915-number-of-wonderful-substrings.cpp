eb03fda8fc819bc7150fb39181f57142a8f2c09a
SongHayoung <37806785+SongHayoung@users.noreply.github.com>
2024-04-30 10:24:24 +0900

    Time: 39 ms (95.81%), Space: 16.7 MB (58.08%) - LeetHub

--- 1915-number-of-wonderful-substrings/1915-number-of-wonderful-substrings.cpp
+++ 1915-number-of-wonderful-substrings/1915-number-of-wonderful-substrings.cpp
@@ -1,22 +1,13 @@
 class Solution {
 public:
     long long wonderfulSubstrings(string word) {
-        long long res = 0, mask = 0;
-        unordered_map<int, int> freq{{0, 1}};
-        
-        for(auto& ch : word) {
-            mask ^= (1<<(ch-'a'));
-            res += freq[mask];
-            
-            for(int i = 0; i < 10; i++) {
-                res += freq[mask ^ (1<<i)];
-            }
-            
-            
-            freq[mask]++;
+        long prefix[1024]{1}, res(0), mask(0);
+        for(auto& c: word) {
+            mask ^= 1<<(c-'a');
+            res += prefix[mask]++;
+            for(int i = 0; i < 10; i++)
+                res += prefix[mask ^ (1<<i)];
         }
-        
-        
         return res;
     }
 };
