8329df158e35f47bf1e1a5225c544dd5c6c36ac2
SongHayoung <37806785+SongHayoung@users.noreply.github.com>
2024-05-03 08:41:23 +0900

    Time: 4 ms (71.88%), Space: 10 MB (77.08%) - LeetHub

--- 3125-maximum-number-that-makes-result-of-bitwise-and-zero/3125-maximum-number-that-makes-result-of-bitwise-and-zero.cpp
+++ 3125-maximum-number-that-makes-result-of-bitwise-and-zero/3125-maximum-number-that-makes-result-of-bitwise-and-zero.cpp
@@ -1,11 +1,7 @@
 class Solution {
 public:
     long long maxNumber(long long n) {
-        long long bit = -1;
-        while(n) {
-            bit++;
-            n /= 2;
-        }
-        return (1ll<<bit) - 1;
+        long long lg2 = log2(n);
+        return (1ll<<lg2) - 1;
     }
 };
