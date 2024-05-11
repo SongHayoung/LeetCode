83451e934336c9463f7eda7e9e8c32c768ca8a24
SongHayoung <37806785+SongHayoung@users.noreply.github.com>
2024-05-11 12:08:18 +0900

    Time: 25 ms (53.81%), Space: 35.5 MB (5.07%) - LeetHub

--- 1052-grumpy-bookstore-owner/1052-grumpy-bookstore-owner.cpp
+++ 1052-grumpy-bookstore-owner/1052-grumpy-bookstore-owner.cpp
@@ -1,20 +1,22 @@
 class Solution {
 public:
-    int maxSatisfied(vector<int>& A, vector<int>& B, int k) {
-        int ma = 0, res = 0, l = 0, r = 0, now = 0, n = A.size();
-        for(int i = 0; i < k; i++) {
-            if(B[i] == 0) res += A[i];
-            else {
-                now += A[i];
-                ma = max(now, ma);
-            }
+    int maxSatisfied(vector<int>& customers, vector<int>& grumpy, int minutes) {
+        int n = customers.size();
+        vector<int> cpre(n+1), gpre(n+1);
+        for(int i = 0; i < n; i++) {
+            cpre[i+1] = cpre[i];
+            gpre[i+1] = gpre[i];
+            if(grumpy[i]) gpre[i+1] += customers[i];
+            else cpre[i+1] += customers[i];
         }
-        for(int i = k; i < n; i++) {
-            if(B[i] == 0) res += A[i];
-            else now += A[i];
-            if(B[i-k] == 1) now -= A[i-k];
-            ma = max(ma, now);
-        }
-        return res + ma;
+        auto qry = [&](int t) {
+            int res = cpre[n];
+            res -= gpre[t];
+            res += gpre[min(t+minutes,n)];
+            return res;
+        };
+        int res = 0;
+        for(int i = 0; i < n; i++) res = max(res, qry(i));
+        return res;
     }
 };
