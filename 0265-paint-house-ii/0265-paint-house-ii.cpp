4d070a2f96be896c8ae83ed2b7a0136eb7de855f
SongHayoung <37806785+SongHayoung@users.noreply.github.com>
2024-05-11 10:52:27 +0900

    Time: 16 ms (30.97%), Space: 15.2 MB (12.09%) - LeetHub

--- 0265-paint-house-ii/0265-paint-house-ii.cpp
+++ 0265-paint-house-ii/0265-paint-house-ii.cpp
@@ -1,23 +1,19 @@
 class Solution {
 public:
     int minCostII(vector<vector<int>>& costs) {
-        int colors = costs[0].size();
-        vector<int> h(colors,0);
-        for(auto& cost : costs) {
-            priority_queue<pair<int,int>> pq;
-            
-            for(int i = 0; i < colors; i++) {
-                pq.push({h[i], i});
-                if(pq.size() > 2) pq.pop();
+        int n = costs.size(), m = costs[0].size();
+        vector<int> dp(m,0);
+        for(auto& c : costs) {
+            vector<pair<int,int>> best;
+            for(int j = 0; j < m; j++) {
+                best.push_back({dp[j],j});
+                sort(begin(best),end(best));
+                if(best.size() > 2) best.pop_back();
             }
-            
-            auto ma = pq.top(); pq.pop();
-            auto mi = pq.top(); pq.pop();
-            
-            for(int i = 0; i < colors; i++) {
-                h[i] = cost[i] + (i == mi.second ? ma.first : mi.first);
+            for(int j = 0; j < m; j++) {
+                dp[j] = c[j] + (best[0].second == j ? best[1].first : best[0].first);
             }
         }
-        return *min_element(begin(h), end(h));
+        return *min_element(begin(dp),end(dp));
     }
 };
