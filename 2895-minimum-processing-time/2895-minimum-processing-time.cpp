4d7f406c8b81365a39b834af64af3800e684ab60
SongHayoung <37806785+SongHayoung@users.noreply.github.com>
2024-05-13 22:14:19 +0900

    Time: 131 ms (82.60%), Space: 104.9 MB (12.09%) - LeetHub

--- 2895-minimum-processing-time/2895-minimum-processing-time.cpp
+++ 2895-minimum-processing-time/2895-minimum-processing-time.cpp
@@ -1,15 +1,11 @@
 class Solution {
 public:
-    int minProcessingTime(vector<int>& A, vector<int>& tasks) {
-        sort(rbegin(tasks), rend(tasks));
-        sort(begin(A), end(A));
+    int minProcessingTime(vector<int>& A, vector<int>& B) {
+        sort(rbegin(A), rend(A));
+        sort(begin(B), end(B));
         int res = 0;
-        while(A.size()) {
-            for(int i = 0; i < 4; i++) {
-                int time = A.back() + tasks.back();
-                res = max(res, time); tasks.pop_back();
-            }
-            A.pop_back();
+        for(int i = 0; i < B.size(); i++) {
+            res = max(res, A[i/4] + B[i]);
         }
         return res;
     }
