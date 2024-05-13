cc057f561a14a4f12adc6d99710b3f9a221f8282
SongHayoung <37806785+SongHayoung@users.noreply.github.com>
2024-05-13 22:07:30 +0900

    Time: 753 ms (20.00%), Space: 251.1 MB (10.00%) - LeetHub

--- 2378-choose-edges-to-maximize-score-in-a-tree/2378-choose-edges-to-maximize-score-in-a-tree.cpp
+++ 2378-choose-edges-to-maximize-score-in-a-tree/2378-choose-edges-to-maximize-score-in-a-tree.cpp
@@ -1,23 +1,30 @@
 class Solution {
-    vector<vector<pair<long long, long long>>> adj;
-    pair<long long, long long> dfs(int u) {
-        long long inc = 0, exc = 0;
+    vector<long long> dfs(vector<vector<pair<long long,long long>>>& adj, int u) {
+        vector<long long> res{0,0};
+        long long best = 0, sum = 0;
+        vector<vector<long long>> dp;
         for(auto& [v,w] : adj[u]) {
-            auto [vinc, vexc] = dfs(v);
-            inc = max(inc, w + vexc - vinc);
-            exc += vinc;
+            auto sub = dfs(adj,v);
+            dp.push_back({w + sub[1], sub[0]});
+            best += max(sub[0], sub[1]);
+            sum += sub[0];
         }
-        return {inc + exc, exc};
+        for(int i = 0; i < dp.size(); i++) {
+            res[0] = max(res[0], sum + dp[i][0] - dp[i][1]);
+        }
+        res[0] = max(res[0], best);
+        res[1] = best;
+        return res;
     }
 public:
-    long long maxScore(vector<vector<int>>& A) {
-        int n = A.size();
-        adj = vector<vector<pair<long long, long long>>>(n);
-        for(int i = 1; i < n; i++) {
-            int par = A[i][0], w = A[i][1];
-            adj[par].push_back({i,max(0,w)});
+    long long maxScore(vector<vector<int>>& edges) {
+        vector<vector<pair<long long,long long>>> adj(edges.size());
+        int root;
+        for(int u = 0; u < edges.size(); u++) {
+            if(edges[u][0] == -1) root = u;
+            else adj[edges[u][0]].push_back({u, max(0,edges[u][1])});
         }
-        auto [inc, _] = dfs(0);
-        return inc;
+        auto res = dfs(adj,root);
+        return max(res[0], res[1]);
     }
 };
