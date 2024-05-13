cd9c90bd15acc0cff8929248b4910a7de2b12126
SongHayoung <37806785+SongHayoung@users.noreply.github.com>
2024-05-13 21:36:19 +0900

    Time: 114 ms (63.05%), Space: 101 MB (51.63%) - LeetHub

--- 1658-minimum-operations-to-reduce-x-to-zero/1658-minimum-operations-to-reduce-x-to-zero.cpp
+++ 1658-minimum-operations-to-reduce-x-to-zero/1658-minimum-operations-to-reduce-x-to-zero.cpp
@@ -1,30 +1,27 @@
 class Solution {
 public:
     int minOperations(vector<int>& nums, int x) {
-        int sz = nums.size();
-        int left = 0, right = sz - 1;
-        int val = 0, res = INT_MAX;
-        for(; left < sz && val + nums[left] <= x; left++) {
-                val += nums[left];
+        int res = INT_MAX;
+        int i = 0, lsum = 0, j = nums.size() - 1, rsum = 0;
+        while(i < nums.size() and lsum + nums[i] <= x) lsum += nums[i++];
+        if(i == nums.size()) {
+            if(lsum == x) return nums.size();
+            return -1;
         }
-        
-        if(left == sz)
-            return val == x ? left : -1;
-        
-        while(right != -1) {
-            if(val == x) {
-                res = min(res, left + sz - right - 1);
+        i--;
+        while(i >= 0) {
+            while(i <= j and lsum + rsum + nums[j] <= x) {
+                rsum += nums[j--];
             }
-            
-            if(right && val + nums[right] <= x) {
-                val += nums[right--];
-            } else if(left){
-                val -= nums[--left];
-            } else {
-                break;
+            if(lsum + rsum == x) {
+                res = min(res, i + 1 + (int)nums.size() - j - 1);
             }
+            lsum -= nums[i--];
         }
-
+        while(j >= 0 and rsum + nums[j] <= x) rsum += nums[j--];
+        if(rsum == x) res = min(res, (int)nums.size() - j - 1);
+        
+        
         return res == INT_MAX ? -1 : res;
     }
 };
