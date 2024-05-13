d227f949251f8a1891363e2961db18b0ee5861cd
SongHayoung <37806785+SongHayoung@users.noreply.github.com>
2024-05-13 21:25:27 +0900

    Time: 5 ms (8.00%), Space: 9.4 MB (7.66%) - LeetHub

--- 0791-custom-sort-string/0791-custom-sort-string.cpp
+++ 0791-custom-sort-string/0791-custom-sort-string.cpp
@@ -3,10 +3,6 @@ public:
     string customSortString(string order, string s) {
         unordered_map<char,int> ord;
         for(int i = 0; i < order.size(); i++) ord[order[i]] = i;
-        for(int i = 0, j = order.size(); i < 26; i++) {
-            if(ord.count(i + 'a')) continue;
-            ord[i+'a'] = j++;
-        }
         sort(begin(s), end(s), [&](char a, char b) {
             return ord[a] < ord[b];
         });
