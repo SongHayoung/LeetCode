0cec73657d19b020a00272b845f099d3a455c8f8
SongHayoung <37806785+SongHayoung@users.noreply.github.com>
2024-05-13 21:22:30 +0900

    Time: 2 ms (56.22%), Space: 7 MB (66.72%) - LeetHub

--- 0319-bulb-switcher/0319-bulb-switcher.cpp
+++ 0319-bulb-switcher/0319-bulb-switcher.cpp
@@ -1,6 +1,15 @@
 class Solution {
 public:
     int bulbSwitch(int n) {
-        return sqrt(n);
+        int l = 0, r = n, res = 0;
+        while(l <= r) {
+            long long m = l + (r - l) / 2;
+            bool ok = m * m <= n;
+            if(ok) {
+                res = m;
+                l = m + 1;
+            } else r = m - 1;
+        }
+        return res;
     }
 };
