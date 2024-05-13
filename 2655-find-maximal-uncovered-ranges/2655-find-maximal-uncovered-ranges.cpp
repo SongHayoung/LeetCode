5c18c10124c2cffdc40d33673c91e994e900a0d1
SongHayoung <37806785+SongHayoung@users.noreply.github.com>
2024-05-13 23:31:37 +0900

    Time: 368 ms (34.61%), Space: 147.5 MB (76.92%) - LeetHub

--- 2655-find-maximal-uncovered-ranges/2655-find-maximal-uncovered-ranges.cpp
+++ 2655-find-maximal-uncovered-ranges/2655-find-maximal-uncovered-ranges.cpp
@@ -1,30 +1,17 @@
 class Solution {
-    map<int, int> range;
-    void add(int s, int e) {
-        if(!range.count(s)) range[s] = -1;
-        range[s] = max(range[s], e);
-        auto it = range.lower_bound(s);
-        if(it != begin(range) and prev(it)->second + 1 >= s) {
-            prev(it)->second = max(prev(it)->second, e);
-            it = prev(it);
-        }
-        while(next(it) != end(range) and it->second + 1 >= next(it)->first) {
-            it->second = max(it->second, next(it)->second);
-            range.erase(next(it));
-        }
-    }
 public:
     vector<vector<int>> findMaximalUncoveredRanges(int n, vector<vector<int>>& ranges) {
-        range.clear();
-        add(-1,-1);
-        add(n,n);
-        for(auto r : ranges) {
-            add(r[0],r[1]);
-        }
+        ranges.push_back({n,n});
+        ranges.push_back({-1,-1});
+        sort(rbegin(ranges), rend(ranges));
         vector<vector<int>> res;
-        for(auto it = begin(range); next(it) != end(range); it++) {
-            int l = it->second + 1, r = next(it)->first - 1;
-            if(0 <= l and l <= n and 0 <= r and r <= n) res.push_back({l,r});
+        while(ranges.size()) {
+            int x = ranges.back()[1];
+            while(ranges.size() and ranges.back()[0] <= x + 1) {
+                x = max(x, ranges.back()[1]);
+                ranges.pop_back();
+            }
+            if(ranges.size()) res.push_back({x+1,ranges.back()[0] - 1});
         }
         return res;
     }
