0aaef9a4567503ec1cae2b343b924311df7c294e
SongHayoung <37806785+SongHayoung@users.noreply.github.com>
2024-05-13 21:15:22 +0900

    Time: 621 ms (7.21%), Space: 9.9 MB (67.66%) - LeetHub

--- 1986-minimum-number-of-work-sessions-to-finish-the-tasks/1986-minimum-number-of-work-sessions-to-finish-the-tasks.cpp
+++ 1986-minimum-number-of-work-sessions-to-finish-the-tasks/1986-minimum-number-of-work-sessions-to-finish-the-tasks.cpp
@@ -1,5 +1,6 @@
 class Solution {
     int dp[1<<14];
+    vector<int> c;
     bool bit(int x, int k) {
         return (x>>k) & 1;
     }
@@ -7,13 +8,9 @@ class Solution {
         int n = A.size();
         if(dp[mask] != -1) return dp[mask];
         int& res = dp[mask] = 22;
-        for(int i = 1; i < 1<<n; i++) {
-            if(mask & i) continue;
-            int sum = 0;
-            for(int j = 0; j < n; j++) {
-                if(bit(i,j)) sum += A[j];
-            }
-            if(sum <= t) res = min(res, 1 + helper(A,mask | i, t));
+        for(auto& x : c) {
+            if(mask & x) continue;
+            res = min(res, 1 + helper(A, mask | x, t));
         }
         return res;
     }
@@ -22,6 +19,14 @@ public:
         int n = A.size();
         memset(dp,-1,sizeof dp);
         dp[(1<<n) - 1] = 0;
+        c = {};
+        for(int i = 1; i < 1<<n; i++) {
+            int sum = 0;
+            for(int j = 0; j < n; j++) {
+                if(bit(i,j)) sum += A[j];
+            }
+            if(sum <= sessionTime) c.push_back(i);
+        }
         return helper(A,0,sessionTime);
     }
 };
