295718cb9fa38e6284c20d8109ec755874960d58
SongHayoung <37806785+SongHayoung@users.noreply.github.com>
2024-05-14 11:54:39 +0900

    Time: 83 ms (97.75%), Space: 85.1 MB (69.66%) - LeetHub

--- 1562-find-latest-group-of-size-m/1562-find-latest-group-of-size-m.cpp
+++ 1562-find-latest-group-of-size-m/1562-find-latest-group-of-size-m.cpp
@@ -1,36 +1,16 @@
 class Solution {
-    map<int, int> r;
-    multiset<int> ms;
-    void merge(int x) {
-        r[x] = x;
-        ms.insert(1);
-        {
-            auto it = r.lower_bound(x);
-            if(it != begin(r)) {
-                --it;
-                if(it->second + 1 == x) x = it->first;
-            }
-        }
-        auto rm = [&](int len) {
-            ms.erase(ms.find(len));
-        };
-        while(r.count(r[x] + 1)) {
-            int y = r[r[x] + 1];
-            rm(y - (r[x] + 1) + 1);
-            rm(r[x] - x + 1);
-            r.erase(r[x] + 1);
-            r[x] = y;
-            ms.insert(y - x + 1);
-        }
-    }
 public:
     int findLatestStep(vector<int>& arr, int m) {
+        vector<int> fl(arr.size() + 2);
+        vector<int> cnt(arr.size() + 1);
         int res = -1;
-        r = {};
-        ms = {};
         for(int i = 0; i < arr.size(); i++) {
-            merge(arr[i]);
-            if(ms.count(m)) res = i + 1;
+            fl[arr[i]] = 1;
+            int le = fl[arr[i] - 1], ri = fl[arr[i]+1], len = le + ri + 1;
+            cnt[le]--, cnt[ri]--;
+            cnt[len]++;
+            fl[arr[i]-le] = fl[arr[i]+ri] = len;
+            if(cnt[m]) res = i + 1;
         }
         return res;
     }
