aac747f2313265d307082deb921591bf7ea562e5
SongHayoung <37806785+SongHayoung@users.noreply.github.com>
2024-05-14 10:49:32 +0900

    Time: 21 ms (11.50%), Space: 15.1 MB (12.39%) - LeetHub

--- 0265-paint-house-ii/0265-paint-house-ii.cpp
+++ 0265-paint-house-ii/0265-paint-house-ii.cpp
@@ -2,18 +2,25 @@ class Solution {
 public:
     int minCostII(vector<vector<int>>& costs) {
         int n = costs.size(), m = costs[0].size();
-        vector<int> dp(m,0);
-        for(auto& c : costs) {
-            vector<pair<int,int>> best;
-            for(int j = 0; j < m; j++) {
-                best.push_back({dp[j],j});
-                sort(begin(best),end(best));
+        vector<int> dp(m);
+        auto who = [](vector<pair<int,int>>& A, int p) {
+            for(auto& [a,b] : A) {
+                if(b != p) return a;
+            }
+            return 0;
+        };
+        for(int i = 0; i < n; i++) {
+            vector<pair<int, int>> best;
+            for(int i = 0; i < m; i++) {
+                best.push_back({dp[i],i});
+                sort(begin(best), end(best));
                 if(best.size() > 2) best.pop_back();
             }
             for(int j = 0; j < m; j++) {
-                dp[j] = c[j] + (best[0].second == j ? best[1].first : best[0].first);
+                dp[j] = costs[i][j] + who(best,j);
             }
         }
-        return *min_element(begin(dp),end(dp));
+        
+        return *min_element(begin(dp), end(dp));
     }
 };
