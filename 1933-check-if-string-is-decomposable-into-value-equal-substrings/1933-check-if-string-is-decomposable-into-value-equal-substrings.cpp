8fa24c77f1db57b76de8efc23a9d1623c7b7a088
SongHayoung <37806785+SongHayoung@users.noreply.github.com>
2024-05-14 18:14:41 +0900

    Time: 4 ms (11.11%), Space: 7.7 MB (100.00%) - LeetHub

--- 1933-check-if-string-is-decomposable-into-value-equal-substrings/1933-check-if-string-is-decomposable-into-value-equal-substrings.cpp
+++ 1933-check-if-string-is-decomposable-into-value-equal-substrings/1933-check-if-string-is-decomposable-into-value-equal-substrings.cpp
@@ -1,13 +1,15 @@
 class Solution {
 public:
     bool isDecomposable(string s) {
-        deque<int> cnt;
-        for(int i = 0; i < s.length(); i++) {
-            if(i and s[i] == s[i-1]) cnt.back()++;
-            else cnt.push_back(1);
+        bool fl = false;
+        while(s.size()) {
+            char ch = s.back();
+            int cnt = 0;
+            while(s.size() and s.back() == ch) s.pop_back(), cnt++;
+            if(cnt % 3 == 0) continue;
+            if((cnt - 2) % 3 or fl) return false;
+            fl = true;
         }
-        while(cnt.size() and cnt.back() % 3 == 0) cnt.pop_back();
-        while(cnt.size() and cnt.front() % 3 == 0) cnt.pop_front();
-        return cnt.size() == 1 and cnt[0] >= 2 and (cnt[0] - 2) % 3 == 0;
+        return fl;
     }
 };
