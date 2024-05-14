0383f67c10ef65f7590de3e73409a90981907e57
SongHayoung <37806785+SongHayoung@users.noreply.github.com>
2024-05-14 22:35:15 +0900

    Time: 489 ms (58.79%), Space: 225.5 MB (42.91%) - LeetHub

--- 2421-number-of-good-paths/2421-number-of-good-paths.cpp
+++ 2421-number-of-good-paths/2421-number-of-good-paths.cpp
@@ -1,51 +1,35 @@
 class Solution {
-    vector<int> uf;
-    unordered_map<int, int> level;
-    unordered_map<int, int> par;
-    unordered_map<int, vector<int>> adj;
-    void dfs(int u, int p, int lvl = 0) {
-        par[u] = p;
-        level[u] = lvl;
-        for(auto v : adj[u]) {
-            if(v == p) continue;
-            dfs(v,u, lvl + 1);
-        }
-    }
+    int uf[30303];
     int find(int u) {
         return uf[u] == u ? u : uf[u] = find(uf[u]);
     }
     void uni(int u, int v) {
         int pu = find(u), pv = find(v);
-        uf[pu] = uf[pv] = level[pu] < level[pv] ? pu : pv;
+        uf[pu] = uf[pv] = min(pu, pv);
     }
 public:
     int numberOfGoodPaths(vector<int>& vals, vector<vector<int>>& edges) {
-        for(auto e : edges) {
+        for(int i = 0; i < vals.size(); i++) uf[i] = i;
+        int n = vals.size();
+        vector<vector<int>> adj(n);
+        for(auto& e : edges) {
             int u = e[0], v = e[1];
             adj[u].push_back(v);
             adj[v].push_back(u);
         }
-        dfs(0,-1);
-        vector<pair<int, int>> A;
-        for(int i = 0; i < vals.size(); i++) A.push_back({vals[i],i});
-        uf = vector<int>(vals.size());
-        iota(begin(uf), end(uf),0);
-        int res = vals.size();
-        sort(begin(A), end(A));
-        int l = 0, r = 0, n = A.size();
-        while(r < n) {
-            while(r < n and A[l].first == A[r].first) r++;
-            unordered_map<int, int> now;
-            while(l < r) {
-                auto [_, u] = A[l];
-                u = find(u);
-                while(par[u] != -1 and vals[par[u]] <= A[l].first) {
-                    uni(u,par[u]);
-                    u = find(u);
+        map<int,vector<int>> mp;
+        for(int i = 0; i < n; i++) mp[vals[i]].push_back(i);
+        int res = 0;
+        for(auto& [_,vec] : mp) {
+            for(auto& u : vec) {
+                for(auto& v : adj[u]) {
+                    if(vals[v] <= vals[u]) uni(u,v);
                 }
-                res += now[u];
-                now[u] += 1;
-                l += 1;
+            }
+            unordered_map<int,int> freq;
+            for(auto& u : vec) {
+                int pu = find(u);
+                res += ++freq[pu];
             }
         }
         return res;
