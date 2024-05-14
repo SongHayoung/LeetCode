d860532ec22fc297bf168dc4f85b75395aa1e8c7
SongHayoung <37806785+SongHayoung@users.noreply.github.com>
2024-05-14 14:30:30 +0900

    Time: 258 ms (76.84%), Space: 135.7 MB (77.18%) - LeetHub

--- 3080-mark-elements-on-array-by-performing-queries/3080-mark-elements-on-array-by-performing-queries.cpp
+++ 3080-mark-elements-on-array-by-performing-queries/3080-mark-elements-on-array-by-performing-queries.cpp
@@ -1,23 +1,28 @@
 class Solution {
 public:
     vector<long long> unmarkedSumArray(vector<int>& nums, vector<vector<int>>& queries) {
-        map<int,set<int>> mp;
         vector<int> fl(nums.size());
+        vector<int> ord(nums.size());
         long long sum = 0;
-        for(int i = 0; i < nums.size(); i++) mp[nums[i]].insert(i), sum += nums[i];
+        for(int i = 0; i < nums.size(); i++) sum += nums[i], ord[i] = i;
+        sort(begin(ord), end(ord), [&](int i, int j) {
+            if(nums[i] == nums[j]) return i > j;
+            return nums[i] > nums[j];
+        });
         vector<long long> res;
-        auto mark = [&](int idx) {
-            if(fl[idx]) return;
-            fl[idx] = true;
-            sum -= nums[idx];
-            mp[nums[idx]].erase(idx);
-            if(mp[nums[idx]].size() == 0) mp.erase(nums[idx]);
-        };
         for(auto& q : queries) {
-            mark(q[0]);
-            while(mp.size() and q[1]--) {
-                int idx = *mp.begin()->second.begin();
-                mark(idx);
+            if(!fl[q[0]]) {
+                sum -= nums[q[0]];
+                fl[q[0]] = true;
+            }
+            while(q[1] and ord.size()) {
+                while(ord.size() and fl[ord.back()] == 1) ord.pop_back();
+                if(ord.size()) {
+                    q[1]--;
+                    sum -= nums[ord.back()];
+                    fl[ord.back()] = 1;
+                    ord.pop_back();
+                }
             }
             res.push_back(sum);
         }
