4993e2ada3e20b684e44b0c2cb57a632a3321de4
SongHayoung <37806785+SongHayoung@users.noreply.github.com>
2024-05-14 10:35:23 +0900

    Time: 4 ms (97.21%), Space: 18.9 MB (31.58%) - LeetHub

--- 1326-minimum-number-of-taps-to-open-to-water-a-garden/1326-minimum-number-of-taps-to-open-to-water-a-garden.cpp
+++ 1326-minimum-number-of-taps-to-open-to-water-a-garden/1326-minimum-number-of-taps-to-open-to-water-a-garden.cpp
@@ -1,13 +1,27 @@
 class Solution {
 public:
     int minTaps(int n, vector<int>& ranges) {
-        vector<int> dp(n + 1, n + 2);
-        dp[0] = 0;
-        for(int i = 0; i <= n; i++) {
-            for(int j = max(0, i - ranges[i]); j <= min(n, i + ranges[i]); j++) {
-                dp[j] = min(dp[j], dp[max(0, i-ranges[i])] + 1);
-            }
+        vector<pair<int, int>> A;
+        for(int i = 0; i < ranges.size(); i++) {
+            int l = max(0, i - ranges[i]);
+            int r = min(n, i + ranges[i]);
+            A.push_back({l,r});
         }
-        return dp.back() == n + 2 ? -1 : dp.back();
+        sort(begin(A), end(A), [&](auto a, auto b) {
+            if(a.first == b.first) return a.second > b.second;
+            return a.first < b.first;
+        });
+        if(A[0].first != 0) return -1;
+        int res = 1, best = A[0].second, ma = best;
+        for(auto& [l,r] : A) {
+            if(l > best) {
+                res++;
+                best = ma;
+            }
+            if(l > best) return -1;
+            ma = max(ma, r);
+        }
+        if(best != n) res++, best = ma;
+        return best == n ? res : -1;
     }
 };
