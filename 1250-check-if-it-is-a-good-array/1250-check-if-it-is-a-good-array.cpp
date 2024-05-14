904b89671923ac31a0c3b34a3fd682c6b50445fa
SongHayoung <37806785+SongHayoung@users.noreply.github.com>
2024-05-14 14:50:40 +0900

    Time: 40 ms (50.51%), Space: 31.7 MB (40.00%) - LeetHub

--- 1250-check-if-it-is-a-good-array/1250-check-if-it-is-a-good-array.cpp
+++ 1250-check-if-it-is-a-good-array/1250-check-if-it-is-a-good-array.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     bool isGoodArray(vector<int>& nums) {
         int g = nums[0];
-        for(auto& x : nums) g = gcd(g,x);
+        for(auto& x : nums) g = __gcd(g,x);
         return g == 1;
     }
 };
