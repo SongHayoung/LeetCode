84ef6d0e5afd2cc2836ec43296706f73712f4e2e
SongHayoung <37806785+SongHayoung@users.noreply.github.com>
2024-05-15 11:06:34 +0900

    Time: 80 ms (58.33%), Space: 84 MB (6.25%) - LeetHub

--- 1599-maximum-profit-of-operating-a-centennial-wheel/1599-maximum-profit-of-operating-a-centennial-wheel.cpp
+++ 1599-maximum-profit-of-operating-a-centennial-wheel/1599-maximum-profit-of-operating-a-centennial-wheel.cpp
@@ -1,28 +1,22 @@
 class Solution {
 public:
-    int minOperationsMaxProfit(vector<int>& customers, int boardingCost, int runningCost) {
-        int profit = 0, ma = 0, res = -1, wait = 0, rotate = 1;
-        for(auto& c : customers) {
-            wait += c;
-            int board = min(wait,4);
-            profit += board*boardingCost - runningCost;
-            wait -= board;
-            if(profit > ma) {
-                ma = profit;
-                res = rotate;
+    int minOperationsMaxProfit(vector<int>& C, int b, int r) {
+        if(b * 4 <= r) return -1;
+        int wait = 0;
+        pair<int,int> best{0,-1};
+        pair<int,int> now{0,0};
+        reverse(begin(C), end(C));
+        while(C.size() or wait) {
+            if(C.size()) {
+                wait += C.back();
+                C.pop_back();
             }
-            rotate++;
+            int mi = min(4, wait);
+            now.first += mi * b - r;
+            now.second++;
+            if(best.first < now.first) best = now;
+            wait -= mi;
         }
-        while(wait) {
-            int board = min(wait,4);
-            profit += board*boardingCost - runningCost;
-            wait -= board;
-            if(profit > ma) {
-                ma = profit;
-                res = rotate;
-            }
-            rotate++;
-        }
-        return res;
+        return best.second;
     }
 };
