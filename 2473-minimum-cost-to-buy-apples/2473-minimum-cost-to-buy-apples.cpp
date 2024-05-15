8eef2478a41c324c838cedd6be9967e9f3cc7325
SongHayoung <37806785+SongHayoung@users.noreply.github.com>
2024-05-15 10:49:49 +0900

    Time: 224 ms (33.92%), Space: 113.9 MB (22.91%) - LeetHub

--- 2473-minimum-cost-to-buy-apples/2473-minimum-cost-to-buy-apples.cpp
+++ 2473-minimum-cost-to-buy-apples/2473-minimum-cost-to-buy-apples.cpp
@@ -1,34 +1,36 @@
 class Solution {
-    long long bfs(vector<vector<pair<long long, long long>>>& adj, int start, int n, vector<int>& apple, int k) {
-        long long res = apple[start];
-        vector<long long> cost(n,1e18);
-        cost[start] = 0;
-        priority_queue<pair<long long, long long>, vector<pair<long long, long long>>, greater<pair<long long, long long>>> q;
-        q.push({0,start});
+    vector<vector<pair<long long, long long>>> adj; 
+    long long helper(vector<int>& C, int u) {
+        priority_queue<pair<long long, long long>, vector<pair<long long, long long>>, greater<>> q;
+        long long res = 1e18;
+        vector<long long> D(C.size(), 1e18);
+        auto push = [&](long long u, long long c) {
+            if(D[u] > c) {
+                D[u] = c;
+                q.push({c,u});
+            }
+        };
+        push(u,0);
         while(q.size() and q.top().first < res) {
             auto [c,u] = q.top(); q.pop();
-            if(cost[u] != c) continue;
-            if(u != start) res = min(res, cost[u] * (k + 1) + apple[u]);
+            res = min(res, c + C[u]);
             for(auto& [v,w] : adj[u]) {
-                if(cost[v] > cost[u] + w) {
-                    cost[v] = cost[u] + w;
-                    q.push({cost[v], v});
-                }
+                push(v,c+w);
             }
         }
         return res;
     }
 public:
     vector<long long> minCost(int n, vector<vector<int>>& roads, vector<int>& appleCost, int k) {
-        vector<long long> res;
-        vector<vector<pair<long long, long long>>> adj(n);
-        for(auto r : roads) {
-            int u = r[0] - 1, v = r[1] - 1, w = r[2];
-            adj[u].push_back({v,w});
-            adj[v].push_back({u,w});
+        adj = vector<vector<pair<long long, long long>>>(n);
+        for(auto& r : roads) {
+            long long u = r[0] - 1, v = r[1] - 1, w = r[2];
+            adj[u].push_back({v,w * (1 + k)});
+            adj[v].push_back({u,w * (1 + k)});
         }
+        vector<long long> res;
         for(int i = 0; i < n; i++) {
-            res.push_back(bfs(adj,i,n,appleCost,k));
+            res.push_back(helper(appleCost,i));
         }
         return res;
     }
