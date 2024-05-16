a10f4e7513abf1efd5fe53a50c03bae0e53757e0
SongHayoung <37806785+SongHayoung@users.noreply.github.com>
2024-05-16 18:42:56 +0900

    Time: 7 ms (87.82%), Space: 22.5 MB (53.38%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.cpp
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -1,19 +1,19 @@
 class Solution {
 public:
     int trap(vector<int>& A) {
-        vector<pair<int,int>> st;
         int res = 0;
+        vector<pair<int,int>> st;
         for(int i = 0; i < A.size(); i++) {
-            int at = i;
+            int p = i;
             while(st.size() and st.back().second <= A[i]) {
-                auto [pos, h] = st.back(); st.pop_back();
-                at = pos;
-                if(st.size()) {
-                    int w = i - at;
-                    res += w * (min(A[i], st.back().second) - h);
-                }
+                auto [at,h] = st.back(); st.pop_back();
+                if(st.size() == 0) break;
+                int w = (i - at);
+                int x = min(A[i],st.back().second) - h;
+                res += w * x;
+                p = at;
             }
-            st.push_back({at,A[i]});
+            st.push_back({p,A[i]});
         }
         return res;
     }
