821eaa390030b616ebbdc239019595e508a5ef28
SongHayoung <37806785+SongHayoung@users.noreply.github.com>
2024-05-16 12:06:18 +0900

    Time: 20 ms (14.29%), Space: 11.7 MB (15.21%) - LeetHub

--- 2914-minimum-number-of-changes-to-make-binary-string-beautiful/2914-minimum-number-of-changes-to-make-binary-string-beautiful.cpp
+++ 2914-minimum-number-of-changes-to-make-binary-string-beautiful/2914-minimum-number-of-changes-to-make-binary-string-beautiful.cpp
@@ -3,7 +3,7 @@ public:
     int minChanges(string s) {
         int res = 0;
         for(int i = 0; i < s.length(); i += 2) {
-            if(s[i] != s[i+1]) res += 1;
+            if(s[i] != s[i+1]) res++;
         }
         return res;
     }
