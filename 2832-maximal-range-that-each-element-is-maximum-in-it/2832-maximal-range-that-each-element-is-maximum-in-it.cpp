7789bd38ff24420b263efa18a32a0afe815e9fd0
SongHayoung <37806785+SongHayoung@users.noreply.github.com>
2024-05-16 10:24:19 +0900

    Time: 251 ms (11.11%), Space: 129.9 MB (37.78%) - LeetHub

--- 2832-maximal-range-that-each-element-is-maximum-in-it/2832-maximal-range-that-each-element-is-maximum-in-it.cpp
+++ 2832-maximal-range-that-each-element-is-maximum-in-it/2832-maximal-range-that-each-element-is-maximum-in-it.cpp
@@ -1,30 +1,31 @@
-int uf[101010], sz[101010];
-int find(int u) {
-    return uf[u] == u? u : uf[u] = find(uf[u]);
-}
-int uni(int u, int v) {
-    int pu = find(u), pv = find(v);
-    sz[pu] = sz[pv] = sz[pu] + sz[pv];
-    uf[pu] = uf[pv] = min(pu,pv);
-    return sz[pu];
-}
 class Solution {
+    int uf[101010], cnt[101010];
+    int find(int u) {
+        return uf[u] == u ? u : uf[u] = find(uf[u]);
+    }
+    void uni(int u, int v) {
+        int pu = find(u), pv = find(v);
+        if(pu == pv) return;
+        cnt[pu] = cnt[pv] = cnt[pu] + cnt[pv];
+        uf[pu] = uf[pv] = min(pu,pv);
+    }
 public:
-    vector<int> maximumLengthOfRanges(vector<int>& nums) {
-        int n = nums.size();
-        vector<int> ord(n), on(n), res(n);
-        for(int i = 0; i < n; i++) ord[i] = uf[i] = i, sz[i] = 1;
-        sort(begin(ord), end(ord), [&](int i, int j) {return nums[i]<nums[j];});
-        for(int i = 0; i < n; i++) {
-            int p = ord[i], ma = 1;
-            on[p] = true;
-            if(p - 1 >= 0 and on[p-1]) {
-                ma = max(ma, uni(p,p-1));
-            }
-            if(p + 1 < n and on[p+1]) {
-                ma = max(ma, uni(p,p+1));
-            }
-            res[p] = ma;
+    vector<int> maximumLengthOfRanges(vector<int>& A) {
+        vector<int> ord(A.size());
+        for(int i = 0; i < A.size(); i++) {
+            ord[i] = uf[i] = i;
+            cnt[i] = 1;
+        }
+        sort(begin(ord), end(ord), [&](int i, int j) {
+            return A[i] < A[j];
+        });
+        vector<int> res(ord.size());
+        for(int i = 0; i < A.size(); i++) {
+            int x = ord[i];
+            int le = x - 1, ri = x + 1;
+            if(0 <= le and le < A.size() and A[le] <= A[x]) uni(le,x);
+            if(0 <= ri and ri < A.size() and A[ri] <= A[x]) uni(ri,x);
+            res[x] = cnt[find(x)];
         }
         return res;
     }
