4b8d93676d381244f481b93c97044b966ab3090b
SongHayoung <37806785+SongHayoung@users.noreply.github.com>
2024-05-16 16:06:09 +0900

    Time: 22 ms (99.04%), Space: 30.6 MB (28.60%) - LeetHub

--- 1626-best-team-with-no-conflicts/1626-best-team-with-no-conflicts.cpp
+++ 1626-best-team-with-no-conflicts/1626-best-team-with-no-conflicts.cpp
@@ -1,41 +1,43 @@
-class Solution {
-    int dp[1001];
-    vector<pair<int, int>> vec;
-    int dfs(int& n, int p) {
-        if(p == n) return 0;
-        if(dp[p] != -1) return dp[p];
-        
-        int& res = dp[p] = vec[p].second;
-        
-        for(int i = p + 1; i < n; i++) {
-            if(vec[i].first == vec[p].first) {
-                continue;
-            } else if(vec[i].second >= vec[p].second) {
-                res = max(res, dfs(n,i) + vec[p].second);
-            }
+struct Seg {
+    int mi, ma, val;
+    Seg *left, *right;
+    Seg(vector<int>& A, int l, int r) : mi(A[l]), ma(A[r]), val(0), left(nullptr), right(nullptr) {
+        if(l^r) {
+            int m = l + (r - l) / 2;
+            left = new Seg(A,l,m);
+            right = new Seg(A,m+1,r);
         }
-        
-        for(int i = p + 1; i < n and vec[p].first == vec[i].first; i++) {
-            res = max(res, dfs(n,i) + vec[p].second);
-        }
-        
-        return res;
     }
+    void update(int n, int k) {
+        if(mi <= n and n <= ma) {
+            val = max(val, k);
+            if(left) left->update(n,k);
+            if(right) right->update(n,k);
+        }
+    }
+    int query(int n) {
+        if(mi >= n) return val;
+        if(ma < n) return 0;
+        return max(left->query(n), right->query(n));
+    }
+};
+class Solution {
 public:
     int bestTeamScore(vector<int>& scores, vector<int>& ages) {
-        int n = scores.size();
-        memset(dp,-1,sizeof(dp));
-        for(int i = 0; i < n; i++) {
-            vec.push_back({ages[i], scores[i]});
-        }
-
-        sort(vec.begin(), vec.end());
-
+        vector<int> S = scores;
+        sort(begin(S), end(S));
+        S.erase(unique(begin(S), end(S)), end(S));
+        Seg* seg = new Seg(S,0,S.size() - 1);
+        vector<pair<int,int>> ord;
+        for(int i = 0; i < ages.size(); i++) ord.push_back({ages[i], scores[i]});
+        sort(begin(ord), end(ord));
         int res = 0;
-        for(int i = 0; i < n; i++) {
-            res = max(res, dfs(n,i));
+        while(ord.size()) {
+            auto [_,s] = ord.back(); ord.pop_back();
+            int x = seg->query(s) + s;
+            res = max(res, x);
+            seg->update(s,x);
         }
-
         return res;
     }
 };
