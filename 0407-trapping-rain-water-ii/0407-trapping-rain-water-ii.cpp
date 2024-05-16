65aa68fdf246ef1ae83878ed2b63af8b0c30d8c1
SongHayoung <37806785+SongHayoung@users.noreply.github.com>
2024-05-16 18:49:59 +0900

    Time: 53 ms (46.95%), Space: 16.5 MB (88.33%) - LeetHub

--- 0407-trapping-rain-water-ii/0407-trapping-rain-water-ii.cpp
+++ 0407-trapping-rain-water-ii/0407-trapping-rain-water-ii.cpp
@@ -1,32 +1,34 @@
 class Solution {
+    int mark[202][202];
+    int dy[4]{-1,0,1,0}, dx[4]{0,1,0,-1};
 public:
     int trapRainWater(vector<vector<int>>& A) {
         int n = A.size(), m = A[0].size();
-        priority_queue<array<long long, 3>, vector<array<long long, 3>>, greater<array<long long, 3>>> q;
-        vector<vector<int>> vis(n, vector<int>(m));
-        for(int i = 0; i < n; i++) {
-            vis[i][0] = vis[i][m-1] = true;
-            q.push({A[i][0],i,0});
-            q.push({A[i][m-1],i,m-1});
-        }
-        for(int j = 1; j < m-1; j++) {
-            vis[0][j] = vis[n-1][j] = true;
-            q.push({A[0][j], 0, j});
-            q.push({A[n-1][j], n-1, j});
-        }
+        memset(mark, 0, sizeof mark);
+        priority_queue<array<int,3>,vector<array<int,3>>, greater<>> pq;
+           for(int i = 0; i < n; i++) {
+               pq.push({A[i][0],i,0});
+               pq.push({A[i][m-1],i,m-1});
+               mark[i][0] = mark[i][m-1] = 1;
+            }
+            for(int j = 1; j < m - 1; j++) {
+                pq.push({A[0][j],0,j});
+                pq.push({A[n-1][j],n-1,j});
+                mark[0][j] = mark[n-1][j] = 1;
+            }
         int res = 0;
-        int dy[4]{-1,0,1,0}, dx[4]{0,1,0,-1};
-        while(q.size()) {
-            auto [h,y,x] = q.top(); q.pop();
+        while(pq.size()) {
+            auto [h,y,x] = pq.top(); pq.pop();
             for(int i = 0; i < 4; i++) {
                 int ny = y + dy[i], nx = x + dx[i];
-                if(0 <= ny and ny < n and 0 <= nx and nx < m and !vis[ny][nx]) {
-                    vis[ny][nx] = true;
-                    res += max(0ll, h - A[ny][nx]);
-                    q.push({max(h, 1ll * A[ny][nx]), ny, nx});
+                if(0 <= ny and ny < n and 0 <= nx and nx < m and !mark[ny][nx]) {
+                    res += max(0, h - A[ny][nx]);
+                    mark[ny][nx] = true;
+                    pq.push({max(h,A[ny][nx]), ny,nx});
                 }
             }
         }
+        
         return res;
     }
 };
