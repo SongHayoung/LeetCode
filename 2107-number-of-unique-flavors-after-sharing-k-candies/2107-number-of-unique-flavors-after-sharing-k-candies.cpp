bf8916f7496e76a99e8a84fe0091784d895381d0
SongHayoung <37806785+SongHayoung@users.noreply.github.com>
2024-05-16 16:38:26 +0900

    Time: 104 ms (76.47%), Space: 66.6 MB (70.59%) - LeetHub

--- 2107-number-of-unique-flavors-after-sharing-k-candies/2107-number-of-unique-flavors-after-sharing-k-candies.cpp
+++ 2107-number-of-unique-flavors-after-sharing-k-candies/2107-number-of-unique-flavors-after-sharing-k-candies.cpp
@@ -2,12 +2,14 @@ class Solution {
 public:
     int shareCandies(vector<int>& A, int k) {
         unordered_map<int, int> freq;
-        for(auto& a : A) freq[a]++;
-        int res = 0, now = freq.size();
-        for(int i = 0; i < A.size(); i++) {
-            if(--freq[A[i]] == 0) now -= 1;
-            if(i >= k and ++freq[A[i-k]] == 1) now += 1;
-            if(i >= k - 1) res = max(res, now);
+        for(int i = k; i < A.size(); i++) {
+            freq[A[i]]++;
+        }
+        int res = freq.size();
+        for(int i = k; i < A.size(); i++) {
+            if(--freq[A[i]] == 0) freq.erase(A[i]);
+            freq[A[i-k]]++;
+            res = max(res, (int)freq.size());
         }
         return res;
     }
