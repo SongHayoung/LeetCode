7f0f019e31fa126ea8ab86a7caca24ade289a5f8
SongHayoung <37806785+SongHayoung@users.noreply.github.com>
2024-05-17 08:55:36 +0900

    Time: 3 ms (91.53%), Space: 14.3 MB (22.03%) - LeetHub

---
+++ 1815-maximum-number-of-groups-getting-fresh-donuts/1815-maximum-number-of-groups-getting-fresh-donuts.cpp
@@ -0,0 +1,47 @@
+class Solution {
+    unordered_map<string,int> dp[10];
+    bool fin(vector<int>& A) {
+        for(int i = 0; i < A.size(); i++) if(A[i]) return false;
+        return true;
+    }
+    string key(vector<int>& A) {
+        string res = "";
+        for(int i = 0; i < A.size(); i++) res += to_string(A[i]) + "#";
+        return res;
+    }
+    int helper(vector<int>& A, int sum, int x) {
+        if(fin(A)) return 0;
+        string k = key(A);
+        if(dp[sum].count(k)) return dp[sum][k];
+        int& res = dp[sum][k] = sum == 0;
+        for(int i = 0; i < x; i++) {
+            if(!A[i]) continue;
+            A[i] -= 1;
+            res = max(res, (sum == 0) + helper(A,(sum + i) % x, x));
+            A[i] += 1;
+        }
+        return res;
+    }
+public:
+    int maxHappyGroups(int k, vector<int>& A) {
+        vector<int> buc(k);
+        for(auto& x : A) buc[x%k]++;
+        int res = buc[0]; buc[0] = 0;
+        for(int i = 1, j = k - 1; i <= j; i++,j--) {
+            if(i != j) {
+                int mi = min(buc[i], buc[j]);
+                res += mi;
+                buc[i] -= mi; buc[j] -= mi;
+            } else {
+                int mi = buc[i] / 2;
+                res += mi;
+                buc[i] -= mi * 2;
+            }
+        }
+        for(int i = 0; i < 10; i++) dp[i].clear();
+        if(accumulate(begin(buc), end(buc), 0)) {
+            res += helper(buc,0,k);
+        }
+        return res;
+    }
+};
