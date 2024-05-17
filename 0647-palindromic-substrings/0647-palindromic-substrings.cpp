62e695bbef3bd5da72dd1c90723adf30dad95515
SongHayoung <37806785+SongHayoung@users.noreply.github.com>
2024-05-17 17:02:38 +0900

    Time: 48 ms (21.05%), Space: 25.6 MB (21.39%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.cpp
+++ 0647-palindromic-substrings/0647-palindromic-substrings.cpp
@@ -1,23 +1,17 @@
 class Solution {
 public:
     int countSubstrings(string s) {
-        string ss = "#";
-        for(auto& ch : s) {
-            ss.push_back(ch);
-            ss.push_back('#');
+        queue<pair<int,int>> q;
+        for(int i = 0; i < s.length(); i++) {
+            q.push({i,i});
+            if(i + 1 < s.length() and s[i] == s[i+1]) q.push({i,i+1});
         }
-        int n = ss.length(), res = 0;
-        vector<int> dp(n);
-        int l = 0, r = -1;
-        for(int i = 0; i < n; i++) {
-            dp[i] = max(0, min(r - i, (r + l - i >= 0 ? dp[r + l - i] : -1)));
-            while(i + dp[i] < n and i - dp[i] >= 0 and ss[i-dp[i]] == ss[i+dp[i]])
-                dp[i]++;
-            res += dp[i] / 2;
-            if(i + dp[i] > r) {
-                r = i + dp[i];
-                l = i - dp[i];
-            }
+        int res = 0;
+        while(q.size()) {
+            auto [l,r] = q.front(); q.pop();
+            l -= 1, r += 1;
+            if(0 <= l and r < s.length() and s[l] == s[r]) q.push({l,r});
+            res++;
         }
         return res;
     }
