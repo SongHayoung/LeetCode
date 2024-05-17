2473635dc34153ae1f2821a03d714452b641252a
SongHayoung <37806785+SongHayoung@users.noreply.github.com>
2024-05-17 10:39:51 +0900

    Time: 4 ms (63.78%), Space: 7.6 MB (100.00%) - LeetHub

--- 0940-distinct-subsequences-ii/0940-distinct-subsequences-ii.cpp
+++ 0940-distinct-subsequences-ii/0940-distinct-subsequences-ii.cpp
@@ -1,11 +1,12 @@
 class Solution {
 public:
     int distinctSubseqII(string s) {
-        long dp[27] = {0,}, mod = 1e9+7, res = 0, sum = 0;
+        vector<long long> dp(26);
+        long long mod = 1e9 + 7, sum = 0;
         for(auto& ch : s) {
-            long tot = (1 + sum) % mod;
-            sum = (sum - dp[ch-'a'] + tot + mod) % mod;
-            dp[ch-'a'] = tot;
+            long long cnt = (1 + sum) % mod;
+            sum = (sum - dp[ch-'a'] + cnt + mod) % mod;
+            dp[ch-'a'] = cnt;
         }
         return sum;
     }
