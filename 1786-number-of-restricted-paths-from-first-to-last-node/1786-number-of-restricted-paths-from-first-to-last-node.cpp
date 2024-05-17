418fec639b02b37fd9e1c81108d931acf380184a
SongHayoung <37806785+SongHayoung@users.noreply.github.com>
2024-05-17 13:38:53 +0900

    Time: 394 ms (75.82%), Space: 155.4 MB (73.13%) - LeetHub

--- 1786-number-of-restricted-paths-from-first-to-last-node/1786-number-of-restricted-paths-from-first-to-last-node.cpp
+++ 1786-number-of-restricted-paths-from-first-to-last-node/1786-number-of-restricted-paths-from-first-to-last-node.cpp
@@ -3,41 +3,46 @@ public:
     int countRestrictedPaths(int n, vector<vector<int>>& edges) {
         vector<vector<pair<int,int>>> adj(n);
         for(auto& e : edges) {
-            int u = e[0], v = e[1] , w = e[2];
-            u -= 1 , v -= 1;
+            int u = e[0], v = e[1], w = e[2];
+            u -= 1, v -= 1;
             adj[u].push_back({v,w});
             adj[v].push_back({u,w});
         }
-        vector<int> cost(n,INT_MAX);
-        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>> q;
-        auto push = [&](int u, int c) {
-            if(cost[u] > c) {
-                cost[u] = c;
-                q.push({c,u});
+        vector<int> D(n,INT_MAX);
+        {
+            priority_queue<pair<int,int>,vector<pair<int,int>>, greater<pair<int,int>>> q;
+            auto push = [&](int u, int c) {
+                if(D[u] > c) {
+                    D[u] = c;
+                    q.push({c,u});
+                }
+            };
+            push(n-1,0);
+            while(q.size()) {
+                auto [c,u] = q.top(); q.pop();
+                if(D[u] != c) continue;
+                for(auto& [v,w] : adj[u]) {
+                    push(v,w+c);
+                }
             }
-        };
-        push(n-1,0);
-        while(q.size()) {
-            auto [c,u] = q.top(); q.pop();
-            if(cost[u] != c) continue;
-            for(auto& [v,w] : adj[u]) push(v,c + w);
         }
-        int mod = 1e9 + 7;
-        vector<long> dp(n);
+        long long mod = 1e9 + 7;
+        vector<long long> dp(n);
         dp[0] = 1;
-        vector<int> ord(n);
-        for(int i = 0; i < n; i++) ord[i] = i;
-        sort(begin(ord), end(ord), [&](int i, int j) {
-            return cost[i] > cost[j];
-        });
+        vector<pair<int,int>> ord;
         for(int i = 0; i < n; i++) {
-            int u = ord[i];
+            ord.push_back({D[i],i});
+        }
+        sort(rbegin(ord), rend(ord));
+        for(auto& [_,u] : ord) {
             if(!dp[u]) continue;
-            for(auto& [v,w] : adj[u]) {
-                if(cost[v] >= cost[u]) continue;
+            for(auto& [v,_] : adj[u]) {
+                if(D[v] >= D[u]) continue;
                 dp[v] = (dp[v] + dp[u]) % mod;
             }
         }
+        
+        
         return dp[n-1];
     }
 };
