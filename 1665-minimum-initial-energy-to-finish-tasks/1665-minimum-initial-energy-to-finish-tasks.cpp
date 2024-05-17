4838a39dcd03e69a9b62c47048451191cc5b709f
SongHayoung <37806785+SongHayoung@users.noreply.github.com>
2024-05-17 11:08:10 +0900

    Time: 1893 ms (5.16%), Space: 493.3 MB (5.16%) - LeetHub

--- 1665-minimum-initial-energy-to-finish-tasks/1665-minimum-initial-energy-to-finish-tasks.cpp
+++ 1665-minimum-initial-energy-to-finish-tasks/1665-minimum-initial-energy-to-finish-tasks.cpp
@@ -1,23 +1,18 @@
 class Solution {
 public:
     int minimumEffort(vector<vector<int>>& A) {
-        sort(begin(A), end(A), [](vector<int>&a, vector<int>& b) {
-           int ad = a[1] - a[0], bd = b[1] - b[0];
-            if(ad == bd)
-                return a[1] > b[1];
-            return ad > bd;
+        sort(begin(A), end(A), [](auto a, auto b) {
+            int x = a[1] - a[0], y = b[1] - b[0];
+            if(x == y) return a[1] > b[1];
+            return x > y;
         });
-        
-        int res = 0, cur = 0;
-        for(auto& t : A) {
-            int req = t[1], use = t[0];
-            
-            if(cur < req) {
-                res += (req - cur);
-                cur = req - use;
-            } else {
-                cur = cur - use;
-            }
+        int res = 0, rem = 0;
+        for(auto& a : A) {
+            int use = a[0], req = a[1];
+            if(rem < req) {
+                res += (req - rem);
+                rem = req - use;
+            } else rem = rem - use;
         }
         return res;
     }
