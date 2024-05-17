292f4e6dd325384f80bb52e31b6c43dba7f69757
SongHayoung <37806785+SongHayoung@users.noreply.github.com>
2024-05-17 14:31:22 +0900

    Time: 323 ms (67.86%), Space: 147.4 MB (75.00%) - LeetHub

--- 2655-find-maximal-uncovered-ranges/2655-find-maximal-uncovered-ranges.cpp
+++ 2655-find-maximal-uncovered-ranges/2655-find-maximal-uncovered-ranges.cpp
@@ -1,17 +1,18 @@
 class Solution {
 public:
-    vector<vector<int>> findMaximalUncoveredRanges(int n, vector<vector<int>>& ranges) {
-        ranges.push_back({n,n});
-        ranges.push_back({-1,-1});
-        sort(rbegin(ranges), rend(ranges));
+    vector<vector<int>> findMaximalUncoveredRanges(int n, vector<vector<int>>& A) {
+        A.push_back({n,n});
+        A.push_back({-1,-1});
+        sort(rbegin(A),rend(A));
         vector<vector<int>> res;
-        while(ranges.size()) {
-            int x = ranges.back()[1];
-            while(ranges.size() and ranges.back()[0] <= x + 1) {
-                x = max(x, ranges.back()[1]);
-                ranges.pop_back();
+        while(A.size()) {
+            int x = A.back()[1];
+            while(A.size() and A.back()[0] <= x + 1) {
+                x = max(x, A.back()[1]); A.pop_back();
             }
-            if(ranges.size()) res.push_back({x+1,ranges.back()[0] - 1});
+            if(A.size()) {
+                res.push_back({x+1,A.back()[0]-1});
+            }
         }
         return res;
     }
