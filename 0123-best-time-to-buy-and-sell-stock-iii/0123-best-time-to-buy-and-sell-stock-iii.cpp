2f314bfcd5fc50d29efa9077551a92f070079bcb
SongHayoung <37806785+SongHayoung@users.noreply.github.com>
2024-05-18 18:09:50 +0900

    Time: 96 ms (81.34%), Space: 77.6 MB (96.56%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
@@ -1,15 +1,12 @@
 class Solution {
 public:
-    int maxProfit(vector<int>& prices) {
-        int res = 0;
-        int cheapBuy = INT_MAX;
-        int sellOnce = 0;
-        int buyTwice = INT_MIN;
-        for(auto& p : prices) {
-            res = max(res, buyTwice + p);
-            buyTwice = max(buyTwice, sellOnce - p);
-            sellOnce = max(sellOnce, p - cheapBuy);
-            cheapBuy = min(cheapBuy, p);
+    int maxProfit(vector<int>& A) {
+        long long dp1 = INT_MIN, dp2 = INT_MIN, dp3 = INT_MIN, res = 0;
+        for(auto& a : A) {
+            res = max({res, dp1 + a, dp3 + a});
+            dp3 = max(dp3, dp2 - a);
+            dp2 = max(dp2, dp1 + a);
+            dp1 = max(dp1, -1ll * a);
         }
         return res;
     }
