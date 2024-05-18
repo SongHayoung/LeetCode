38cb13be243abf99a7e12f7792a52768e77a7ee7
SongHayoung <37806785+SongHayoung@users.noreply.github.com>
2024-05-18 21:36:42 +0900

    Time: 92 ms (87.85%), Space: 74.2 MB (95.41%) - LeetHub

--- 2863-maximum-length-of-semi-decreasing-subarrays/2863-maximum-length-of-semi-decreasing-subarrays.cpp
+++ 2863-maximum-length-of-semi-decreasing-subarrays/2863-maximum-length-of-semi-decreasing-subarrays.cpp
@@ -1,15 +1,15 @@
 class Solution {
 public:
     int maxSubarrayLength(vector<int>& A) {
-        vector<int> st;
         int n = A.size(), res = 0;
-        for(int i = n - 1; i >= 0; i--) {
-            if(st.empty() or A[st.back()] > A[i]) st.push_back(i);
+        vector<int> st{n-1};
+        for(int i = n - 2; i >= 0; i--) {
+            if(A[st.back()] > A[i]) st.push_back(i);
         }
-        for(int i = 0, mi = INT_MIN; i < n and st.size(); i++) {
+        for(int i = 0; i < n - 1 and st.size(); i++) {
             while(st.size() and st.back() <= i) st.pop_back();
             while(st.size() and A[i] > A[st.back()]) {
-               res = max(res, st.back() - i + 1);
+                res = max(res, st.back() - i + 1);
                 st.pop_back();
             }
         }
