835c2248d5dd1fb7587920e0a43044e848d410b5
SongHayoung <37806785+SongHayoung@users.noreply.github.com>
2024-05-19 09:24:13 +0900

    Time: 47 ms (92.73%), Space: 20.3 MB (33.64%) - LeetHub

--- 0531-lonely-pixel-i/0531-lonely-pixel-i.cpp
+++ 0531-lonely-pixel-i/0531-lonely-pixel-i.cpp
@@ -1,12 +1,11 @@
 class Solution {
 public:
     int findLonelyPixel(vector<vector<char>>& A) {
-        int n = A.size(), m = A[0].size();
+        int n = A.size(), m = A[0].size(), res = 0;
         vector<int> r(n), c(m);
         for(int i = 0; i < n; i++) for(int j = 0; j < m; j++) {
             if(A[i][j] == 'B') r[i]++,c[j]++;
         }
-        int res = 0;
         for(int i = 0; i < n; i++) for(int j = 0; j < m; j++) {
             if(A[i][j] == 'B') {
                 if(r[i] == 1 and c[j] == 1) res++;
