4549ed9359e153efd0f1f017509284ca54df9b71
SongHayoung <37806785+SongHayoung@users.noreply.github.com>
2024-05-20 07:54:49 +0900

    Time: 0 ms (100.00%), Space: 7.3 MB (60.79%) - LeetHub

---
+++ 0856-score-of-parentheses/0856-score-of-parentheses.cpp
@@ -0,0 +1,23 @@
+class Solution {
+    int helper(string& s, int& p) {
+        int res = 0;
+        while(p < s.length()) {
+            p++;
+            if(s[p] == ')') {
+                res += 1;
+            } else {
+                res += 2 * helper(s,p);
+            }
+            
+                p++;
+                if(p == s.length() or s[p] == ')') break;
+        }
+        
+        return res;
+    }
+public:
+    int scoreOfParentheses(string s) {
+        int p = 0;
+        return helper(s,p);
+    }
+};
