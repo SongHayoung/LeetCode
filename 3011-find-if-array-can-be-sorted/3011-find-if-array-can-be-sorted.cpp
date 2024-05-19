c2aa18c0ec238d98071163e362a1ff32f5e0f6be
SongHayoung <37806785+SongHayoung@users.noreply.github.com>
2024-05-19 21:41:45 +0900

    Time: 11 ms (70.33%), Space: 31.2 MB (48.08%) - LeetHub

--- 3011-find-if-array-can-be-sorted/3011-find-if-array-can-be-sorted.cpp
+++ 3011-find-if-array-can-be-sorted/3011-find-if-array-can-be-sorted.cpp
@@ -4,14 +4,14 @@ public:
         vector<int> S;
         while(A.size()) {
             int pc = __builtin_popcount(A.back());
-            vector<int> X;
+            int ma = INT_MIN, mi = INT_MAX;
             while(A.size() and __builtin_popcount(A.back()) == pc) {
-                X.push_back(A.back()); A.pop_back();
+                mi = min(mi, A.back());
+                ma = max(ma, A.back());
+                A.pop_back();
             }
-            sort(begin(X), end(X));
-            while(X.size()) {
-                S.push_back(X.back()); X.pop_back();
-            }
+            S.push_back(ma);
+            S.push_back(mi);
         }
         
         for(int i = 0; i < S.size() - 1; i++) {
