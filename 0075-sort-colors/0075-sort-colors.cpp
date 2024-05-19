f62c9bdb4bb9d7ed0c4540a786c5b1a302a15be6
SongHayoung <37806785+SongHayoung@users.noreply.github.com>
2024-05-19 17:00:32 +0900

    Time: 0 ms (100.00%), Space: 9.8 MB (32.93%) - LeetHub

---
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    void sortColors(vector<int>& A) {
+        int n = A.size();
+        for(int i = 0, j = 0; i < n; i++) {
+            if(A[i] == 0) {
+                swap(A[i], A[j]);
+                j++;
+            }
+        }
+        for(int i = n - 1, j = n - 1; i >= 0; i--) {
+            if(A[i] == 2) {
+                swap(A[i], A[j]);
+                j--;
+            }
+        }
+    }
+};
