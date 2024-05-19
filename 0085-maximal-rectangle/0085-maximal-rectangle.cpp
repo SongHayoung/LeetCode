13aeed2aeb236ffe1e8fa511d77e68f42cf88874
SongHayoung <37806785+SongHayoung@users.noreply.github.com>
2024-05-19 14:32:18 +0900

    Time: 32 ms (54.14%), Space: 15.7 MB (79.41%) - LeetHub

--- 0085-maximal-rectangle/0085-maximal-rectangle.cpp
+++ 0085-maximal-rectangle/0085-maximal-rectangle.cpp
@@ -1,28 +1,28 @@
 class Solution {
-    int helper(vector<int>& A) {
-        vector<pair<int, int>> st;
+    int helper(vector<int>& A, int n) {
         int res = 0;
-        for(int i = 0; i < A.size(); i++) {
-            int at = i;
+        vector<pair<int,int>> st;
+        for(int i = 0; i < n; i++) {
+            int p = i;
             while(st.size() and st.back().second >= A[i]) {
-                auto [pos, h] = st.back(); st.pop_back();
-                res = max(res, (i - pos) * h);
-                at = pos;
+                auto [at,h] = st.back(); st.pop_back();
+                res = max(res, h * (i - at));
+                p = at;
             }
-            st.push_back({at, A[i]});
+            st.push_back({p,A[i]});
         }
         return res;
     }
 public:
-    int maximalRectangle(vector<vector<char>>& matrix) {
-        vector<int> dp(matrix[0].size() + 1);
-        int res = 0;
-        for(auto& r : matrix) {
-            for(int i = 0; i < r.size(); i++) {
-                if(r[i] == '0') dp[i] = 0;
-                else dp[i]++;
+    int maximalRectangle(vector<vector<char>>& A) {
+        int res = 0, n = A.size(), m = A[0].size();
+        vector<int> dp(m+1);
+        for(auto& a : A) {
+            for(int i = 0; i < m; i++) {
+                if(a[i] == '1') dp[i]++;
+                else dp[i] = 0;
             }
-            res = max(res, helper(dp));
+            res = max(res, helper(dp,m+1));
         }
         return res;
     }
