1469c785c98fe08fc89440b5b6d7b97b685b278a
SongHayoung <37806785+SongHayoung@users.noreply.github.com>
2024-05-19 16:03:07 +0900

    Time: 3 ms (92.92%), Space: 12.7 MB (51.93%) - LeetHub

--- 0256-paint-house/0256-paint-house.cpp
+++ 0256-paint-house/0256-paint-house.cpp
@@ -1,12 +1,14 @@
 class Solution {
 public:
-    int minCost(vector<vector<int>>& costs) {
-        int r = 0, g = 0, b = 0;
-        for(auto& c : costs) {
-            int R = c[0], G = c[1], B = c[2];
-            int nr = min(g,b) + R, ng = min(r,b) + G, nb = min(r,g) + B;
-            r = nr, g = ng, b = nb;
+    int minCost(vector<vector<int>>& A) {
+        vector<int> dp = A[0];
+        for(int i = 1; i < A.size(); i++) {
+            vector<int> dpp(3,INT_MAX);
+            dpp[0] = A[i][0] + min(dp[1], dp[2]);
+            dpp[1] = A[i][1] + min(dp[0], dp[2]);
+            dpp[2] = A[i][2] + min(dp[0], dp[1]);
+            swap(dp,dpp);
         }
-        return min({r,g,b});
+        return *min_element(begin(dp),end(dp));
     }
 };
