a3fdc51faa88775494729c6f2fdadd766675b06e
SongHayoung <37806785+SongHayoung@users.noreply.github.com>
2024-05-19 17:00:39 +0900

    Time: 4 ms (30.57%), Space: 9.8 MB (32.93%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -4,14 +4,12 @@ public:
         int n = A.size();
         for(int i = 0, j = 0; i < n; i++) {
             if(A[i] == 0) {
-                swap(A[i], A[j]);
-                j++;
+                swap(A[i], A[j++]);
             }
         }
         for(int i = n - 1, j = n - 1; i >= 0; i--) {
             if(A[i] == 2) {
-                swap(A[i], A[j]);
-                j--;
+                swap(A[i], A[j--]);
             }
         }
     }
