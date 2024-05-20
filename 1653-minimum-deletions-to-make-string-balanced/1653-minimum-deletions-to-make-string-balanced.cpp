9160bd3bf374da8ac1b25795805e192207e20ef6
SongHayoung <37806785+SongHayoung@users.noreply.github.com>
2024-05-20 10:29:53 +0900

    Time: 83 ms (74.20%), Space: 23.4 MB (68.62%) - LeetHub

--- 1653-minimum-deletions-to-make-string-balanced/1653-minimum-deletions-to-make-string-balanced.cpp
+++ 1653-minimum-deletions-to-make-string-balanced/1653-minimum-deletions-to-make-string-balanced.cpp
@@ -1,17 +1,11 @@
 class Solution {
 public:
     int minimumDeletions(string s) {
-        int n = s.size(), res = INT_MAX, a = count(begin(s), end(s),'a'), pb = 0, pa = 0, b = n - a;
+        int n = s.size(), res = INT_MAX, a = count(begin(s), end(s),'a'), pb = 0;
         for(int i = 0; i < n; i++) {
-            if(s[i] == 'a') {
-                a--;
-                res = min(res, pb + a);
-                pa++;
-            } else {
-                res = min(res, pb + a);
-                pb++;
-                b--;
-            }
+            if(s[i] == 'a') a--;
+            res = min(res, pb + a);
+            if(s[i] == 'b') pb++;
         }
         return res;
     }
