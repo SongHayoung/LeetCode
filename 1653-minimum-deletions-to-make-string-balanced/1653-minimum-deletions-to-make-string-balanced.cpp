20c53a3720e31c1142f9059a942e993ee1cf9bbf
SongHayoung <37806785+SongHayoung@users.noreply.github.com>
2024-05-20 10:29:18 +0900

    Time: 85 ms (71.54%), Space: 23.4 MB (75.80%) - LeetHub

--- 1653-minimum-deletions-to-make-string-balanced/1653-minimum-deletions-to-make-string-balanced.cpp
+++ 1653-minimum-deletions-to-make-string-balanced/1653-minimum-deletions-to-make-string-balanced.cpp
@@ -1,15 +1,17 @@
 class Solution {
 public:
     int minimumDeletions(string s) {
-        int n = s.length(), res = INT_MAX;
-        vector<int> B;
-        for(int i = 0, a = 0, b = 0; i < n; i++) {
-            if(s[i] == 'b') b++;
-            B.push_back(b);
-        }
-        for(int i = n - 1, a = 0; i >= 0; i--) {
-            res = min(res, B[i] + a - (s[i] == 'b'));
-            if(s[i] == 'a') a++;
+        int n = s.size(), res = INT_MAX, a = count(begin(s), end(s),'a'), pb = 0, pa = 0, b = n - a;
+        for(int i = 0; i < n; i++) {
+            if(s[i] == 'a') {
+                a--;
+                res = min(res, pb + a);
+                pa++;
+            } else {
+                res = min(res, pb + a);
+                pb++;
+                b--;
+            }
         }
         return res;
     }
