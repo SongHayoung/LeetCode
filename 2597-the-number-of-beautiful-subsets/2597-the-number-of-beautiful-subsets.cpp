80baba65a196102803732a4e14db8d0551716b61
SongHayoung <37806785+SongHayoung@users.noreply.github.com>
2024-05-23 09:19:04 +0900

    Time: 600 ms (37.40%), Space: 35.9 MB (90.91%) - LeetHub

--- 2597-the-number-of-beautiful-subsets/2597-the-number-of-beautiful-subsets.cpp
+++ 2597-the-number-of-beautiful-subsets/2597-the-number-of-beautiful-subsets.cpp
@@ -1,27 +1,27 @@
 class Solution {
-    int dp[22];
+    bool bit(int x, int p) {
+        return (x>>p) & 1;
+    }
 public:
     int beautifulSubsets(vector<int>& A, int k) {
-        unordered_map<int, vector<int>> mp;
-        for(auto a : A) mp[a%k].push_back(a);
-        dp[0] = 1, dp[1] = 2;
-        for(int i = 2; i < 22; i++) dp[i] = dp[i-1] + dp[i-2];
-        int res = 1;
-        for(auto [_,vec] : mp) {
-            if(vec.size() == 1) res = res * 2;
-            else {
-                sort(begin(vec), end(vec));
-                vector<int> now{vec[0]};
-                for(int i = 1; i < vec.size(); i++) {
-                    if(now.back() + k == vec[i]) now.push_back(vec[i]);
-                    else {
-                        res *= dp[now.size()];
-                        now = {vec[i]};
-                    }
-                }
-                res *= dp[now.size()];
+        sort(begin(A), end(A));
+        int n = A.size(), res = 0;
+        vector<int> bad;
+        for(int i = 0; i < n; i++) {
+            int mask = 0;
+            for(int j = 0; j < n; j++) {
+                if(abs(A[i] - A[j]) == k) mask ^= 1<<j;
             }
+            bad.push_back(mask);
         }
-        return res - 1;
+        for(int i = 1; i < 1<<n; i++) {
+            bool ok = true;
+            for(int j = 0; j < n and ok; j++) {
+                if(!bit(i,j)) continue;
+                if(i & bad[j]) ok = false;
+            }
+            if(ok) res++;
+        }
+        return res;
     }
 };
