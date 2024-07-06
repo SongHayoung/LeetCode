500652a9f8b2adafbab715083a7a71e1dfb8b1a1
SongHayoung <37806785+SongHayoung@users.noreply.github.com>
2024-07-07 01:01:54 +0900

    Time: 11 ms (40%), Space: 25.8 MB (60%) - LeetHub

---
+++ 3463-alternating-groups-i/3463-alternating-groups-i.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int numberOfAlternatingGroups(vector<int>& A) {
+        int res = 0, n = A.size();
+        for(int i = 0; i < n; i++) {
+            if(A[i] != A[(i+1)%n]) {
+                if(A[i] == A[(i+2)%n]) res++;
+            }
+        }
+        return res;
+    }
+};
