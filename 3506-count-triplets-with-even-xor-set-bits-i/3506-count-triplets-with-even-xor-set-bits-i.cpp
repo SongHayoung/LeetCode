e4e7be643b6237e7e9608e18fb26b38fbc8c37f6
SongHayoung <37806785+SongHayoung@users.noreply.github.com>
2024-07-07 00:52:04 +0900

    Time: 15 ms (77.01%), Space: 47.2 MB (8.05%) - LeetHub

--- 3506-count-triplets-with-even-xor-set-bits-i/3506-count-triplets-with-even-xor-set-bits-i.cpp
+++ 3506-count-triplets-with-even-xor-set-bits-i/3506-count-triplets-with-even-xor-set-bits-i.cpp
@@ -1,17 +1,16 @@
 class Solution {
 public:
     int tripletCount(vector<int>& a, vector<int>& b, vector<int>& c) {
+        vector<vector<int>> bits(3,vector<int>(2));
+        for(auto& x : a) bits[0][__builtin_popcount(x) % 2]++;
+        for(auto& x : b) bits[1][__builtin_popcount(x) % 2]++;
+        for(auto& x : c) bits[2][__builtin_popcount(x) % 2]++;
         int res = 0;
-        unordered_map<int,int> ab;
-        for(auto& x : a) {
-            for(auto& y : b) {
-                ab[x^y]++;
-            }
-        }
-        for(auto& z : c) {
-            for(auto& [k,v] : ab) {
-                int pc = __builtin_popcountll(k ^ z);
-                if(pc % 2 == 0) res += v;
+        for(int i = 0; i < 2; i++) {
+            for(int j = 0; j < 2; j++) {
+                for(int k = 0; k < 2; k++) {
+                    if(1 ^ i ^ j ^ k) res += bits[0][i] * bits[1][j] * bits[2][k];
+                }
             }
         }
         return res;
