4ef5dd35333f3b5595a6c9049d86326b288945c6
SongHayoung <37806785+SongHayoung@users.noreply.github.com>
2024-07-07 00:52:26 +0900

    Time: 13 ms (78.16%), Space: 47.2 MB (8.05%) - LeetHub

--- 3506-count-triplets-with-even-xor-set-bits-i/3506-count-triplets-with-even-xor-set-bits-i.cpp
+++ 3506-count-triplets-with-even-xor-set-bits-i/3506-count-triplets-with-even-xor-set-bits-i.cpp
@@ -9,7 +9,7 @@ public:
         for(int i = 0; i < 2; i++) {
             for(int j = 0; j < 2; j++) {
                 for(int k = 0; k < 2; k++) {
-                    if(1 ^ i ^ j ^ k) res += bits[0][i] * bits[1][j] * bits[2][k];
+                    if(!i ^ j ^ k) res += bits[0][i] * bits[1][j] * bits[2][k];
                 }
             }
         }
