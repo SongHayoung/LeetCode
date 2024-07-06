e4909702074c55bdc46762fa6b09195ad0de5bcf
SongHayoung <37806785+SongHayoung@users.noreply.github.com>
2024-07-07 01:11:53 +0900

    Time: 112 ms (50%), Space: 75 MB (83.33%) - LeetHub

--- 3264-maximum-points-after-enemy-battles/3264-maximum-points-after-enemy-battles.cpp
+++ 3264-maximum-points-after-enemy-battles/3264-maximum-points-after-enemy-battles.cpp
@@ -1,15 +1,12 @@
 class Solution {
 public:
     long long maximumPoints(vector<int>& A, int k) {
-        long long res = 0;
-        sort(begin(A), end(A));
-        if(A[0] > k) return 0;
-        while(A.size()) {
-            long long x = A.back();
-            res += k / A[0];
-            k = k % A[0] + x;
-            A.pop_back();
+        long long res = 0, tot = k;
+        int mi = INT_MAX;
+        for(auto& x : A) {
+            mi = min(mi, x);
+            tot += x;
         }
-        return res;
+        return k < mi ? 0 : (tot - mi) / mi;
     }
 };
