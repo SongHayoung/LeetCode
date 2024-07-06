cc7377bc1dd52798a871d93ae069ae0a051795f7
SongHayoung <37806785+SongHayoung@users.noreply.github.com>
2024-07-07 00:49:37 +0900

    Time: 29 ms (66.67%), Space: 50.6 MB (5.75%) - LeetHub

--- 3506-count-triplets-with-even-xor-set-bits-i/3506-count-triplets-with-even-xor-set-bits-i.cpp
+++ 3506-count-triplets-with-even-xor-set-bits-i/3506-count-triplets-with-even-xor-set-bits-i.cpp
@@ -2,12 +2,16 @@ class Solution {
 public:
     int tripletCount(vector<int>& a, vector<int>& b, vector<int>& c) {
         int res = 0;
+        unordered_map<int,int> ab;
         for(auto& x : a) {
             for(auto& y : b) {
-                for(auto& z : c) {
-                    int bit = x ^ y ^ z;
-                    if(__builtin_popcountll(bit) % 2 == 0) res++;
-                }
+                ab[x^y]++;
+            }
+        }
+        for(auto& z : c) {
+            for(auto& [k,v] : ab) {
+                int pc = __builtin_popcountll(k ^ z);
+                if(pc % 2 == 0) res += v;
             }
         }
         return res;
