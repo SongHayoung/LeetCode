d8365fcdbd9e9c1f42c6da8ec7ead924130cd9c6
SongHayoung <37806785+SongHayoung@users.noreply.github.com>
2024-07-10 09:02:34 +0900

    Time: 6 ms (40.12%), Space: 14 MB (61.11%) - LeetHub

--- 1720-crawler-log-folder/1720-crawler-log-folder.cpp
+++ 1720-crawler-log-folder/1720-crawler-log-folder.cpp
@@ -4,8 +4,7 @@ public:
         int res = 0;
         for(auto& log : logs) {
             if(log == "../") res = max(0, res - 1);
-            else if(log == "./") continue;
-            else res++;
+            else if(log != "./") res++;
         }
         return res;
     }
