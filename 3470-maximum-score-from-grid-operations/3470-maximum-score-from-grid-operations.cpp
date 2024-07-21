e601e2884b76b5067862aff3863de5e9d327bd74
SongHayoung <37806785+SongHayoung@users.noreply.github.com>
2024-07-21 12:52:11 +0900

    Time: 2726 ms (16.67%), Space: 43 MB (100%) - LeetHub

--- 3470-maximum-score-from-grid-operations/3470-maximum-score-from-grid-operations.cpp
+++ 3470-maximum-score-from-grid-operations/3470-maximum-score-from-grid-operations.cpp
@@ -1,6 +1,4 @@
 
-
-
 long long dp[102][102][102];
 long long pre[102][102];
 class Solution {
@@ -11,22 +9,23 @@ public:
         if(n == 1) return 0;
 
         for(int i = 0; i < n; i++) for(int j = 0; j < n; j++) {
-            pre[i+1][j] = pre[i][j] + grid[i][j];
-        }
+                pre[i+1][j] = pre[i][j] + grid[i][j];
+            }
+        long long op = 0;
         for(int i = 0; i <= n; i++) dp[1][0][i] = 0;
         for(int col = 1; col < n; col++) {
             for(int x = 0; x <= n; x++) {
                 for(int y = 0; y <= n; y++) {
                     if(dp[col][x][y] == -1) continue;
-                    int t = max(x,y);
-                    for(int k = t + 1; k <= n; k++) {
-                        dp[col+1][y][k] = max(dp[col+1][y][k], dp[col][x][y] + pre[k][col-1] - pre[t][col-1]);
+                    int ma = max(x,y);
+                    for(int z = 0; z < y; z++) {
+                        dp[col+1][y][z] = max(dp[col+1][y][z], dp[col][x][y] + pre[y][col] - pre[z][col]);
                     }
-                    for(int k = 0; k < y; k++) {
-                        dp[col+1][y][k] = max(dp[col+1][y][k], dp[col][x][y] + pre[y][col] - pre[k][col]);
+                    for(int z = y; z <= ma; z++) {
+                          dp[col+1][y][z] = max(dp[col+1][y][z], dp[col][x][y]);
                     }
-                    for(int k = y; k <= t; k++) {
-                        dp[col+1][y][k] = max(dp[col+1][y][k], dp[col][x][y]);
+                    for(int z = ma + 1; z <= n; z++) {
+                        dp[col+1][y][z] = max(dp[col+1][y][z], dp[col][x][y] + pre[z][col-1] - pre[ma][col-1]);
                     }
                 }
             }
