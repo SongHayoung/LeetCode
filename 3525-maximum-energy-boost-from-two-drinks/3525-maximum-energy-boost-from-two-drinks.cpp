1518fa06ef0dceb74c284a48f504089c12a31498
SongHayoung <37806785+SongHayoung@users.noreply.github.com>
2024-08-18 19:35:29 +0900

    Time: 312 ms (9.09%), Space: 216.5 MB (9.09%) - LeetHub

--- 3525-maximum-energy-boost-from-two-drinks/3525-maximum-energy-boost-from-two-drinks.cpp
+++ 3525-maximum-energy-boost-from-two-drinks/3525-maximum-energy-boost-from-two-drinks.cpp
@@ -1,22 +1,24 @@
 class Solution {
 public:
-    long long maxProfit(vector<int>& profitsA, vector<int>& profitsB) {
-        int n = profitsA.size();
-        if (n == 0) return 0;
-
-        vector<long long> dpA(n, 0);
-        vector<long long> dpB(n, 0);
-
-        // Initial values for day 0
-        dpA[0] = profitsA[0];
-        dpB[0] = profitsB[0];
-
-        for (int i = 1; i < n; ++i) {
-            dpA[i] = max(dpA[i-1] + profitsA[i], (i-2 >= 0 ? dpB[i-2] : 0) + profitsA[i]);
-            dpB[i] = max(dpB[i-1] + profitsB[i], (i-2 >= 0 ? dpA[i-2] : 0) + profitsB[i]);
+    long long maxEnergyBoost(vector<int>& profitsA, vector<int>& profitsB) {
+         int n = profitsA.size();
+        deque<long long> dpA(3), dpB(3);
+        for(int i = 0; i < n; i++) {
+            dpA[0] += profitsA[i];
+            dpB[0] += profitsB[i];
+            if(i + 2 < n) {
+                dpB[2] = dpA[0];
+                dpA[2] = dpB[0];
+            }
+            if(i + 1 < n) {
+                dpA[1] = max(dpA[1], dpA[0]);
+                dpB[1] = max(dpB[1], dpB[0]);
+                dpA.push_back(0);
+                dpB.push_back(0);
+                dpA.pop_front();
+                dpB.pop_front();
+            }
         }
-
-        // The maximum profit we can get on the last day
-        return max(dpA[n-1], dpB[n-1]);
+        return max(dpA[0],dpB[0]);
     }
 };
