a94c03d441d29fb30c63a51dca86825fd88291ca
SongHayoung <37806785+SongHayoung@users.noreply.github.com>
2024-08-31 20:32:15 +0900

    Time: 68 ms (20%), Space: 64.5 MB (100%) - LeetHub

--- 3586-constructing-two-increasing-arrays/3586-constructing-two-increasing-arrays.cpp
+++ 3586-constructing-two-increasing-arrays/3586-constructing-two-increasing-arrays.cpp
@@ -4,21 +4,16 @@ public:
         int n = A.size(), m = B.size();
         vector<vector<int>> dp(n + 1, vector<int>(m + 1, (n + 1) * (m + 1) * 2));
         dp[0][0] = 0;
-        auto helper = [&](int x, int fl) {
-            if(x % 2 == fl % 2) return x + 2;
-            return x + 1;
-        };
         for(int i = 0; i <= n; i++) {
             for(int j = 0; j <= m; j++) {
                 if(i < n) {
-                    dp[i+1][j] = min(dp[i+1][j], helper(dp[i][j], A[i]));
+                    dp[i+1][j] = min(dp[i+1][j], dp[i][j] + ((A[i] ^ dp[i][j]) & 1 ? 1 : 2));
                 }
                 if(j < m) {
-                    dp[i][j+1] = min(dp[i][j+1], helper(dp[i][j], B[j]));
+                    dp[i][j+1] = min(dp[i][j+1], dp[i][j] + ((B[j] ^ dp[i][j]) & 1 ? 1 : 2));
                 }
             }
         }
         return dp[n][m];
     }
 };
-
