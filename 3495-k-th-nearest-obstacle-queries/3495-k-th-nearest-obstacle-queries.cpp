0a37915067e65a236922f4744a8cd51016174822
SongHayoung <37806785+SongHayoung@users.noreply.github.com>
2024-09-01 13:24:51 +0900

    Time: 576 ms (100%), Space: 256.7 MB (100%) - LeetHub

--- 3495-k-th-nearest-obstacle-queries/3495-k-th-nearest-obstacle-queries.cpp
+++ 3495-k-th-nearest-obstacle-queries/3495-k-th-nearest-obstacle-queries.cpp
@@ -1,6 +1,13 @@
 class Solution {
 public:
     vector<int> resultsArray(vector<vector<int>>& queries, int k) {
-        
+        priority_queue<int> q;
+        vector<int> res;
+        for(auto& qry : queries) {
+            q.push(abs(qry[0]) + abs(qry[1]));
+            while(q.size() > k) q.pop();
+            res.push_back(q.size() < k ? -1 : q.top());
+        }
+        return res;
     }
 };
