c2a76553555ea2adc35d660351c2fa0604971055
SongHayoung <37806785+SongHayoung@users.noreply.github.com>
2024-09-01 13:21:40 +0900

    Time: 1264 ms (27.27%), Space: 306.4 MB (90.91%) - LeetHub

--- 3551-maximum-xor-score-subarray-queries/3551-maximum-xor-score-subarray-queries.cpp
+++ 3551-maximum-xor-score-subarray-queries/3551-maximum-xor-score-subarray-queries.cpp
@@ -1,42 +1,21 @@
-const maxN = 1010
-
-var dp [maxN][maxN]int
-var dpp [maxN][maxN]int
-
-func max(a, b int) int {
-    if a > b {
-        return a
+int dp[2020][2020], dpp[2020][2020];
+class Solution {
+    int helper(vector<int>& A, int l, int r) {
+        if(dp[l][r] != -1) return dp[l][r];
+        if(l == r) return dp[l][r] = dpp[l][r] = A[l];
+        int& res = dp[l][r] = max({helper(A,l+1,r), helper(A,l,r-1)});
+        dpp[l][r] = dpp[l+1][r] ^ dpp[l][r-1];
+        res = max(res, dpp[l][r]);
+        return res;
     }
-    return b
-}
-
-func helper(A []int, l, r int) int {
-    if dp[l][r] != -1 {
-        return dp[l][r]
-    }
-    if l == r {
-        dp[l][r] = A[l]
-        dpp[l][r] = A[l]
-        return dp[l][r]
-    }
-    res := max(helper(A, l+1, r), helper(A, l, r-1))
-    dp[l][r] = res
-    dpp[l][r] = dpp[l+1][r] ^ dpp[l][r-1]
-    dp[l][r] = max(res, dpp[l][r])
-    return dp[l][r]
-}
-
-func maximumSubarrayXor(nums []int, queries [][]int) []int {
-    for i := range dp {
-        for j := range dp[i] {
-            dp[i][j] = -1
-            dpp[i][j] = -1
+public:
+    vector<int> maximumSubarrayXor(vector<int>& nums, vector<vector<int>>& queries) {
+        memset(dp,-1,sizeof dp);
+        memset(dpp,-1,sizeof dpp);
+        vector<int> res;
+        for(auto& q : queries) {
+            res.push_back(helper(nums,q[0],q[1]));
         }
+        return res;
     }
-
-    var res []int
-    for _, q := range queries {
-        res = append(res, helper(nums, q[0], q[1]))
-    }
-    return res
-}
+};
