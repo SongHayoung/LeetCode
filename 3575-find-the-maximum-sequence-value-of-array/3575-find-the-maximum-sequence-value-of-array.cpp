e9fdd9414971abd598a7d73314430bdd8af3455e
SongHayoung <37806785+SongHayoung@users.noreply.github.com>
2024-09-15 10:55:09 +0900

    Time: 221 ms (63.16%), Space: 66.6 MB (63.16%) - LeetHub

--- 3575-find-the-maximum-sequence-value-of-array/3575-find-the-maximum-sequence-value-of-array.cpp
+++ 3575-find-the-maximum-sequence-value-of-array/3575-find-the-maximum-sequence-value-of-array.cpp
@@ -1,17 +1,7 @@
 class Solution {
     int findMaximumXOR(unordered_set<int>& a, unordered_set<int>& b) {
-        if(a.size() == 0 or b.size() == 0) return 0;
-        int res = 0, mask = 0;
-        int bits[128]{};
-        for(int i = 6; i >= 0; i--) {
-            mask |= 1<<i;
-            int t = res | (1<<i);
-            for(auto& x : a) bits[x & mask] = i + 1;
-            for(auto& x : b) if(bits[(x & mask) ^ t] == i + 1) {
-                res = t;
-                break;
-            }
-        }
+        int res = 0;
+        for(auto& x : a) for(auto& y : b) res = max(res, x ^ y);
         return res;
     }
 public:
