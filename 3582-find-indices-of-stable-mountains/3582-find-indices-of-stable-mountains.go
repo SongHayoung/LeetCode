ee4fde706c80a98f49dd3cf54dc5da0c493586bb
SongHayoung <37806785+SongHayoung@users.noreply.github.com>
2024-09-15 10:35:37 +0900

    Time: 0 ms (100%), Space: 3.1 MB (100%) - LeetHub

---
+++ 3582-find-indices-of-stable-mountains/3582-find-indices-of-stable-mountains.go
@@ -0,0 +1,11 @@
+package main
+
+func stableMountains(height []int, threshold int) []int {
+	res := []int{}
+	for i := 1; i < len(height); i++ {
+		if height[i-1] > threshold {
+			res = append(res, i)
+		}
+	}
+	return res
+}
