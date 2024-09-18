afc99e3b135a3bee36c64cc38268f6ff091704e6
SongHayoung <37806785+SongHayoung@users.noreply.github.com>
2024-09-18 10:45:45 +0900

    Time: 18 ms (31.41%), Space: 19.7 MB (30.59%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.cpp
+++ 0045-jump-game-ii/0045-jump-game-ii.cpp
@@ -1,14 +1,16 @@
 class Solution {
 public:
-    int jump(vector<int>& nums) {
-        int cur = 0, res = 0, end = 0;
-        for(int i = 0; cur < nums.size() - 1; i++) {
-            end = max(end, i + nums[i]);
-            if(cur == i) {
-                res++;
-                cur = end;
-            }
+    int jump(vector<int>& station) {
+        deque<pair<int,int>> cost{{1,station[0]}};
+        int n = station.size();
+        if(cost[0].second >= n - 1) return n != 1;
+        for(int i = 1; i < n; i++) {
+            while(cost.size() and cost[0].second < i) cost.pop_front();
+            if(cost.size() == 0) return -1;
+            int until = i + station[i], weight = cost[0].first + 1;
+            if(until >= n - 1) return weight;
+            if(cost.back().second < until) cost.push_back({weight, until});
         }
-        return res;
+        return 0;
     }
 };
