87b419201ccf8957ffcd3cd499d03ef31bb198f4
SongHayoung <37806785+SongHayoung@users.noreply.github.com>
2024-09-18 09:46:51 +0900

    Time: 23 ms (93.77%), Space: 17.1 MB (89.01%) - LeetHub

--- 0085-maximal-rectangle/0085-maximal-rectangle.cpp
+++ 0085-maximal-rectangle/0085-maximal-rectangle.cpp
@@ -1,29 +1,36 @@
 class Solution {
-    int helper(vector<int>& A, int n) {
-        int res = 0;
+    int helper(vector<int>& A) {
+        int res = 0, n = A.size();
         vector<pair<int,int>> st;
         for(int i = 0; i < n; i++) {
-            int p = i;
-            while(st.size() and st.back().second >= A[i]) {
-                auto [at,h] = st.back(); st.pop_back();
-                res = max(res, h * (i - at));
-                p = at;
+            int h = A[i], pos = i;
+            while(st.size() and st.back().first >= h) {
+                res = max(res, st.back().first * (i - st.back().second));
+                pos = st.back().second;
+                st.pop_back();
             }
-            st.push_back({p,A[i]});
+            st.push_back({h,pos});
         }
+        for(int i = 0; i < st.size(); i++) res = max(res, st[i].first * (n - st[i].second));
         return res;
     }
 public:
-    int maximalRectangle(vector<vector<char>>& A) {
-        int res = 0, n = A.size(), m = A[0].size();
-        vector<int> dp(m+1);
-        for(auto& a : A) {
-            for(int i = 0; i < m; i++) {
-                if(a[i] == '1') dp[i]++;
-                else dp[i] = 0;
+    int mostComplexTerrain(vector<vector<int>>& terrain) {
+        int n = terrain.size(), m = terrain[0].size(), res = 0;
+        vector<int> dp(m);
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < m; j++) {
+                if(terrain[i][j]) dp[j]++;
+                else dp[j] = 0;
             }
-            res = max(res, helper(dp,m+1));
+            res = max(res, helper(dp));
         }
         return res;
+    }
+    int maximalRectangle(vector<vector<char>>& matrix) {
+        int n = matrix.size(), m = matrix[0].size();
+        vector<vector<int>> mat(n,vector<int>(m));
+        for(int i = 0; i < n; i++) for(int j = 0; j < m; j++) mat[i][j] = matrix[i][j] - '0';
+        return mostComplexTerrain(mat);
     }
 };
