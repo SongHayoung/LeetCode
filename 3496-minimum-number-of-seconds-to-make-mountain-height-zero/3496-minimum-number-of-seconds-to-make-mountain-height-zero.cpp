a19acbcd9ea33ce1c98c16172075b23c13f99d0e
SongHayoung <37806785+SongHayoung@users.noreply.github.com>
2024-09-22 13:26:54 +0900

    Time: 79 ms (100%), Space: 27.9 MB (100%) - LeetHub

---
+++ 3496-minimum-number-of-seconds-to-make-mountain-height-zero/3496-minimum-number-of-seconds-to-make-mountain-height-zero.cpp
@@ -0,0 +1,28 @@
+
+class Solution {
+    bool helper(int mountainHeight, long long k, vector<int>& workerTimes) {
+        for(auto& t : workerTimes) {
+            long long x = t, lim = k;
+            while(mountainHeight and lim >= x) {
+                lim -= x;
+                mountainHeight--;
+                x += t;
+            }
+            if(mountainHeight == 0) return true;
+        }
+        return false;
+    }
+public:
+    long long minNumberOfSeconds(int mountainHeight, vector<int>& workerTimes) {
+        long long l = 0, r = 1e18, res = r;
+        while(l <= r) {
+            long long m = l + (r - l) / 2;
+            bool ok = helper(mountainHeight, m, workerTimes);
+            if(ok) {
+                res = m;
+                r = m - 1;
+            } else l = m + 1;
+        }
+        return res;
+    }
+};
