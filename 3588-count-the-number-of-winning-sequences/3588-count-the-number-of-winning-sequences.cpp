06b392271db55fb7b0a96f9be4c124ed9e61fd2f
SongHayoung <37806785+SongHayoung@users.noreply.github.com>
2024-10-13 13:06:19 +0900

    Time: 1804 ms (16.67%), Space: 501.2 MB (8.33%) - LeetHub

--- 3588-count-the-number-of-winning-sequences/3588-count-the-number-of-winning-sequences.cpp
+++ 3588-count-the-number-of-winning-sequences/3588-count-the-number-of-winning-sequences.cpp
@@ -1,39 +1,39 @@
-
 class Solution {
     long long mod = 1e9 + 7;
 public:
     int countWinningSequences(string s) {
-        vector<vector<long long>> dp(3, vector<long long>(2020));
-        if(s[0] == 'R') {
-            dp[0][1010] = dp[1][1009] = dp[2][1011] = 1;
+        vector<unordered_map<long long, long long>> dp(3);
+        if(s[0] == 'F') {
+            dp[0][0] = dp[1][-1] = dp[2][1] = 1;
         }
-        if(s[0] == 'S') {
-            dp[1][1010] = dp[2][1009] = dp[0][1011] = 1;
+        if(s[0] == 'E') {
+            dp[1][0] = dp[2][-1] = dp[0][1] = 1;
         }
-        if(s[0] == 'P') {
-            dp[2][1010] = dp[0][1009] = dp[1][1011] = 1;
+        if(s[0] == 'W') {
+            dp[2][0] = dp[0][-1] = dp[1][1] = 1;
         }
         for(int i = 1; i < s.length(); i++) {
-            vector<vector<long long>> dpp(3, vector<long long>(2020));
+            vector<unordered_map<long long, long long>> dpp(3);
             vector<long long> p(3);
-            if(s[i] == 'R') p[2] = 1, p[1] = -1;
-            if(s[i] == 'S') p[0] = 1, p[2] = -1;
-            if(s[i] == 'P') p[1] = 1, p[0] = -1;
-            for(int j = 0; j < 2020; j++) {
-                for(int x = 0; x < 3; x++) {
-                    if(!dp[x][j]) continue;
-                    for(int y = 0; y < 3; y++) {
-                        if(x == y) continue;
-                        long long pp = j + p[y];
-                        dpp[y][pp] = (dpp[y][pp] + dp[x][j]) % mod;
+            if(s[i] == 'F') p[2] = 1, p[1] = -1;
+            if(s[i] == 'E') p[0] = 1, p[2] = -1;
+            if(s[i] == 'W') p[1] = 1, p[0] = -1;
+            for(int x = 0; x < 3; x++) {
+                for(int y = 0; y < 3; y++) {
+                    if(x == y) continue;
+                    for(auto& [k,v] : dp[x]) {
+                        long long nk = k + p[y];
+                        dpp[y][nk] = (dpp[y][nk] + v) % mod;
                     }
                 }
             }
             swap(dp,dpp);
         }
         long long res = 0;
-        for(int i =1011; i < 2020; i++) {
-            res = (res + dp[0][i] + dp[1][i] + dp[2][i]) % mod;
+        for(int i = 0; i < 3; i++) {
+            for(auto& [k,v] : dp[i]) {
+                if(k > 0) res = (res + v) % mod;
+            }
         }
         return res;
     }
