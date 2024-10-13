bd9cae8b88fec3ff0118511d6ac31ee160e5f658
SongHayoung <37806785+SongHayoung@users.noreply.github.com>
2024-10-13 13:20:45 +0900

    Time: 1304 ms (60%), Space: 302.4 MB (60%) - LeetHub

--- 3592-find-x-sum-of-all-k-long-subarrays-ii/3592-find-x-sum-of-all-k-long-subarrays-ii.cpp
+++ 3592-find-x-sum-of-all-k-long-subarrays-ii/3592-find-x-sum-of-all-k-long-subarrays-ii.cpp
@@ -1,70 +1,186 @@
-struct Helper {
-    long long lim, sum;
+struct Node {
+    Node *l, *r, *p;
+    pair<long long, long long> key;
+    long long sum, subset;
+    Node(pair<long long, long long> key, Node* p) : key(key), sum(key.first * key.second), subset(1), l(nullptr), r(nullptr), p(p) {}
+    void update() {
+        subset = 1;
+        sum = key.first * key.second;
+        if(l) subset += l->subset, sum += l->sum;
+        if(r) subset += r->subset, sum += r->sum;
+    }
+};
+
+struct SplayTree {
+    Node* tree;
+    SplayTree() : tree(nullptr) {
+        insert({INT_MIN, 0});
+        insert({INT_MAX, 0});
+    }
+
+    void rotate(Node *x) {
+        Node *p = x->p, *b;
+        if (x == p->l) {
+            p->l = b = x->r;
+            x->r = p;
+        } else {
+            p->r = b = x->l;
+            x->l = p;
+        }
+        x->p = p->p;
+        p->p = x;
+        if (b) b->p = p;
+        (x->p ? p == x->p->l ? x->p->l : x->p->r : tree) = x;
+        p->update();
+        x->update();
+    }
+
+    void splay(Node *x, Node *g = nullptr) {
+        Node* y;
+        while(x->p != g){
+            Node* p = x->p;
+            if(p->p == g){
+                rotate(x); break;
+            }
+            auto pp = p->p;
+            if((p->l == x) == (pp->l == p)){
+                rotate(p); rotate(x);
+            } else {
+                rotate(x); rotate(x);
+            }
+        }
+        if(!g) tree = x;
+    }
+    void insert(pair<long long, long long> key) {
+        if(!tree) {
+            tree = new Node(key, nullptr);
+            return;
+        }
+        Node *p = tree, **pp;
+        while (1) {
+            if (key == p->key) return;
+            if (key < p->key) {
+                if (!p->l) {
+                    pp = &p->l;
+                    break;
+                }
+                p = p->l;
+            } else {
+                if (!p->r) {
+                    pp = &p->r;
+                    break;
+                }
+                p = p->r;
+            }
+        }
+        *pp = new Node(key, p);
+        splay(*pp);
+    }
+    bool find(pair<long long, long long> key) {
+        Node *p = tree;
+        if (!p) return false;
+        while (p) {
+            if (key == p->key) break;
+            if (key < p->key) {
+                if (!p->l) break;
+                p = p->l;
+            } else {
+                if (!p->r) break;
+                p = p->r;
+            }
+        }
+        splay(p);
+        return key == p->key;
+    }
+
+    void remove(pair<long long, long long> key) {
+        if (!find(key)) return;
+        Node *p = tree;
+        if (p->l and p->r) {
+            tree = p->l, tree->p = nullptr;
+            Node *x = tree;
+            while (x->r) x = x->r;
+            x->r = p->r, p->r->p = x;
+            splay(x), tree->update();
+            delete p; return;
+        }
+        if(p->l) {
+            tree = p->l, tree->p = nullptr;
+            tree->update();
+            delete p; return;
+        }
+        if (p->r) {
+            tree = p->r, tree->p = nullptr;
+            tree->update();
+            delete p; return;
+        }
+        delete p;
+        tree = nullptr;
+    }
+
+    void kth(int k) {
+        Node* x = tree;
+        while(1){
+            while(x->l && x->l->subset > k) x = x->l;
+            if(x->l) k -= x->l->subset;
+            if(!k--) break;
+            x = x->r;
+        }
+        splay(x);
+    }
+
+    Node* gather(int l, int r) {
+        l += 1, r += 1;
+        kth(r+1);
+        Node* x = tree;
+        kth(l-1);
+        splay(x, tree);
+        return tree->r->l;
+    }
+
+    long long sum(int l, int r) {
+        return gather(l,r)->sum;
+    }
+};
+
+class Streamer {
+    deque<int> streams;
+    SplayTree* st;
     unordered_map<long long, long long> freq;
-    set<pair<long long, long long>> hi, lo;
-    Helper(long long k) : lim(k), sum(0) {}
-
-    void remove(long long x) {
-        if(freq[x]) {
-            if(hi.count({freq[x], x})) {
-                hi.erase({freq[x], x});
-                sum -= freq[x] * x;
-            } else lo.erase({freq[x], x});
+    int limit, x;
+public:
+    Streamer(int k, int x) : limit(k), x(x), st(new SplayTree()) {}
+    
+    void update(int x, int operation) {
+        if(freq.count(x)) {
+            st->remove({freq[x], x});
         }
+        freq[x] += operation;
+        if(freq[x] == 0) freq.erase(x);
+        else st->insert({freq[x], x});
     }
-
-    void insert(long long f, long long x) {
-        sum += f * x;
-        hi.insert({f,x});
-    }
-    void balance() {
-        while(hi.size() > lim) {
-            auto [ff,xx] = *hi.begin(); hi.erase(begin(hi));
-            sum -= ff * xx;
-            lo.insert({ff,xx});
+    
+    long long query(int n) {
+        streams.push_back(n);
+        update(streams.back(),1);
+        
+        if(streams.size() > limit) {
+            update(streams.front(), -1);
+            streams.pop_front();
         }
-        while(hi.size() < lim and lo.size()) {
-            auto [ff,xx] = *prev(end(lo)); lo.erase(prev(end(lo)));
-            sum += ff * xx;
-            hi.insert({ff,xx});
-        }
-        while(hi.size() and lo.size() and *hi.begin() < *prev(lo.end())) {
-            auto [f1,x1] = *hi.begin(); hi.erase(begin(hi));
-            auto [f2,x2] = *prev(end(lo)); lo.erase(prev(end(lo)));
-            sum = sum - f1 * x1 + f2 * x2;
-            hi.insert({f2,x2});
-            lo.insert({f1,x1});
-        }
-    }
-    void add(long long x) {
-        remove(x);
-        insert(++freq[x], x);
-        balance();
-    }
-    void del(long long x) {
-        remove(x);
-        if(--freq[x]) {
-            insert(freq[x], x);
-        }
-        balance();
-    }
-    long long query() {
-        return sum;
+        if(streams.size() != limit) return -1;
+        if(freq.size() <= x) return st->tree->sum;
+        return st->sum(freq.size() - x, freq.size() - 1);
     }
 };
 class Solution {
 public:
     vector<long long> findXSum(vector<int>& nums, int k, int x) {
-        Helper* h = new Helper(x);
+        Streamer* s = new Streamer(k,x);
         vector<long long> res;
         for(int i = 0; i < nums.size(); i++) {
-            h->add(nums[i]);
-            if(i >= k) {
-                h->del(nums[i-k]);
-            }
-            if(i + 1 >= k) {
-                res.push_back(h->query());
-            }
+            long long now = s->query(nums[i]);
+            if(now != -1) res.push_back(now);
         }
         return res;
     }
