265b57fcd9623bf7ff1ab1aa1b3075bdab42f480
SongHayoung <37806785+SongHayoung@users.noreply.github.com>
2024-10-13 09:42:46 +0900

    Time: 9 ms (45.45%), Space: 27.4 MB (9.09%) - LeetHub

--- 3611-construct-the-minimum-bitwise-array-ii/3611-construct-the-minimum-bitwise-array-ii.cpp
+++ 3611-construct-the-minimum-bitwise-array-ii/3611-construct-the-minimum-bitwise-array-ii.cpp
@@ -1,42 +1,20 @@
-#include <vector>
-using namespace std;
-
 class Solution {
+    bool bit(int x, int k) {
+        return (x>>k) & 1;
+    }
+    int helper(int x) {
+        if(x % 2 == 0) return -1;
+        int mask = INT_MAX;
+        for(int i = 0; i < 32; i++) {
+            if(bit(x,i)) continue;
+            return x ^ (1ll<<(i-1));
+        }
+        return -1;
+    }
 public:
     vector<int> minBitwiseArray(vector<int>& nums) {
-        vector<int> ans;
-        for(auto &p : nums){
-            if(p < 2){
-                ans.push_back(-1);
-                continue;
-            }
-            bool found = false;
-            int min_a = INT32_MAX;
-            for(int i=0;i<32;i++){
-                if(p & (1 << i)){
-                    int a_candidate = p & (~(1 << i));
-                    if(a_candidate < 0){
-                        continue;
-                    }
-                    // To prevent overflow when a_candidate +1 exceeds integer limits
-                    if(a_candidate +1 < 0){
-                        continue;
-                    }
-                    if( (a_candidate | (a_candidate +1)) == p ){
-                        if(a_candidate < min_a){
-                            min_a = a_candidate;
-                            found = true;
-                        }
-                    }
-                }
-            }
-            if(found){
-                ans.push_back(min_a);
-            }
-            else{
-                ans.push_back(-1);
-            }
-        }
-        return ans;
+        vector<int> res;
+        for(auto& n : nums) res.push_back(helper(n));
+        return res;
     }
 };
