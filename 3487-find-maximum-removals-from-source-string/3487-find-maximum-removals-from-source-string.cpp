4bbbc086e137c9716acff7b54edc5e5131a2934b
SongHayoung <37806785+SongHayoung@users.noreply.github.com>
2024-10-13 09:38:27 +0900

    Time: 41 ms (86.36%), Space: 28.3 MB (95.45%) - LeetHub

--- 3487-find-maximum-removals-from-source-string/3487-find-maximum-removals-from-source-string.cpp
+++ 3487-find-maximum-removals-from-source-string/3487-find-maximum-removals-from-source-string.cpp
@@ -1,31 +1,19 @@
-
 class Solution {
 public:
-    int maximumRemovals(string s, string p, vector<int>& o) {
-        if(s == "yeyeykyded") return 2;
-        vector<int> suf(s.length() + 1, p.length() - 1);
-        for(int i = s.length() - 1; i >= 0; i--) {
-            if(suf[i] >= 0 and s[i] == p[suf[i]]) --suf[i];
-            if(i) suf[i-1] = suf[i];
-        }
-        unordered_set<int> us(begin(o), end(o));
-        int res = 0, now = 0;
-        for(int i = 0; i < s.length(); i++) {
-            if(now == p.length()) {
-                if(us.count(i)) res++;
-            }
-            else {
-                if(p[now] != s[i]) {
-                    if(us.count(i)) res++;
-                    continue;
+    int maximumRemovals(string source, string pattern, vector<int>& order) {
+        int n = source.size(), m = pattern.size();
+        vector<int> dp(m + 1, INT_MIN);
+        dp[0] = 0;
+        for(int i = 0, k = 0; i < n; i++) {
+            bool fl = k < order.size() and order[k] == i;
+            for(int j = m; j >= 0; j--) {
+                if(j + 1 <= m and pattern[j] == source[i]) {
+                    dp[j+1] = max(dp[j+1], dp[j]);
                 }
-                if(!us.count(i)) now++;
-                else {
-                    if(suf[i+1] < now) res++;
-                    else now++;
-                }
+                dp[j] += fl;
             }
+            if(fl) k++;
         }
-        return res;
+        return dp[m];
     }
 };
