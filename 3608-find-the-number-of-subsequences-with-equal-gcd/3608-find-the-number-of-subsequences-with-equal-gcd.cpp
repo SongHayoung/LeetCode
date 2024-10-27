a76b0d3b7f8bc2123b72ab41588dbd24ee65a6b0
SongHayoung <37806785+SongHayoung@users.noreply.github.com>
2024-10-27 15:01:19 +0900

    Time: 306 ms (75%), Space: 407.3 MB (15%) - LeetHub

--- 3608-find-the-number-of-subsequences-with-equal-gcd/3608-find-the-number-of-subsequences-with-equal-gcd.cpp
+++ 3608-find-the-number-of-subsequences-with-equal-gcd/3608-find-the-number-of-subsequences-with-equal-gcd.cpp
@@ -1,14 +1,26 @@
 class Solution {
     long long mod = 1e9 + 7;
-    long long dp[202][202][202];
-    long long helper(vector<int>& A, int p, int f, int s) {
-        if(p == A.size()) return f == s and f > 0 and s > 0;
-        if(dp[p][f][s] != -1) return dp[p][f][s];
-        return dp[p][f][s] = (helper(A,p+1,f,s) + helper(A,p+1,__gcd(f,A[p]),s) + helper(A,p+1,f,__gcd(s,A[p]))) % mod;
-    }
 public:
     int subsequencePairCount(vector<int>& nums) {
-        memset(dp,-1,sizeof dp);
-        return helper(nums,0,0,0);
+        int ma = *max_element(begin(nums), end(nums)) + 1;
+        vector<vector<long long>> dp(ma, vector<long long>(ma));
+        for(auto& n : nums) {
+            vector<vector<long long>> dpp(ma, vector<long long>(ma));
+            for(int f = 0; f < dp.size(); f++) {
+                for(int s = 0; s < dp.size(); s++) {
+                    if(!dp[f][s]) continue;
+                    dpp[f][s] = (dpp[f][s] + dp[f][s]) % mod;
+                    long long fg = __gcd(f,n), sg = __gcd(s,n), c = dp[f][s];
+                    dpp[fg][s] = (dpp[fg][s] + c) % mod;
+                    dpp[f][sg] = (dpp[f][sg] + c) % mod;
+                }
+            }
+            dpp[n][0] = (dpp[n][0] + 1) % mod;
+            dpp[0][n] = (dpp[0][n] + 1) % mod;
+            swap(dp,dpp);
+        }
+        long long res = 0;
+        for(int i = 1; i < dp.size(); i++) res = (res + dp[i][i]) % mod;
+        return res;
     }
 };
