0fd3fb402afd2c972a5e5dd5d1e45ae22ef22961
SongHayoung <37806785+SongHayoung@users.noreply.github.com>
2024-11-24 13:10:55 +0900

    Time: 2 ms (58.33%), Space: 31 MB (8.33%) - LeetHub

--- 3644-minimum-positive-sum-subarray/3644-minimum-positive-sum-subarray.cpp
+++ 3644-minimum-positive-sum-subarray/3644-minimum-positive-sum-subarray.cpp
@@ -1,13 +1,21 @@
 class Solution {
 public:
     int minimumSumSubarray(vector<int>& nums, int l, int r) {
-        int res = INT_MAX, n = nums.size();
-        for(int i = 0; i < n; i++) {
-            for(int j = i, sum = 0; j < n and j < i + r; j++) {
-                sum += nums[j];
-                if(sum > 0 and j >= i + l - 1) {
-                    res = min(res, sum);
-                }
+        long long res = INT_MAX, n = nums.size();
+        vector<long long> pre{nums[0]};
+        for(int i = 1; i < n; i++) pre.push_back(pre.back() + nums[i]);
+        multiset<long long> ms;
+        for(int i = n - 1; i >= 0; i--) {
+            if(i + r < n) {
+                ms.erase(ms.find(pre[i+r]));
+            }
+            if(i + l - 1 < n) {
+                ms.insert(pre[i+l-1]);
+            }
+            long long x = i ? pre[i-1] : 0;
+            auto it = ms.upper_bound(x);
+            if(it != end(ms)) {
+                res = min(res, *it - x);
             }
         }
         return res == INT_MAX ? -1 : res;
