7bcb5f4bcd30b89c812d6490eee9f10d3d30cae2
SongHayoung <37806785+SongHayoung@users.noreply.github.com>
2024-11-24 14:28:03 +0900

    Time: 66 ms (84.44%), Space: 137 MB (82.22%) - LeetHub

--- 3652-find-sorted-submatrices-with-maximum-element-at-most-k/3652-find-sorted-submatrices-with-maximum-element-at-most-k.cpp
+++ 3652-find-sorted-submatrices-with-maximum-element-at-most-k/3652-find-sorted-submatrices-with-maximum-element-at-most-k.cpp
@@ -1,28 +1,39 @@
-
 class Solution {
-    long long helper(vector<long long>& A, int n) {
-        long long res = 0;
-
-        for(int i = 0; i <= n; i++) {
-            for(long long j = i, mi = A[j];  j >= 0; j--) {
-                mi = min(mi, A[j]);
-                res += mi;
-            }
-        }
-
-
-        return res;
-    }
 public:
     long long countSubmatrices(vector<vector<int>>& grid, int k) {
         long long res = 0, n = grid.size(), m = grid[0].size();
-        vector<long long> dp(m+1);
-        for(int i = 0; i < n; i++) {
-            for(int j = 0; j < m; j++) {
-                if(grid[i][j] <= k) dp[j]++;
-                else dp[j] = 0;
+        grid.push_back(vector<int>(m,k + 1));
+        vector<vector<pair<long long, long long>>> dp(m);
+        auto clean = [&](int x) {
+            int t = 0;
+            for(int i = 0; i < dp[x].size(); i++) {
+                int h = dp[x].back().first - dp[x][i].first;
+                res += h * (h + 1) / 2 * (dp[x][i].second - t);
+                t = dp[x][i].second;
             }
-            res += helper(dp,m);
+            dp[x] = {};
+        };
+        for(int i = 0; i <= n; i++) {
+            for(long long j = 0, len = 0; j < m; j++) {
+                if(grid[i][j] > k) {
+                    dp[j].push_back({i,0});
+                    clean(j), len = 0;
+                } else {
+                    if(!j or grid[i][j] <= grid[i][j-1]) len++;
+                    else len = 1;
+                    int pos = i;
+                    while(dp[j].size() and dp[j].back().second > len) {
+                        long long h = i - dp[j].back().first;
+                        long long w = dp[j].back().second - max(len, dp[j].size() > 1 ? dp[j][dp[j].size()-2].second : 0);
+                        res += h * (h + 1) / 2 * w;
+                        pos = dp[j].back().first;
+                        dp[j].pop_back();
+                    }
+                    if(dp[j].size() == 0 or dp[j].back().second != len) {
+                        dp[j].push_back({pos,len});
+                    }
+                }
+            }
         }
         return res;
     }
