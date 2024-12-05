d1f5a8b6a5041857ee83fc40961c17992c513990
SongHayoung <37806785+SongHayoung@users.noreply.github.com>
2024-12-05 09:17:32 +0900

    Time: 30 ms (29.89%), Space: 27.3 MB (20.72%) - LeetHub

--- 2414-move-pieces-to-obtain-a-string/2414-move-pieces-to-obtain-a-string.cpp
+++ 2414-move-pieces-to-obtain-a-string/2414-move-pieces-to-obtain-a-string.cpp
@@ -2,17 +2,17 @@ class Solution {
 public:
     bool canChange(string start, string target) {
         if(start.size() != target.size()) return false;
-        vector<int> a,b;
+        deque<int> a,b;
         for(int i = 0; i < start.size(); i++) {
             if(start[i] != '_') a.push_back(i);
             if(target[i] != '_') b.push_back(i);
+            if(a.size() and b.size()) {
+                if(start[a[0]] != target[b[0]]) return false;
+                if(start[a[0]] == 'L' and a[0] < b[0]) return false;
+                if(start[a[0]] == 'R' and a[0] > b[0]) return false;
+                a.pop_front(), b.pop_front();
+            }
         }
-        if(a.size() != b.size()) return false;
-        for(int i = 0; i < a.size(); i++) {
-            if(start[a[i]] != target[b[i]]) return false;
-            if(start[a[i]] == 'L' and a[i] < b[i]) return false;
-            if(start[a[i]] == 'R' and a[i] > b[i]) return false;
-        }
-        return true;
+        return a.size() == 0 and b.size() == 0;
     }
 };
