ae69ef064e0fb4ad9a8b4992f851070fe16123af
SongHayoung <37806785+SongHayoung@users.noreply.github.com>
2024-12-15 13:06:01 +0900

    Time: 1174 ms (30.77%), Space: 196.6 MB (46.15%) - LeetHub

--- 3686-count-beautiful-splits-in-an-array/3686-count-beautiful-splits-in-an-array.cpp
+++ 3686-count-beautiful-splits-in-an-array/3686-count-beautiful-splits-in-an-array.cpp
@@ -1,24 +1,40 @@
+vector<int> zfunction(deque<int>& s) {
+    int l = 0, r = 0, n = s.size();
+    vector<int> z(n);
+    for(int i = 1; i < n; i++) {
+        if(i > r) {
+            l = r = i;
+            while(r < n and s[r-l] == s[r]) r++;
+            z[i] = r - l;
+            r--;
+        } else {
+            int k = i - l;
+            if(z[k] < r - i + 1) z[i] = z[k];
+            else {
+                l = i;
+                while(r < n and s[r-l] == s[r]) r++;
+                z[i] = r - l;
+                r --;
+            }
+        }
+    }
+    return z;
+}
 class Solution {
 public:
     int beautifulSplits(vector<int>& nums) {
-        int res = 0, n = nums.size();
-        unordered_set<long long> matched;
-        vector<long long> hashs(n), pre(n + 1);
-        long long mod = 1e9 + 7, po = 222, inv = 436936940;
-        for(int i = 0; i < n; i++) pre[i+1] = (pre[i] * po + nums[i]) % mod;
-        for(long long len = 1, ppo = po, invv = inv; len < n; len++, ppo = ppo * po % mod, invv = invv * inv % mod) {
-            for(int i = 0; i < n; i++) {
-                if(i + 1 >= len) {
-                    hashs[i+1-len] = (pre[i+1] - pre[i+1-len] * ppo % mod + mod) % mod * invv % mod;
-                    if(matched.count(i+1-2*len)) continue;
-                    if(i + 1 - 2 * len >= 0 and hashs[i+1-len] == hashs[i+1-2*len]) {
-                        if(i + 1 - 2 * len == 0) {
-                            matched.insert(i + 1 - len);
-                            res += n - i - 1;
-                        } else res++;
-                    }
-                }
+        deque<int> A(begin(nums), end(nums));
+        int res = 0;
+        vector<int> z1 = zfunction(A);
+        for(int i = 1; i < z1.size(); i++) {
+            A.pop_front();
+            vector<int> z2 = zfunction(A);
+            int until = z2.size();
+            if(z1[i] >= i and nums.size() - 2 * i > 0) {
+                res += nums.size() - i * 2;
+                until = i;
             }
+            for(int j = 1; j < until; j++) if(j <= z2[j]) res++;
         }
         return res;
     }
