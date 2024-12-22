a228b1ed426ccb37cd4771cb0ff7386c3f5bfdc8
SongHayoung <37806785+SongHayoung@users.noreply.github.com>
2024-12-22 14:15:57 +0900

    Time: 91 ms (93.62%), Space: 62.4 MB (22.21%) - LeetHub

--- 1780-lowest-common-ancestor-of-a-binary-tree-ii/1780-lowest-common-ancestor-of-a-binary-tree-ii.cpp
+++ 1780-lowest-common-ancestor-of-a-binary-tree-ii/1780-lowest-common-ancestor-of-a-binary-tree-ii.cpp
@@ -8,8 +8,27 @@
  * };
  */
 class Solution {
+    TreeNode* res = nullptr;
+    bool dfs(TreeNode* node, TreeNode* p, TreeNode* q) {
+        if(!node) return false;
+        if(res) return false;
+        if(node == p or node == q) {
+            if(dfs(node->left, p, q) or dfs(node->right, p, q)) {
+                res = node;
+                return false;
+            }
+            return true;
+        }
+        auto l = dfs(node->left, p, q), r = dfs(node->right, p, q);
+        if(l and r) {
+            res = node;
+            return false;
+        }
+        return l or r;
+    }
 public:
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-        
+        dfs(root, p, q);
+        return res;
     }
 };
