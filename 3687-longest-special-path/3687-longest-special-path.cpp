378fac608aca3041c1c34154d060b0e99add50fb
SongHayoung <37806785+SongHayoung@users.noreply.github.com>
2025-01-19 01:04:11 +0900

    Time: 1538 ms (50%), Space: 462.8 MB (66.67%) - LeetHub

--- 3687-longest-special-path/3687-longest-special-path.cpp
+++ 3687-longest-special-path/3687-longest-special-path.cpp
@@ -1,23 +1,54 @@
+struct Seg{
+    int mi,ma,sum;
+    Seg *left, *right;
+    Seg(int l, int r) : mi(l), ma(r), sum(0), left(nullptr), right(nullptr) {
+        if(l^r) {
+            int m = l + (r - l) / 2;
+            left = new Seg(l,m);
+            right = new Seg(m+1,r);
+        }
+    }
+    void update(int n, int x) {
+        if(mi <= n and n <= ma) {
+            if(mi == ma) {
+                sum = x;
+            } else {
+                left->update(n,x);
+                right->update(n,x);
+                sum = left->sum + right->sum;
+            }
+        }
+    }
+    int query(int l, int r) {
+        if(l <= mi and ma <= r) return sum;
+        if(l > ma or r < mi) return 0;
+        return left->query(l,r) + right->query(l,r);
+    }
+};
 class Solution {
     vector<vector<pair<int,int>>> adj;
+    Seg* seg;
     unordered_map<int,vector<int>> deps;
-    vector<int> sums;
-    void dfs(int u, int par, int dep, int hi, vector<int>& res, vector<int>& A, int sum) {
-        if(deps[A[u]].size()) {
-            while(hi <= deps[A[u]].back()) sum -= sums[hi++];
-        }
+    void dfs(int u, int par, int dep, int hi, vector<int>& res, vector<int>& A) {
+        if(deps[A[u]].size()) hi = max(hi, deps[A[u]].back() + 1);
         deps[A[u]].push_back(dep);
-        res = min(res, {-sum,dep - hi + 1});
+        int w = seg->query(hi,dep);
+        int l = dep - hi + 1;
+        if(res[0] < w) res = {w,l};
+        else if(res[0] == w) res[1] = min(res[1],l);
+        
         for(auto& [v,w] : adj[u]) {
             if(v == par) continue;
-            sums.push_back(w);
-            dfs(v,u,dep+1,hi,res,A,sum + w);
-            sums.pop_back();
+            seg->update(dep,w);
+            dfs(v,u,dep+1,hi,res,A);
         }
+        seg->update(dep,0);
         deps[A[u]].pop_back();
     }
 public:
     vector<int> longestSpecialPath(vector<vector<int>>& edges, vector<int>& nums) {
+        seg = new Seg(0, edges.size() + 1);
+        deps = {};
         adj = vector<vector<pair<int,int>>>(edges.size() + 1);
         for(auto& e : edges) {
             int u = e[0], v = e[1], w = e[2];
@@ -25,8 +56,7 @@ public:
             adj[v].push_back({u,w});
         }
         vector<int> res{0,1};
-        dfs(0,-1,0,0,res,nums,0);
-        res[0] = -res[0];
+        dfs(0,-1,0,0,res,nums);
         return res;
     }
 };
