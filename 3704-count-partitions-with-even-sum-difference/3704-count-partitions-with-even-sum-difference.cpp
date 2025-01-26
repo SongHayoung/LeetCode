1bb88eaabeb33390f88d148f2bbf40cfeb74fc3b
SongHayoung <37806785+SongHayoung@users.noreply.github.com>
2025-01-26 14:39:31 +0900

    Time: 0 ms (100%), Space: 22.2 MB (91.67%) - LeetHub

--- 3704-count-partitions-with-even-sum-difference/3704-count-partitions-with-even-sum-difference.cpp
+++ 3704-count-partitions-with-even-sum-difference/3704-count-partitions-with-even-sum-difference.cpp
@@ -3,6 +3,6 @@ public:
     int countPartitions(vector<int>& nums) {
         int bit = 0;
         for(auto& n : nums) bit ^= n;
-        return bit ? 0 : nums.size() - 1;
+        return bit & 1 ? 0 : nums.size() - 1;
     }
 };
