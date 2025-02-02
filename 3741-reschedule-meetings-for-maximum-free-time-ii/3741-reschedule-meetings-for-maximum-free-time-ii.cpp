be61cbad106e07340c8e5800af4ec974c0f4f459
SongHayoung <37806785+SongHayoung@users.noreply.github.com>
2025-02-02 09:41:31 +0900

    Time: 161 ms (33.33%), Space: 196.4 MB (33.33%) - LeetHub

--- 3741-reschedule-meetings-for-maximum-free-time-ii/3741-reschedule-meetings-for-maximum-free-time-ii.cpp
+++ 3741-reschedule-meetings-for-maximum-free-time-ii/3741-reschedule-meetings-for-maximum-free-time-ii.cpp
@@ -4,29 +4,38 @@ public:
         int res = 0, n = startTime.size();
         multiset<int> ms;
         for(int i = 0; i < n; i++) {
-            int st = i ? endTime[i-1] : 0;
-            int ed = i + 1 == n ? eventTime : startTime[i+1];
             if(i) {
                 ms.insert(startTime[i] - endTime[i-1]);
                 if(i + 1 == n) ms.insert(eventTime - endTime[i]);
             } else ms.insert(startTime[i]);
-            res = max(res, ed - st - (endTime[i] - startTime[i]));
+            
+            while(ms.size() > 3) ms.erase(begin(ms));
         }
+        vector<function<void()>> defers;
+        
         for(int i = 0; i < n; i++) {
             int le = i ? startTime[i] - endTime[i-1] : startTime[i];
             int ri = i + 1 == n? eventTime - endTime[i] : startTime[i+1] - endTime[i];
-            ms.erase(ms.find(le));
-            ms.erase(ms.find(ri));
             
-            int size = endTime[i] - startTime[i];
-            if(ms.size() and *prev(end(ms)) >= size) {
-                int st = i ? endTime[i-1] : 0;
-                int ed = i + 1 == n ? eventTime : startTime[i+1];
-                res = max(res, ed - st);
+            if(ms.find(le) != end(ms)) {
+                ms.erase(ms.find(le));
+                defers.push_back([&ms, le]() { ms.insert(le); });
+            }
+            if(ms.find(ri) != end(ms)) {
+                ms.erase(ms.find(ri));
+                defers.push_back([&ms, ri]() { ms.insert(ri); });
             }
             
-            ms.insert(le);
-            ms.insert(ri);
+            int size = endTime[i] - startTime[i];
+            int st = i ? endTime[i-1] : 0;
+            int ed = i + 1 == n ? eventTime : startTime[i+1];
+            
+            res = max(res, ed - st - (*prev(end(ms)) >= size ? 0 : size));
+            
+            while(defers.size()) {
+                auto defer = defers.back(); defers.pop_back();
+                defer();
+            }
         }
         return res;
     }
