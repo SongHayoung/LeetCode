9bd9ba0b105b515ae756b14feb8e32676b50db87
SongHayoung <37806785+SongHayoung@users.noreply.github.com>
2025-02-06 23:14:56 +0900

    Time: 159 ms (39.6%), Space: 312.2 MB (17.92%) - LeetHub

--- 3674-count-non-decreasing-subarrays-after-k-operations/3674-count-non-decreasing-subarrays-after-k-operations.cpp
+++ 3674-count-non-decreasing-subarrays-after-k-operations/3674-count-non-decreasing-subarrays-after-k-operations.cpp
@@ -1,19 +1,18 @@
 class Solution {
 public:
-    long long countNonDecreasingSubarrays(vector<int>& nums, int kk) {
-        deque<array<int,3>> dq;
-        long long res = 0, k = kk;
+    long long countNonDecreasingSubarrays(vector<int>& nums, long long k) {
+        deque<array<long long,3>> dq;
+        long long res = 0;
         for(int i = nums.size() - 1; i >= 0; i--) {
             int cnt = 1;
             while(dq.size() and dq.front()[2] <= nums[i]) {
                 auto [idx, ccnt, val] = dq.front(); dq.pop_front();
                 cnt += ccnt;
-                k -= ccnt * (1ll * nums[i] - val);
+                k -= ccnt * (nums[i] - val);
             }
             dq.push_front({i,cnt,nums[i]});
             while(k < 0) {
-                int at = dq.back()[0] + dq.back()[1] - 1;
-                k += dq.back()[2] - nums[at];
+                k += dq.back()[2] - nums[dq.back()[0] + dq.back()[1] - 1];
                 if(--dq.back()[1] == 0) dq.pop_back();
             }
 
