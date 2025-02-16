def6ec904fd61becc2d54ccb479191328c19de51
SongHayoung <37806785+SongHayoung@users.noreply.github.com>
2025-02-16 09:28:33 +0900

    Time: 149 ms (43.33%), Space: 182.2 MB (23.33%) - LeetHub

--- 3763-separate-squares-i/3763-separate-squares-i.cpp
+++ 3763-separate-squares-i/3763-separate-squares-i.cpp
@@ -1,25 +1,24 @@
 class Solution {
+    const long double EPS = 1e-9;
+    long long helper(vector<vector<int>>& A, long double bar, long double area) {
+        for(auto& a : A) {
+            long double y = a[1], yy = a[1] + a[2];
+            if(y >= bar) continue;
+            area -= (min(yy,bar) - y) * a[2];
+        }
+        return area <= 0;
+    }
 public:
     double separateSquares(vector<vector<int>>& squares) {
-        long double area = 0, sum = 0, py = 0, now = 0;
-        for(auto& s : squares) area += s[2] / 2. * s[2];
-        vector<pair<long long, long long>> S;
-        unordered_map<long long, long long> mp;
-        for(auto& s : squares) {
-            mp[s[1]] += s[2];
-            mp[s[1] + s[2]] -= s[2];
+        long double area = 0, l = 0, r = 2e9;
+        for(auto& s : squares) area += 1ll * s[2] * s[2] / 2.;
+        while(r - l > EPS) {
+            long double m = l + (r - l) / 2;
+            long long ok = helper(squares, m, area);
+            if(ok) {
+                r = m;
+            } else l = m;
         }
-        for(auto& [k,v] : mp) if(v) S.push_back({k,v});
-        sort(begin(S), end(S));
-        for(auto& [y, l] : S) {
-            long double nxt = now + (y - py) * sum;
-            if(nxt >= area) {
-                return py + (area - now) / sum;
-            }
-            sum += l;
-            now = nxt;
-            py = y;
-        }
-        return -1;
+        return l;
     }
 };
