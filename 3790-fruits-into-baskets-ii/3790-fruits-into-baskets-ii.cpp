13e07bd97104f5cabd200711641aa061c0a02879
SongHayoung <37806785+SongHayoung@users.noreply.github.com>
2025-03-09 18:40:38 +0900

    Time: 33 ms (26.67%), Space: 77.8 MB (13.33%) - LeetHub

--- 3790-fruits-into-baskets-ii/3790-fruits-into-baskets-ii.cpp
+++ 3790-fruits-into-baskets-ii/3790-fruits-into-baskets-ii.cpp
@@ -1,18 +1,58 @@
+
+
+struct Seg {
+    int mi,ma,best;
+    Seg *left, *right;
+    Seg(vector<int>& A, int l, int r) : mi(A[l]), ma(A[r]), best(INT_MAX), left(nullptr), right(nullptr) {
+        if(l^r) {
+            int m = l + (r - l) / 2;
+            left = new Seg(A,l,m);
+            right = new Seg(A,m+1,r);
+        }
+    }
+    void add(int x, int idx) {
+        if(mi <= x and x <= ma) {
+            best = min(best, idx);
+            if(left) left->add(x,idx);
+            if(right) right->add(x,idx);
+        }
+    }
+    void remove(int x) {
+        if(mi <= x and x <= ma) {
+            best = INT_MAX;
+            if(left and right) {
+                right->remove(x);
+                left->remove(x);
+                best = min(left->best, right->best);
+            }
+        }
+    }
+    int query(int x) {
+        if(mi >= x) return best;
+        if(ma < x) return INT_MAX;
+        return min(left->query(x), right->query(x));
+    }
+};
 class Solution {
 public:
     int numOfUnplacedFruits(vector<int>& fruits, vector<int>& baskets) {
+        vector<int> S = baskets;
+        sort(begin(S), end(S));
+        S.erase(unique(begin(S), end(S)), end(S));
+        Seg* seg = new Seg(S,0,S.size() - 1);
+        unordered_map<int,deque<int>> mp;
+        for(int i = 0; auto& b : baskets) mp[b].push_back(i++);
+        for(auto& [k,v] : mp) seg->add(k,v[0]);
         int res = 0;
-        vector<int> use(fruits.size());
         for(auto& f : fruits) {
-            int at = -1;
-            for(int i = 0; i < baskets.size(); i++) {
-                if(use[i]) continue;
-                if(baskets[i] <= f) continue;
-                at = i;
-                break;
+            int idx = seg->query(f);
+            if(idx == INT_MAX) res++;
+            else {
+                seg->remove(baskets[idx]);
+                int cap = baskets[idx];
+                mp[cap].pop_front();
+                if(mp[cap].size()) seg->add(cap, mp[cap][0]);
             }
-            if(at == -1) res++;
-            else use[at] = 1;
         }
         return res;
     }
