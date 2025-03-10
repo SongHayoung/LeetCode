0f5dad12f9f82c28c90ec3bb2bff3820718ca90e
SongHayoung <37806785+SongHayoung@users.noreply.github.com>
2025-03-10 10:36:05 +0900

    Time: 264 ms (33.42%), Space: 56.5 MB (39.18%) - LeetHub

--- 3569-count-of-substrings-containing-every-vowel-and-k-consonants-ii/3569-count-of-substrings-containing-every-vowel-and-k-consonants-ii.cpp
+++ 3569-count-of-substrings-containing-every-vowel-and-k-consonants-ii/3569-count-of-substrings-containing-every-vowel-and-k-consonants-ii.cpp
@@ -1,45 +1,27 @@
 class Solution {
 public:
     long long countOfSubstrings(string word, int k) {
-        unordered_set<char> vowels = {'a', 'e', 'i', 'o', 'u'};
-        unordered_map<char, int> vowel_count;
-        int n = word.length();
-        int consonant_count = 0;
-        int left = 0;
-        long long result = 0;
-        
-        for (int right = 0; right < n; ++right) {
-            char right_char = word[right];
-
-            // Add the current character to the window
-            if (vowels.count(right_char)) {
-                vowel_count[right_char]++;
-            } else {
-                consonant_count++;
-            }
-
-            // Shrink the window from the left if consonants exceed k
-            while (consonant_count > k) {
-                char left_char = word[left];
-                if (vowels.count(left_char)) {
-                    vowel_count[left_char]--;
-                    if (vowel_count[left_char] == 0) {
-                        vowel_count.erase(left_char);
-                    }
-                } else {
-                    consonant_count--;
-                }
-                left++;
-            }
-
-            // Check if we have all vowels and exactly k consonants
-            if (vowel_count.size() == 5 && consonant_count == k) {
-                // Since all substrings ending at 'right' and starting from 'left' to 'right'
-                // are valid, we add the number of such substrings to the result
-                result += (left + 1);
-            }
+        long long n = word.length(), res = 0;
+        unordered_set<char> vowels{'a','e','i','o','u'};
+        vector<int> suf(n + 1);
+        for(int i = n - 1; i >= 0; i--) {
+            if(i + 1 < n and vowels.count(word[i+1])) suf[i] = suf[i+1] + 1;
+            else suf[i] = vowels.count(word[i]);
         }
-
-        return result;
+        unordered_map<char,int> freq;
+        for(int i = 0, j = 0, cnt = 0; i < n; i++) {
+            while(j < n and (freq.size() != 5 or cnt < k)) {
+                if(vowels.count(word[j])) freq[word[j]]++;
+                else cnt++;
+                j++;
+            }
+            if(freq.size() == 5 and cnt == k) {
+                res += max(1, suf[j-1]);
+            }
+            if(vowels.count(word[i])) {
+                if(--freq[word[i]] == 0) freq.erase(word[i]);
+            } else cnt--;
+        }
+        return res;
     }
 };
