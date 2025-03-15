9212c11c19d124059b48dabb0ebf64d6277add40
SongHayoung <37806785+SongHayoung@users.noreply.github.com>
2025-03-16 08:07:21 +0900

    Time: 304 ms (17.86%), Space: 271.9 MB (5.36%) - LeetHub

--- 3784-longest-common-prefix-of-k-strings-after-removal/3784-longest-common-prefix-of-k-strings-after-removal.cpp
+++ 3784-longest-common-prefix-of-k-strings-after-removal/3784-longest-common-prefix-of-k-strings-after-removal.cpp
@@ -1,39 +1,46 @@
-class Solution {
-    int helper(string& a, string& b) {
-        int res = 0;
-        for(; res < a.length() and res < b.length() and a[res] == b[res]; res++) {}
-        return res;
+struct Trie {
+    int cnt, depth, k;
+    int best;
+    Trie *next[26];
+    Trie(int k) : cnt(0), depth(0), k(k), best(0) {
+        memset(next, 0, sizeof next);
     }
-    pair<vector<int>,int> helper(vector<string>& S, vector<int>& ord, int k) {
-        vector<int> best{0,0};
-        int bestIndex = -1, n = S.size();
-        for(int i = k - 1, j = 0; i < n; i++,j++) {
-            int ii = ord[i], jj = ord[j];
-
-            if(min(S[ii].length(), S[jj].length()) <= best[1]) continue;
-            int match = helper(S[ii], S[jj]);
-            if(match <= best[1]) continue;
-            
-            if(match > best[0]) swap(match, best[0]), bestIndex = i;
-            if(match > best[1]) swap(match, best[1]);
+    void update() {
+        best = 0;
+        if(cnt >= k) {
+            best = max(best, depth);
+            for(int i = 0; i < 26; i++) {
+                if(next[i]) best = max(best, next[i]->best);
+            }
         }
-
-        return {best, bestIndex};
     }
+    void insert(string& s, int pos = 0) {
+        cnt++, depth = pos;
+        if(pos != s.size()) {
+            if(!next[s[pos]-'a']) next[s[pos]-'a'] = new Trie(k);
+            next[s[pos]-'a']->insert(s,pos+1);
+        }
+        update();
+    }
+    void clean(string& s, int pos = 0) {
+        cnt--, depth = pos;
+        if(pos != s.size()) {
+            next[s[pos]-'a']->clean(s,pos+1);
+        }
+        update();
+    }
+};
+
+class Solution {
 public:
     vector<int> longestCommonPrefix(vector<string>& words, int k) {
-        if(words.size() <= k) return vector<int>(words.size());
-        int n = words.size();
-        vector<int> ord(n);
-        for(int i = 0; i < n; i++) ord[i] = i;
-        sort(begin(ord), end(ord), [&](int& i, int& j) {return words[i] < words[j];});
-        auto [best, bestIndex] = helper(words,ord,k);
-        vector<int> rord(n);
-        vector<int> res(n, best[0]);
-        if(best[0] != best[1]) {
-            for(int i = bestIndex - k + 1; i <= bestIndex; i++) {
-                res[ord[i]] = best[1];
-            }
+        Trie* t = new Trie(k);
+        for(auto& w : words) t->insert(w);
+        vector<int> res;
+        for(auto& w : words) {
+            t->clean(w);
+            res.push_back(t->best);
+            t->insert(w);
         }
         return res;
     }
