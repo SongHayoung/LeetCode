512f14015b3891a79f5544d69bfa2b745d2c2b9e
SongHayoung <37806785+SongHayoung@users.noreply.github.com>
2025-04-20 15:17:10 +0900

    Time: 2 ms (71.42%), Space: 206.4 MB (56.46%) - LeetHub

--- 3738-make-array-non-decreasing/3738-make-array-non-decreasing.cpp
+++ 3738-make-array-non-decreasing/3738-make-array-non-decreasing.cpp
@@ -2,11 +2,8 @@ class Solution {
 public:
     int maximumPossibleSize(vector<int>& nums) {
         int res = 0, ma = 0;
-        vector<int> st;
         for(auto& n : nums) {
-            if(n >= ma) {
-                ma = n, res++;
-            }
+            if(n >= ma) ma = n, res++;
         }
         return res;
     }
