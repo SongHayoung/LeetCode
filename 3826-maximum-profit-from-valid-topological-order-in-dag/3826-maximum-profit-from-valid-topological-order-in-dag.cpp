76082b323e9b1eebe14a537f772d985f64c3ff57
SongHayoung <37806785+SongHayoung@users.noreply.github.com>
2025-04-27 10:27:53 +0900

    Time: 2570 ms (8.33%), Space: 61.5 MB (58.33%) - LeetHub

--- 3826-maximum-profit-from-valid-topological-order-in-dag/3826-maximum-profit-from-valid-topological-order-in-dag.cpp
+++ 3826-maximum-profit-from-valid-topological-order-in-dag/3826-maximum-profit-from-valid-topological-order-in-dag.cpp
@@ -6,11 +6,7 @@ class Solution {
 public:
     int maxProfit(int n, vector<vector<int>>& edges, vector<int>& score) {
         vector<int> adj(n);
-        for(auto& e : edges) {
-            int u = e[0], v = e[1];
-            adj[v] |= 1<<u;
-        }
-        dp[0] = 0;
+        for(auto& e : edges) adj[e[1]] |= 1<<e[0];
         int ma = 1<<n;
         for(int mask = 1; mask < ma; mask++) {
             int ord = __builtin_popcount(mask);
