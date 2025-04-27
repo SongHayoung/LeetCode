8574ff7510e6f969f345cb729905ab7560e5decc
SongHayoung <37806785+SongHayoung@users.noreply.github.com>
2025-04-27 10:15:37 +0900

    Time: 84 ms (11.11%), Space: 125 MB (11.11%) - LeetHub

--- 3821-count-cells-in-overlapping-horizontal-and-vertical-substrings/3821-count-cells-in-overlapping-horizontal-and-vertical-substrings.cpp
+++ 3821-count-cells-in-overlapping-horizontal-and-vertical-substrings/3821-count-cells-in-overlapping-horizontal-and-vertical-substrings.cpp
@@ -1,23 +1,26 @@
-class Solution {
-    vector<int> pi(string& p) {
-        vector<int> PI(p.length());
-        for(int i = 1, j = 0; i < p.length(); i++) {
-            while(j and p[i] != p[j]) j = PI[j-1];
-            if(p[i] == p[j]) PI[i] = ++j;
+struct RollingHash {
+    vector<long long> hash, po;
+    long long base, mod;
+    RollingHash(string& s) : base(131), mod(1e9 + 7) {
+        hash = vector<long long>(s.length() + 1), po = vector<long long>(s.length() + 1);
+        po[0] = 1;
+        for(int i = 0; i < s.length(); i++) {
+            po[i+1] = po[i] * base % mod;
+            hash[i+1] = (hash[i] * base + s[i] - 'a') % mod;
         }
-        return PI;
     }
-    vector<int> kmp(string& s, string& p) {
-        vector<int> PI = pi(p);
+    long long get(int pos, int len) {
+        return (hash[pos + len] - hash[pos] * po[len] % mod + mod) % mod;
+    }
+};
+
+class Solution {
+    vector<int> rh(string& s, string& p) {
+        RollingHash srh(s), prh(p);
         vector<int> at;
-        for(int i = 0, j = 0; i < s.length(); i++) {
-            while(j and s[i] != p[j]) j = PI[j-1];
-            if(s[i] == p[j]) {
-                if(++j == p.length()) {
-                    at.push_back(i - p.length() + 1);
-                    j = PI[j-1];
-                }
-            }
+        long long hash = prh.get(0,p.length());
+        for(int i = 0; i <= s.length() - p.length(); i++) {
+            if(hash == srh.get(i,p.length())) at.push_back(i);
         }
         vector<int> res;
         for(int i = 0, pos = 0; i < at.size(); i++) {
@@ -31,17 +34,17 @@ public:
         string ho = "", ve = "";
         int n = grid.size(), m = grid[0].size();
         for(int i = 0; i < n; i++) for(int j = 0; j < m; j++) {
-            ho.push_back(grid[i][j]);
-        }
+                ho.push_back(grid[i][j]);
+            }
         for(int j = 0; j < m; j++) for(int i = 0; i < n; i++) {
-            ve.push_back(grid[i][j]);
-        }
+                ve.push_back(grid[i][j]);
+            }
         vector<vector<int>> mask(n, vector<int>(m));
-        
-        vector<int> hoo = kmp(ho,pattern), vee = kmp(ve,pattern);
+
+        vector<int> hoo = rh(ho,pattern), vee = rh(ve,pattern);
         for(auto& h : hoo) mask[h/m][h%m] |= 1;
         for(auto& v : vee) mask[v%n][v/n] |= 2;
-        
+
         int res = 0;
         for(int i = 0; i < n; i++) for(int j = 0; j < m; j++) if(mask[i][j] == 3) res++;
         return res;
