75a934bbee8c2f1a5e912d93736aebb951fd9492
SongHayoung <37806785+SongHayoung@users.noreply.github.com>
2025-05-04 14:29:28 +0900

    Time: 25 ms (5.88%), Space: 65.9 MB (99.75%) - LeetHub

--- 3822-fill-a-special-grid/3822-fill-a-special-grid.cpp
+++ 3822-fill-a-special-grid/3822-fill-a-special-grid.cpp
@@ -1,19 +1,16 @@
 class Solution {
-    void dnc(vector<vector<int>>& res, int y, int x, int len, int& ord) {
-        if(len == 1) res[y][x] = ord++;
-        else {
-            len>>=1;
-            dnc(res, y, x + len, len, ord);
-            dnc(res, y + len, x + len, len, ord);
-            dnc(res, y + len, x, len, ord);
-            dnc(res, y, x, len, ord);
-        }
-    }
 public:
     vector<vector<int>> specialGrid(int N) {
-        int n = 1<<N, ord = 0;
-        vector<vector<int>> res(n, vector<int>(n));
-        dnc(res, 0,0,n, ord);
+        int size = 1 << N;
+        vector<vector<int>> res(size, vector<int>(size, 0));
+        int table[2][2] {{3, 0}, {2, 1}};
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                for(int k = N - 1; k >= 0; k--) {
+                    res[i][j] = (res[i][j]<<2) + table[(i >> k) & 1][(j >> k) & 1];
+                }
+            }
+        }
         return res;
     }
 };
