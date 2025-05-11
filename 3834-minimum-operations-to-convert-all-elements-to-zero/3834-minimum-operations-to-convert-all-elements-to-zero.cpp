3658e8e3cfa95fbb2abe9381313e9789fc01b113
SongHayoung <37806785+SongHayoung@users.noreply.github.com>
2025-05-11 14:09:05 +0900

    Time: 971 ms (52.63%), Space: 423.3 MB (31.58%) - LeetHub

--- 3834-minimum-operations-to-convert-all-elements-to-zero/3834-minimum-operations-to-convert-all-elements-to-zero.cpp
+++ 3834-minimum-operations-to-convert-all-elements-to-zero/3834-minimum-operations-to-convert-all-elements-to-zero.cpp
@@ -1,13 +1,24 @@
 class Solution {
 public:
     int minOperations(vector<int>& nums) {
-        int res = 0;
-        vector<int> st{0};
-        for(auto& n : nums) {
-            while(st.back() > n) st.pop_back();
-            if(st.back() < n) {
+        map<int,vector<int>> at;
+        int n = nums.size(), res = 0;
+        vector<int> le(n), ri(n);
+        for(int i = 0; i < n; i++) {
+            le[i] = ri[i] = i;
+            if(nums[i]) at[-nums[i]].push_back(i);
+        }
+        auto merge = [&](int i, int j) {
+            ri[i] = ri[j];
+            le[j] = le[i];
+        };
+        for(auto& [_, vec] : at) {
+            while(vec.size()) {
+                auto p = vec.back(); vec.pop_back();
+                if(ri[p] != p) continue;
+                if(p + 1 < n and nums[p+1] >= nums[p]) merge(p,p+1);
+                while(le[p] and nums[le[p]-1] >= nums[p]) merge(le[p]-1,p);
                 res++;
-                st.push_back(n);
             }
         }
         return res;
