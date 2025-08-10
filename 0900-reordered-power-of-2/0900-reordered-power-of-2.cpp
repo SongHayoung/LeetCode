f1269fd1a24199cddbdfe34e6f9ec3dca0084a9d
SongHayoung <37806785+SongHayoung@users.noreply.github.com>
2025-08-10 09:07:31 +0900

    Time: 7 ms (12.09%), Space: 11.7 MB (5.01%) - LeetHub

---
+++ 0900-reordered-power-of-2/0900-reordered-power-of-2.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    bool reorderedPowerOf2(int N) {
+        map<int, set<map<int, int>>> powOfTwo;
+        for(long start = 1; start <= INT_MAX; start<<=1) {
+            int len = 0;
+            long num = start;
+            map<int, int> numMap;
+            while(num) {
+                numMap[num % 10]++;
+                len++;
+                num /= 10;
+            }
+            powOfTwo[len].insert(numMap);
+        }
+        
+        int len = 0;
+        map<int, int> m;
+        while(N) {
+            m[N % 10]++;
+            len++;
+            N /= 10;
+        }
+        
+        return powOfTwo[len].count(m);
+    }
+};
