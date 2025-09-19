fd443131cfe8e1ceef3a4c986f57b09187d9a22b
SongHayoung <37806785+SongHayoung@users.noreply.github.com>
2025-09-19 14:36:14 +0900

    Time: 0 ms (100%), Space: 36.8 MB (78.56%) - LeetHub

---
+++ 4012-earliest-time-to-finish-one-task/4012-earliest-time-to-finish-one-task.cpp
@@ -0,0 +1,8 @@
+class Solution {
+public:
+    int earliestTime(vector<vector<int>>& tasks) {
+        int res = INT_MAX;
+        for(auto& t : tasks) res = min(res, t[0] + t[1]);
+        return res;
+    }
+};
