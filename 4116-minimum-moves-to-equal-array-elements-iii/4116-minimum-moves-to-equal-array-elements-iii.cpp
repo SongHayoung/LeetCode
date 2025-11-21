34a1000006e3986e38bf87313b94a41242009f0d
SongHayoung <37806785+SongHayoung@users.noreply.github.com>
2025-11-21 16:21:53 +0900

    Time: 0 ms (100%), Space: 31.5 MB (56.23%) - LeetHub

--- 4116-minimum-moves-to-equal-array-elements-iii/4116-minimum-moves-to-equal-array-elements-iii.cpp
+++ 4116-minimum-moves-to-equal-array-elements-iii/4116-minimum-moves-to-equal-array-elements-iii.cpp
@@ -1,11 +1,6 @@
 class Solution {
 public:
     int minMoves(vector<int>& nums) {
-        int ma = 0, res = 0;
-        for(auto& n : nums) {
-            ma = max(ma, n);
-            res += ma - n;
-        }
-        return res;
+        return *max_element(begin(nums), end(nums)) * nums.size() - accumulate(begin(nums), end(nums), 0);
     }
 };
