da696e19e4d713c0bbbe7c07323f748d49d2d5ef
SongHayoung <37806785+SongHayoung@users.noreply.github.com>
2025-11-21 23:02:56 +0900

    Time: 33 ms (42.84%), Space: 18.1 MB (66.61%) - LeetHub

--- 0407-trapping-rain-water-ii/0407-trapping-rain-water-ii.cpp
+++ 0407-trapping-rain-water-ii/0407-trapping-rain-water-ii.cpp
@@ -1,34 +1,35 @@
 class Solution {
-    int mark[202][202];
+    bool walls[202][202];
     int dy[4]{-1,0,1,0}, dx[4]{0,1,0,-1};
 public:
     int trapRainWater(vector<vector<int>>& A) {
         int n = A.size(), m = A[0].size();
-        memset(mark, 0, sizeof mark);
-        priority_queue<array<int,3>,vector<array<int,3>>, greater<>> pq;
-           for(int i = 0; i < n; i++) {
-               pq.push({A[i][0],i,0});
-               pq.push({A[i][m-1],i,m-1});
-               mark[i][0] = mark[i][m-1] = 1;
-            }
-            for(int j = 1; j < m - 1; j++) {
-                pq.push({A[0][j],0,j});
-                pq.push({A[n-1][j],n-1,j});
-                mark[0][j] = mark[n-1][j] = 1;
-            }
+        priority_queue<array<int,3>, vector<array<int,3>>, greater<>> q;
+        memset(walls,0,sizeof walls);
+        auto push = [&](int y, int x) {
+            walls[y][x] = true;
+            q.push({A[y][x],y,x});
+        };
+        for(int i = 0; i < n; i++) {
+            push(i,0), push(i,m-1);
+        }
+        for(int j = 1; j < m - 1; j++) {
+            push(0,j), push(n-1,j);
+        }
         int res = 0;
-        while(pq.size()) {
-            auto [h,y,x] = pq.top(); pq.pop();
+        while(q.size()) {
+            auto [_,y,x] = q.top(); q.pop();
             for(int i = 0; i < 4; i++) {
-                int ny = y + dy[i], nx = x + dx[i];
-                if(0 <= ny and ny < n and 0 <= nx and nx < m and !mark[ny][nx]) {
-                    res += max(0, h - A[ny][nx]);
-                    mark[ny][nx] = true;
-                    pq.push({max(h,A[ny][nx]), ny,nx});
+                int ny = dy[i] + y, nx = x + dx[i];
+                if(0 <= ny and ny < n and 0 <= nx and nx < m and !walls[ny][nx]) {
+                    if(A[ny][nx] < A[y][x]) {
+                        res += A[y][x] - A[ny][nx];
+                        A[ny][nx] = A[y][x];
+                    }
+                    push(ny,nx);
                 }
             }
         }
-        
         return res;
     }
 };
