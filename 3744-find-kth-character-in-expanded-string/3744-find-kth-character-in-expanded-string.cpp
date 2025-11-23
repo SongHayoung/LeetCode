ab9a8ec886d5c37e4e77c30d2ef2e2ac7f6bef34
SongHayoung <37806785+SongHayoung@users.noreply.github.com>
2025-11-23 10:05:54 +0900

    Time: 23 ms (33.33%), Space: 44.4 MB (63.33%) - LeetHub

--- 3744-find-kth-character-in-expanded-string/3744-find-kth-character-in-expanded-string.cpp
+++ 3744-find-kth-character-in-expanded-string/3744-find-kth-character-in-expanded-string.cpp
@@ -1,27 +1,13 @@
 class Solution {
 public:
     char kthCharacter(string s, long long k) {
-        s.push_back(' ');
-        string chk = "";
+        int len = 0;
         k++;
         for(auto& ch : s) {
-            if(ch == ' ') {
-                long long len = chk.length();
-                long long tlen = len * (len + 1) / 2;
-                if(tlen < k) {
-                    k -= tlen;
-                    if(k == 1) return ' ';
-                    k -= 1;
-                } else {
-                    long long now = 1;
-                    for(auto& ch : chk) {
-                        if(k <= now) return ch;
-                        k -= now;
-                        now++;
-                    }
-                }
-                chk = "";
-            } else chk.push_back(ch);
+            if(isalpha(ch)) len++;
+            else len = 0;
+            k -= max(len, 1);
+            if(k <= 0) return ch;
         }
         return '#';
     }
