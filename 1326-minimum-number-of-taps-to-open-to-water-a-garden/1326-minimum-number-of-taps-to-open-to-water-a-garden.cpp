2eae31984f86b7d1a2bdf09915b26a5143208480
SongHayoung <37806785+SongHayoung@users.noreply.github.com>
2025-11-28 00:43:33 +0900

    Time: 13 ms (25.22%), Space: 23.4 MB (14.9%) - LeetHub

--- 1326-minimum-number-of-taps-to-open-to-water-a-garden/1326-minimum-number-of-taps-to-open-to-water-a-garden.cpp
+++ 1326-minimum-number-of-taps-to-open-to-water-a-garden/1326-minimum-number-of-taps-to-open-to-water-a-garden.cpp
@@ -1,27 +1,24 @@
 class Solution {
 public:
     int minTaps(int n, vector<int>& ranges) {
-        vector<pair<int, int>> A;
-        for(int i = 0; i < ranges.size(); i++) {
-            int l = max(0, i - ranges[i]);
-            int r = min(n, i + ranges[i]);
-            A.push_back({l,r});
-        }
-        sort(begin(A), end(A), [&](auto a, auto b) {
-            if(a.first == b.first) return a.second > b.second;
-            return a.first < b.first;
-        });
-        if(A[0].first != 0) return -1;
-        int res = 1, best = A[0].second, ma = best;
-        for(auto& [l,r] : A) {
-            if(l > best) {
+        vector<pair<int,int>> R;
+        for(int i = 0; i <= n; i++) R.push_back({i-ranges[i], i + ranges[i]});
+        sort(rbegin(R), rend(R));
+        int res = 0, until = 0, best = 0;
+        while(R.size()) {
+            auto [l,r] = R.back(); R.pop_back();
+            if(l > until) {
+                if(best < l) return -1;
                 res++;
-                best = ma;
-            }
-            if(l > best) return -1;
-            ma = max(ma, r);
+                cout<<until<<' '<<best<<endl;
+                until = best;
+                best = max(best, r);
+            } else best = max(best, r);
         }
-        if(best != n) res++, best = ma;
-        return best == n ? res : -1;
+        if(until < n) {
+            if(best >= n) return res + 1;
+            return -1;
+        }
+        return res;
     }
 };
