2f22a02f2685e3ee8899ad1c2b0c89f8da047a4d
SongHayoung <37806785+SongHayoung@users.noreply.github.com>
2026-09-03 21:13:49 +0900

    Time: 0 ms (100%), Space: 50.4 MB (73.39%) - LeetHub

---
+++ 4034-minimum-bishop-moves-to-reach-target/4034-minimum-bishop-moves-to-reach-target.cpp
@@ -0,0 +1,7 @@
+class Solution {
+public:
+    int minBishopMoves(vector<int>& source, vector<int>& target) {
+        if((source[0] + target[0] + source[1] + target[1]) & 1) return -1;
+        return abs(source[0] - target[0]) == abs(source[1] - target[1]) ? 1 : 2;
+    }
+};
