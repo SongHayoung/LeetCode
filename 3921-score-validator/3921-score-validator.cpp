8bc75f385fc95c0276aa47e2fe82753841956ea7
SongHayoung <37806785+SongHayoung@users.noreply.github.com>
2026-09-04 14:39:05 +0900

    Time: 0 ms (100%), Space: 53.8 MB (15.5%) - LeetHub

---
+++ 3921-score-validator/3921-score-validator.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    vector<int> scoreValidator(vector<string>& events) {
+        int score = 0, counter = 0;
+        for(int i = 0; i < events.size() and counter < 10; i++) {
+            if(events[i] == "W") counter++;
+            else if(events[i].length() == 2) score++;
+            else score += stoi(events[i]);
+        }
+        return {score, counter};
+    }
+};
