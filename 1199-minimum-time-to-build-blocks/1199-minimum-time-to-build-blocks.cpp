df67c81c034be9663676fc4a1ecdbb292f3d1554
SongHayoung <37806785+SongHayoung@users.noreply.github.com>
2026-09-05 22:41:00 +0900

    Time: 6 ms (63.44%), Space: 13.1 MB (75.23%) - LeetHub

--- 1199-minimum-time-to-build-blocks/1199-minimum-time-to-build-blocks.cpp
+++ 1199-minimum-time-to-build-blocks/1199-minimum-time-to-build-blocks.cpp
@@ -1,13 +1,13 @@
 class Solution {
 public:
   int minBuildTime(vector<int>& blocks, int split) {
-        priority_queue<int> pq;
-        for (int b : blocks) pq.push(-b);
+        priority_queue<int,vector<int>,greater<>> pq;
+        for (int b : blocks) pq.push(b);
         while (pq.size() > 1) {
-            int a = -pq.top(); pq.pop();
-            int b = -pq.top(); pq.pop();
-            pq.push(-(split + b));
+            int a = pq.top(); pq.pop();
+            int b = pq.top(); pq.pop();
+            pq.push((split + b));
         }
-        return -pq.top();
+        return pq.top();
     }
 };
