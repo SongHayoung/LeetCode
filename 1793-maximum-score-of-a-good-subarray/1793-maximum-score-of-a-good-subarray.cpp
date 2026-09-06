5d5652135371cef623a5a6e066a8b4168b423be3
SongHayoung <37806785+SongHayoung@users.noreply.github.com>
2026-09-06 16:10:00 +0900

    Time: 6 ms (48.61%), Space: 93.4 MB (76.39%) - LeetHub

--- 1793-maximum-score-of-a-good-subarray/1793-maximum-score-of-a-good-subarray.cpp
+++ 1793-maximum-score-of-a-good-subarray/1793-maximum-score-of-a-good-subarray.cpp
@@ -1,20 +1,29 @@
 class Solution {
 public:
     int maximumScore(vector<int>& nums, int k) {
-        int res = nums[k], l = k, r = k, n = nums.size(), mi = nums[k];
-        while(nums.size() != 1) {
-            if(l != 0 and nums[l-1] >= mi) l--;
-            else if(r != n - 1 and nums[r + 1] >= mi) r++;
-            else if(l != 0 and r != n - 1) {
-                if(nums[l-1] >= nums[r+1]) l--;
-                else r++;
-            } else if(l == 0) r++;
-            else l--;
-            
-            mi = min({mi, nums[l], nums[r]});
-            res = max(res, mi * (r - l + 1));
-            
-            if(l == 0 and r == n - 1) break;
+        int res = nums[k], n = nums.size(), l = k, r = k, mi = nums[k];
+        while(!(l == 0 and r == n - 1)) {
+            if(l > 0 and r < n - 1) {
+                if(nums[l-1] >= nums[r+1]) {
+                    l--;
+                    mi = min(mi,nums[l]);
+                } else {
+                    r++;
+                    mi = min(mi, nums[r]);
+                }
+            } else if(l > 0) {
+                l--;
+                mi = min(mi,nums[l]);
+            } else { // r
+                r++;
+                mi = min(mi, nums[r]);
+            }
+
+
+
+
+
+            res = max(res, (r - l + 1) * mi);
         }
         return res;
     }
