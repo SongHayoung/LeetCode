09817a6bcc06753b07ed923c82509c2a87292f4a
SongHayoung <37806785+SongHayoung@users.noreply.github.com>
2026-09-06 15:48:26 +0900

    Time: 161 ms (9.18%), Space: 77.8 MB (7.17%) - LeetHub

--- 0992-subarrays-with-k-different-integers/0992-subarrays-with-k-different-integers.cpp
+++ 0992-subarrays-with-k-different-integers/0992-subarrays-with-k-different-integers.cpp
@@ -1,20 +1,47 @@
 class Solution {
 public:
-    int subarraysWithKDistinct(vector<int>& nums, int k) {
-        return atMostK(nums, k) - atMostK(nums, k - 1);
-    }
-    int atMostK(vector<int>& nums, int k) {
-        unordered_map<int, int> counter;
-        int res = 0;
-        for(int l = 0, r = 0; r < nums.size(); r++) {
-            if(counter[nums[r]]++ == 0) k--;
-            while(k < 0) {
-                if(counter[nums[l]]-- == 1)
-                    k++;
-                l++;
-            }
-            res += r - l;
+    int subarraysWithKDistinct(vector<int>& A, int k) {
+      int res = 0, l = 0, r = 0, n = A.size();
+    unordered_map<int,int> freq, lastSeen;
+    multiset<int> lastSeens;
+    auto add = [&](int idx) {
+      freq[A[idx]]++;
+      if(lastSeen.count(A[idx])) {
+        auto it = lastSeens.find(lastSeen[A[idx]]);
+        lastSeens.erase(it);
+      }
+      lastSeen[A[idx]] = idx;
+      lastSeens.insert(idx);
+    };
+    auto del = [&](int idx) {
+      if(--freq[A[idx]] == 0) {
+        freq.erase(A[idx]);
+        auto it = lastSeens.find(lastSeen[A[idx]]);
+      lastSeens.erase(it);
+      lastSeen.erase(A[idx]);
+      }
+      
+    };
+    while(r < n) {
+      while(r < n and freq.size() <= k) {
+        add(r++);
+        if(freq.size() == k) {
+          res += *begin(lastSeens) - l + 1;
         }
-        return res;
+      }
+      while(r < n and freq.size() > k) {
+        del(l++);
+        if(freq.size() == k) {
+          res += *begin(lastSeens) - l + 1;
+        }
+      }
+    }
+    while(freq.size() > k) {
+        del(l++);
+        if(freq.size() == k) {
+          res += *begin(lastSeens) - l + 1;
+        }
+      }
+    return res;
     }
 };
